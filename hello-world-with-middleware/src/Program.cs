1224eea17a7c211a05c3145060037da53c9a2bd3
Dody Gunawinata <dodyg@silverkeytech.com>
2016-08-03 18:05:22 +0300

    Implement hello world with middlewares

---
+++ hello-world-with-middleware/src/Program.cs
@@ -0,0 +1,46 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+
+namespace HelloWorldWithReload 
+{
+    public class Startup
+    {
+        public void Configure(IApplicationBuilder app)
+        {
+            //The order of these things are important. 
+            app.Use(async (context, next) =>
+            {
+                await context.Response.WriteAsync("[1] ----- \n");//1
+                await next.Invoke();
+                await context.Response.WriteAsync("[5] =====\n");
+            });
+
+            app.Use(async (context, next) =>
+            {
+                await context.Response.WriteAsync("[2] Hello world \n");//2
+                await next.Invoke();
+                await context.Response.WriteAsync("[4]  \n");//4
+            });
+
+            app.Run(async context =>
+            {
+                await context.Response.WriteAsync("[3] ----- \n");//3
+            });
+        }
+    }
+    
+   public class Program
+    {
+        public static void Main(string[] args)
+        {
+              var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
+        }
+    }
+}
