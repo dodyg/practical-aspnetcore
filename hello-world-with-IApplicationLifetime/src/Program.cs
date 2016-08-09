2263b1438ac46ee741c3ecc3012e8f45495248d0
Dody Gunawinata <dodyg@silverkeytech.com>
2016-08-09 14:18:25 +0300

    add hellow-world-with-IApplicationLifetime

---
+++ hello-world-with-IApplicationLifetime/src/Program.cs
@@ -0,0 +1,35 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+
+namespace HelloWorldWithMiddleware 
+{
+    public class Startup
+    {
+        public void Configure(IApplicationBuilder app, IApplicationLifetime lifetime)
+        {
+        
+            lifetime.ApplicationStarted.Register(() => System.Console.WriteLine("===== Server is starting"));
+            lifetime.ApplicationStopping.Register(() => System.Console.WriteLine("===== Server is stopping"));
+            lifetime.ApplicationStopped.Register(() => System.Console.WriteLine("===== Server has stopped"));
+            
+            app.Run(async context =>
+            {
+                await context.Response.WriteAsync("Hello world");
+            });
+        }
+    }
+    
+   public class Program
+    {
+        public static void Main(string[] args)
+        {
+              var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
+        }
+    }
+}
