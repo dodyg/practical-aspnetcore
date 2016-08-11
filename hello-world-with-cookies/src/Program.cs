5f6dba0f4767a69a6a90980bc9abb87eb20e3e4a
Dody Gunawinata <dodyg@silverkeytech.com>
2016-08-11 21:43:39 +0300

    Implement hello-world-with-cookies

---
+++ hello-world-with-cookies/src/Program.cs
@@ -0,0 +1,46 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+namespace HelloWorldWithReload 
+{
+    public class Startup
+    {
+        public void Configure(IApplicationBuilder app)
+        {
+            app.Run(context =>
+            {
+                var cookie = context.Request.Cookies["MyCookie"];
+
+                if (string.IsNullOrWhiteSpace(cookie))
+                {
+                    context.Response.Cookies.Append
+                    (
+                        "MyCookie",
+                        "Hello World",
+                        new CookieOptions
+                        {
+                            Path = "/",
+                            HttpOnly = false,
+                            Secure = false
+                        }
+                    );
+                }
+
+                return context.Response.WriteAsync($"Hello World Cookie: {cookie}. Refresh page to see cookie value.");
+            });
+        }
+    }
+    
+   public class Program
+    {
+        public static void Main(string[] args)
+        {
+              var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
+        }
+    }
+}
