51bdd75f4df9b9d56896ade897d8fff4a7ee7b85
Dody Gunawinata <dodyg@silverkeytech.com>
2016-08-12 10:21:29 +0300

    Implement hello-world-routing

---
+++ hello-world-with-routing/src/Program.cs
@@ -0,0 +1,40 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace HelloWorldWithReload 
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddRouting();
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            var defaultHandler = new RouteHandler(context =>
+            {
+                var routeValues = context.GetRouteData().Values;
+                return context.Response.WriteAsync(
+                    $"Hello world! Route values: [{string.Join(", ", routeValues)}] - Path [{context.Request.Path}]. context.GetRouteData() returns nothing regardless what kind of path you are requesting, e.g. '/hello-x'");
+            });
+            app.UseRouter(defaultHandler);
+        }
+    }
+    
+   public class Program
+    {
+        public static void Main(string[] args)
+        {
+              var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
+        }
+    }
+}
