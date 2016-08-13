d80e11b29f679286f4443e111412660627575627
Dody Gunawinata <dodyg@silverkeytech.com>
2016-08-13 16:26:31 +0300

    Add initial files for hello-world-routing-3

---
+++ hello-world-with-routing-3/src/Program.cs
@@ -0,0 +1,45 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace HelloWorldWithReload 
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddRouting();
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            var routes = new RouteBuilder(app);
+            routes.MapGet("", (context) => {
+                return context.Response.WriteAsync($"Home Page. Try /good or /good/morning.");
+            });
+
+            routes.MapGet("{*path}", (context) =>{
+                var routeData = context.GetRouteData();
+                var path = routeData.Values;
+                return context.Response.WriteAsync($"Path: {string.Join(",",path)}");
+            });
+
+            app.UseRouter(routes.Build());
+        }
+    }
+    
+   public class Program
+    {
+        public static void Main(string[] args)
+        {
+              var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
+        }
+    }
+}
