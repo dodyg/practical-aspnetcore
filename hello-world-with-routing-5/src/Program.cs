bda0212b113782e22e83abc47db3e8607e752384
Dody Gunawinata <dodyg@silverkeytech.com>
2016-08-15 18:23:54 +0300

    WIP - still working on nested routing

--- hello-world-with-routing-5/src/Program.cs
+++ hello-world-with-routing-5/src/Program.cs
@@ -39,6 +39,20 @@ namespace HelloWorldWithReload
 
             var routes = new RouteBuilder(app, defaultHandler);
             
+            routes.MapVerb("GET", "hello", (IApplicationBuilder app2) =>{
+                var routes2 = new RouteBuilder(app2);
+                routes2.MapGet("callme", (context) => {
+                    return context.Response.WriteAsync("call me");
+                });
+
+                routes2.MapGet("{name}", context => {
+                    var routeValues = context.GetRouteData().Values;
+                    return context.Response.WriteAsync($"hello {routeValues["name"]}");
+                });
+
+                app2.UseRouter(routes2.Build());
+            });
+
             routes.MapRoute(
                 name: "Default", 
                 template: "{*path}"
