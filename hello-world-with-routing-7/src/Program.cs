0ccec7da3704d24c362ee92dc7a21c14302407aa
Dody Gunawinata <dodyg@silverkeytech.com>
2016-08-26 17:54:30 +0200

    Complete implementaton for routing 7

--- hello-world-with-routing-7/src/Program.cs
+++ hello-world-with-routing-7/src/Program.cs
@@ -11,17 +11,19 @@ namespace Routing7
 {
     public class Startup
     {
-        public void Configure(IApplicationBuilder app, IInlineConstraintResolver resolver)
+        public void Configure(IApplicationBuilder app)
         {
             //We are building a url template from scratch, segment by segemtn, oldskool 
             var segment = new TemplateSegment();
-            segment.Parts.Add(
-                TemplatePart.CreateLiteral("hello")
-            );
-
+            segment.Parts.Add(TemplatePart.CreateLiteral("page"));
+            
             var segment2 = new TemplateSegment();
             segment2.Parts.Add(
-                TemplatePart.CreateLiteral("world")
+                TemplatePart.CreateParameter("title", 
+                    isCatchAll: true, 
+                    isOptional: true, 
+                    defaultValue: null, 
+                    inlineConstraints: new InlineConstraint[]{})
             );
 
             var segments = new TemplateSegment [] {
@@ -29,37 +31,33 @@ namespace Routing7
                 segment2
             };
 
-            var template = new RouteTemplate("hello", segments.ToList());
+            var template = new RouteTemplate("page", segments.ToList());
             var templateMatcher = new TemplateMatcher(template, new RouteValueDictionary());
 
-            app.Use(async (context, next) =>{
-                var path1 = "hello/world";
-                try
-                {
-                    var isMatch1 = templateMatcher.TryMatch(path1, new RouteValueDictionary());
-                    await context.Response.WriteAsync($"{path1} is match? {isMatch1}\n");
-                }
-                catch(Exception ex){
-                    await context.Response.WriteAsync($"Oops {path1}: {ex?.Message}\n\n");
-                }
-                finally
-                {
-                    await next.Invoke();
-                }
+            app.Use(async(context, next) => {
+                await context.Response.WriteAsync("We are using two segments, one with Literal Template Part ('page') and the other with Parameter Template Part ('title')");
+                await context.Response.WriteAsync("\n\n");
+                await next.Invoke();
             });
-
             app.Use(async (context, next) => {
-                var path1 = "/hello/world";
-                var isMatch1 = templateMatcher.TryMatch(path1, new RouteValueDictionary());
-                await context.Response.WriteAsync($"{path1} is match? {isMatch1}\n");
+                var path1 = "/page/what";
+                var routeData = new RouteValueDictionary();//This dictionary will be populated by the parameter template part (in this case "title")
+                var isMatch1 = templateMatcher.TryMatch(path1, routeData);
+                await context.Response.WriteAsync($"{path1} is match? {isMatch1} => route data value for 'title' is {routeData["title"]} \n");
                 await next.Invoke();    
             });
 
             app.Use(async (context, next) => {
-                var path1 = "/hello/";
-                var isMatch1 = templateMatcher.TryMatch(path1, new RouteValueDictionary());
-                await context.Response.WriteAsync($"{path1} is match? {isMatch1}\n");
-                await next.Invoke();    
+                 var path1 = "/page/the/old/man/and/the/sea";
+                var routeData = new RouteValueDictionary();//This dictionary will be populated by the parameter template part (in this case "title")
+                var isMatch1 = templateMatcher.TryMatch(path1, routeData);
+                await context.Response.WriteAsync($"{path1} is match? {isMatch1} => route data value for 'title' is {routeData["title"]} \n");
+                await next.Invoke();      
+            });
+
+            app.Run(async context =>
+            {
+                await context.Response.WriteAsync("");
             });
         }
     }
