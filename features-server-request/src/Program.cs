f28e9465f61ad8c5955344c2fa9b80b723a34d69
Dody Gunawinata <dodyg@silverkeytech.com>
2016-09-16 14:53:00 +0200

    Add initial code for IHttpRequestFeature sample

---
+++ features-server-request/src/Program.cs
@@ -0,0 +1,38 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Hosting.Server.Features;
+using Microsoft.AspNetCore.Http.Features;
+
+namespace HelloWorldWithReload 
+{
+    public class Startup
+    {
+        public void Configure(IApplicationBuilder app)
+        {
+            app.Use(async (context, next ) =>{
+                var request = context.Features.Get<IHttpRequestFeature>();
+                request.Headers.Add("greetings", "hello world");
+                await next.Invoke();
+            });
+
+            app.Run(context =>
+            {
+                return context.Response.WriteAsync($"hello {context.Request.Headers["greetings"]}");
+            });
+        }
+    }
+    
+   public class Program
+    {
+        public static void Main(string[] args)
+        {
+              var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
+        }
+    }
+}
