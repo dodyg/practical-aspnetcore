2290cb1bdf38dcf444278d81f7cbaef3d4dd2589
Dody Gunawinata <dodyg@silverkeytech.com>
2016-09-17 12:31:48 +0200

    Override Feature

---
+++ features-server-custom-override/src/Program.cs
@@ -0,0 +1,66 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Hosting.Server.Features;
+using Microsoft.AspNetCore.Http.Features;
+using System;
+
+namespace HelloWorldWithReload 
+{
+    interface ICustomFeature 
+    {
+        string Greetings {get;}
+    }
+
+    public class CustomFeature : ICustomFeature
+    {
+        
+        string _greetings;
+
+        public CustomFeature (string greetings)
+        {
+            _greetings = greetings;
+        }
+
+        public string  Greetings => _greetings;
+    }
+
+    public class Startup
+    {
+        public void Configure(IApplicationBuilder app)
+        {
+            app.Use(async (context, next ) =>
+            {
+                context.Features.Set<ICustomFeature>(new CustomFeature("First greeting"));
+                await next.Invoke();
+            });
+
+            app.Use(async (context, next) =>
+            {
+                var custom = context.Features.Get<ICustomFeature>();
+                await context.Response.WriteAsync($"{custom?.Greetings}\n");
+                context.Features.Set<ICustomFeature>(new CustomFeature("Second greeting"));
+                await next.Invoke();
+            });
+
+            app.Run(context =>
+            {
+                var custom = context.Features.Get<ICustomFeature>();
+                return context.Response.WriteAsync($"{custom?.Greetings}");
+            });
+        }
+    }
+    
+   public class Program
+    {
+        public static void Main(string[] args)
+        {
+              var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
+        }
+    }
+}
