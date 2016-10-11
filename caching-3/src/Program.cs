cc26e004bedb30e049e523dffdb4194a3476d72d
Dody Gunawinata <dodyg@silverkeytech.com>
2016-10-11 10:37:55 +0200

    Handle event when the cached value is removed

---
+++ caching-3/src/Program.cs
@@ -0,0 +1,68 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Caching.Memory;
+using System;
+using Microsoft.Extensions.FileProviders;
+using System.IO;
+
+namespace Caching.Two
+{
+    public class Startup
+    {
+        static string CACHE_KEY = "MyCache";
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //This is the only service available at ConfigureServices
+            services.AddMemoryCache();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory logger)
+        {
+            var fileProvider = new PhysicalFileProvider(env.ContentRootPath);
+
+            app.Run(async context =>
+            {
+                var cache = context.RequestServices.GetService<IMemoryCache>();
+                var greeting = cache.Get(CACHE_KEY) as string;
+
+                //There is no existing cache, add one
+                if (string.IsNullOrWhiteSpace(greeting))
+                {
+                    var options = new MemoryCacheEntryOptions()
+                        .SetAbsoluteExpiration(TimeSpan.FromSeconds(5))
+                        .RegisterPostEvictionCallback((object key, object value, EvictionReason reason, object state) =>
+                        {
+                            if (state == null)
+                                System.Console.WriteLine("State is null");
+
+                            Console.WriteLine($"Key '{key}' with value '{value}' was removed because of '{reason}'");
+                        });
+                    //You can also use .SetSlidingExpiration
+
+                    var message = "Hello " + DateTimeOffset.UtcNow.ToString();
+                    cache.Set(CACHE_KEY, message, options);
+                    greeting = message;
+                }
+
+                await context.Response.WriteAsync($"The cache is set to expire in 5 seconds. Check your console log to see the post eviction message event. \n");
+            });
+        }
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+              .UseKestrel()
+              .UseStartup<Startup>()
+              .Build();
+
+            host.Run();
+        }
+    }
+}
