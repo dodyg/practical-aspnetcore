b2edc341b8d2df0532416c3fc30e45a9dc16b886
Dody Gunawinata <dodyg@silverkeytech.com>
2016-10-11 10:14:38 +0200

    Add the simplest memory cache sample

---
+++ caching/src/Program.cs
@@ -0,0 +1,60 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Caching.Memory;
+using System;
+using Microsoft.Extensions.FileProviders;
+using System.IO;
+
+namespace Caching.Two 
+{
+    public class Startup
+    {
+        static string CACHE_KEY = "MyCache";
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //This is the only service available at ConfigureServices
+            services.AddMemoryCache();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory logger)
+        {
+            var fileProvider = new PhysicalFileProvider(env.ContentRootPath);
+
+            app.Run(async context =>
+            {
+                var cache = context.RequestServices.GetService<IMemoryCache>();
+                var greeting = cache.Get(CACHE_KEY) as string;
+
+                //There is no existing cache, add one
+                if (string.IsNullOrWhiteSpace(greeting)){
+                    var options = new MemoryCacheEntryOptions()
+                        .SetAbsoluteExpiration(TimeSpan.FromMinutes(100));
+                        //You can also use .SetSlidingExpiration
+
+                    var message = "Hello " + DateTimeOffset.UtcNow.ToString();
+                    cache.Set(CACHE_KEY, message, options);
+                    greeting = message;
+                }
+
+                await context.Response.WriteAsync($"After the first load, this greeting '{greeting}' is cached. \nCheck the time stamp of the message compared to this current one {DateTimeOffset.UtcNow}.\n");
+            });
+        }
+    }
+    
+   public class Program
+    {
+        public static void Main(string[] args)
+        {
+              var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
+        }
+    }
+}
