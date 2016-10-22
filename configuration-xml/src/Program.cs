696adcc86c8e77f94ec4e48d5299879d38f3e900
Dody Gunawinata <dodyg@silverkeytech.com>
2016-10-22 12:47:55 +0200

    demonstrate using configuration xml format

---
+++ configuration-xml/src/Program.cs
@@ -0,0 +1,55 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace StartupBasic 
+{
+    public class Startup
+    {
+        IConfigurationRoot _config;
+
+        public Startup(IHostingEnvironment env, ILoggerFactory logger)
+        {
+            //This is the most basic configuration you can have
+            var builder = new ConfigurationBuilder();
+            builder
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddXmlFile("settings.xml");
+
+            _config = builder.Build();
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //This is the only service available at ConfigureServices
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory logger)
+        {
+            app.Run(async (context) =>
+            {
+                foreach(var c in _config.AsEnumerable())
+                {
+                    await context.Response.WriteAsync($"{c.Key} = {_config[c.Key]}\n");
+                }
+            });
+        }
+    }
+    
+   public class Program
+    {
+        public static void Main(string[] args)
+        {
+              var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
+        }
+    }
+}
