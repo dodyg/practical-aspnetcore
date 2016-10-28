cf4a395aa6a6fc2e0e5bd3709a8abeeec5fdad3b
Dody Gunawinata <dodyg@silverkeytech.com>
2016-10-28 12:15:51 +0200

    The most basic options sample

---
+++ configuration-options/src/Program.cs
@@ -0,0 +1,54 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+
+namespace StartupBasic 
+{
+    public class ApplicationOptions
+    {
+        public string Name { get; set; }
+
+        public int MaximumLimit { get; set;}
+    }
+
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env, ILoggerFactory logger)
+        {
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.Configure<ApplicationOptions>( o =>{
+                o.Name = "Options Sample";
+                o.MaximumLimit = 10;
+            });
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory logger)
+        {
+            app.Run(context =>
+            {
+                var options = context.RequestServices.GetService<IOptions<ApplicationOptions>>();
+
+                return context.Response.WriteAsync($"Settings Name : {options.Value.Name}  - Maximum limit : {options.Value.MaximumLimit}");
+            });
+        }
+    }
+    
+   public class Program
+    {
+        public static void Main(string[] args)
+        {
+              var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
+        }
+    }
+}
