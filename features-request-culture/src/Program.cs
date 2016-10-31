bae4c7fce453c20736c2fc899a099f8d750c1b92
Dody Gunawinata <dodyg@silverkeytech.com>
2016-10-31 14:50:08 +0200

    Add sample for IRequestCultureFeature

---
+++ features-request-culture/src/Program.cs
@@ -0,0 +1,47 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Localization;
+
+namespace StartupBasic 
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env, ILoggerFactory logger)
+        {
+            //These are two services available at constructor
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //This is the only service available at ConfigureServices
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory logger)
+        {
+            app.UseRequestLocalization();
+
+            //These are the three default services available at Configure
+            app.Run(async context =>
+            {
+                var cultureFeature = context.Features.Get<IRequestCultureFeature>();
+                await context.Response.WriteAsync($"Request culture : {cultureFeature.RequestCulture.Culture.EnglishName} ");
+            });
+        }
+    }
+    
+   public class Program
+    {
+        public static void Main(string[] args)
+        {
+              var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
+        }
+    }
+}
