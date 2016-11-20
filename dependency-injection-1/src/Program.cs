72900d70081a71d6240685b637795c440d15af38
Dody Gunawinata <dodyg@silverkeytech.com>
2016-11-20 21:10:58 +0200

    Remove unused using

--- dependency-injection-1/src/Program.cs
+++ dependency-injection-1/src/Program.cs
@@ -3,7 +3,6 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.DependencyInjection;
-using System.Linq;
 using System;
 using System.Threading.Tasks;
 
