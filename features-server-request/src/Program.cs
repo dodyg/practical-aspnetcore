a4c66fd090f03e4866c687c70c14709cdc03622c
Dody Gunawinata <dodyg@silverkeytech.com>
2016-11-20 21:23:04 +0200

    remove unused using

--- features-server-request/src/Program.cs
+++ features-server-request/src/Program.cs
@@ -1,7 +1,6 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Hosting.Server.Features;
 using Microsoft.AspNetCore.Http.Features;
 
 namespace Features.ServerRequest 
