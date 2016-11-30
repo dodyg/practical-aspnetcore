3ff1f429b5b64935bb89ecb1c30982c03cf53bab
Dody Gunawinata <dodyg@silverkeytech.com>
2016-11-30 17:44:13 +0200

    Demonstrate switching culture using QueryStringRequestCultureProvider

---
+++ localization-2/src/Program.cs
@@ -0,0 +1,74 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Localization;
+using Microsoft.Extensions.Localization;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace Local 
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env, ILoggerFactory logger)
+        {
+            //These are two services available at constructor
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddLocalization(options => options.ResourcesPath = "resources");
+            //This is the only service available at ConfigureServices
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory logger, IStringLocalizerFactory stringLocalizerFactory)
+        {
+            var local = stringLocalizerFactory.Create("Common", null);
+
+            //This section is important otherwise aspnet won't be able to pick up the resource
+            var supportedCultures = new List<CultureInfo>
+            {
+                new CultureInfo("fr-FR"),
+                new CultureInfo("en-US")
+            };
+            
+            var options = new RequestLocalizationOptions
+            {
+                DefaultRequestCulture = new RequestCulture("fr-FR"),
+                SupportedCultures = supportedCultures,
+                SupportedUICultures = supportedCultures
+            };
+
+            app.UseRequestLocalization(options);
+
+            //These are the three default services available at Configure
+            app.Run(async context =>
+            {
+                await context.Response.WriteAsync("<h1>QueryStringRequestCultureProvider</h1><p>We are using query string to switch the request culture.</p>");
+
+                var requestCulture = context.Features.Get<IRequestCultureFeature>().RequestCulture;
+                if (requestCulture.Culture.TwoLetterISOLanguageName != "fr")
+                    await context.Response.WriteAsync($"<a href=\"/?culture=fr-FR&ui-culture=fr-FR\">Switch to French</a><br/>");
+                else if (requestCulture.Culture.TwoLetterISOLanguageName != "en")
+                    await context.Response.WriteAsync($"<a href=\"/?culture=en-US&ui-culture=en-US\">Switch to English</a><br/>");
+                
+                await context.Response.WriteAsync($"Request Culture: {requestCulture.Culture} - {local["Hello"]} {local["Goodbye"]} {local["Yes"]} {local["No"]}");
+            });
+        }
+    }
+    
+   public class Program
+    {
+        public static void Main(string[] args)
+        {
+              var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
+        }
+    }
+}
