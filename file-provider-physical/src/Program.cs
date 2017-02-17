b455495ae42ba4f01a55c7499a56d26cb15b2830
Dody Gunawinata <dodyg@silverkeytech.com>
2017-02-17 23:04:06 +0200

    Add sample on how to use PhysicalFileProvider

---
+++ file-provider-physical/src/Program.cs
@@ -0,0 +1,77 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
+using System.IO;
+using Microsoft.Extensions.FileProviders;
+
+namespace FileProviderPhysical 
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env, ILoggerFactory logger)
+        {
+            //These are two services available at constructor
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //This is the only service available at ConfigureServices
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory logger)
+        {
+            //These are the three default services available at Configure
+            
+            app.Run(async context =>
+            {
+                context.Response.Headers.Add("content-type", "text/html");
+
+                var contentRoot = env.ContentRootFileProvider;
+                var contentPhysical = env.ContentRootFileProvider as PhysicalFileProvider;
+
+                await context.Response.WriteAsync("<h1>Content Root</h1>");                
+                await context.Response.WriteAsync($"{contentPhysical.Root}");
+                await context.Response.WriteAsync($"<ul>");
+                foreach(var f in contentRoot.GetDirectoryContents(""))
+                {
+                    if (f.IsDirectory)
+                        await context.Response.WriteAsync($"<li>{f.Name} - Directory</li>");
+                    else
+                        await context.Response.WriteAsync($"<li>{f.Name}</li>");
+                }
+                await context.Response.WriteAsync($"</ul>");
+
+                var webRoot = env.WebRootFileProvider;
+                var webPhysical = env.WebRootFileProvider as PhysicalFileProvider;
+
+                await context.Response.WriteAsync("<h1>Web Root</h1>");                
+                await context.Response.WriteAsync($"{webPhysical.Root}");
+                await context.Response.WriteAsync($"<ul>");
+                foreach(var f in webRoot.GetDirectoryContents(""))
+                {
+                    if (f.IsDirectory)
+                        await context.Response.WriteAsync($"<li>{f.Name} - Directory</li>");
+                    else
+                        await context.Response.WriteAsync($"<li>{f.Name}</li>");
+                }
+                await context.Response.WriteAsync($"</ul>");
+            });
+        }
+    }
+    
+   public class Program
+    {
+        public static void Main(string[] args)
+        {
+              var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory()) //If you remove this, ContentRootFileProvider will return something different. Try it out.
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
+        }
+    }
+}
