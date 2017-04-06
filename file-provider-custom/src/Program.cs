5254ee055b703daaede66432251268141a2ad53f
Dody Gunawinata <dodyg@silverkeytech.com>
2017-04-06 14:45:40 +0200

    Put skeleton of IFileProvider

--- file-provider-custom/src/Program.cs
+++ file-provider-custom/src/Program.cs
@@ -5,9 +5,66 @@ using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.DependencyInjection;
 using System.IO;
 using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.Primitives;
+using System;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace FileProviderPhysical 
 {
+    public class CustomDirectoryContents : IDirectoryContents
+    {
+        public bool Exists => throw new NotImplementedException();
+
+        public IEnumerator<IFileInfo> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class CustomFileInfo : IFileInfo
+    {
+        public bool Exists => throw new NotImplementedException();
+
+        public long Length => throw new NotImplementedException();
+
+        public string PhysicalPath => throw new NotImplementedException();
+
+        public string Name => throw new NotImplementedException();
+
+        public DateTimeOffset LastModified => throw new NotImplementedException();
+
+        public bool IsDirectory => throw new NotImplementedException();
+
+        public Stream CreateReadStream()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class CustomFileProvider : IFileProvider
+    {
+        public IDirectoryContents GetDirectoryContents(string subpath)
+        {
+            return new CustomDirectoryContents();
+        }
+
+        public IFileInfo GetFileInfo(string subpath)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IChangeToken Watch(string filter)
+        {
+            return NullChangeToken.Singleton;
+        }
+    }
+
     public class Startup
     {
         public Startup(IHostingEnvironment env, ILoggerFactory logger)
@@ -22,8 +79,6 @@ namespace FileProviderPhysical
 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory logger)
         {
-            //These are the three default services available at Configure
-            
             app.Run(async context =>
             {
                 context.Response.Headers.Add("content-type", "text/html");
