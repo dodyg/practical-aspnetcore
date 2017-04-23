1900e3c895565700d8de47d3b9894fd193c4a8eb
Dody Gunawinata <dodyg@silverkeytech.com>
2017-04-23 12:08:31 +0200

    Middleware filter

---
+++ middleware-2/src/Program.cs
@@ -0,0 +1,62 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Middleware
+{
+    //https://github.com/aspnet/Entropy/blob/dev/samples/Builder.Filtering.Web/Startup.cs
+    public class Startup
+    {
+        public void Configure(IApplicationBuilder app)
+        {
+            //We use it the type so it is clear. We will use the shortened version from now on.
+            //app.Use(next => context => FilterAsync(context, next));
+            app.Use((RequestDelegate next) =>
+            {
+                return (HttpContext context) => FilterAsync(context, next);
+            });
+
+            app.Run(async context =>
+            {
+                context.Response.Headers.Add("content-type", "text/html");
+                await context.Response.WriteAsync("Hello world");
+            });
+        }
+
+        public async Task FilterAsync(HttpContext context, RequestDelegate next)
+        {
+            var body = context.Response.Body;
+            var buffer = new MemoryStream();
+            context.Response.Body = buffer;
+
+            try
+            {
+                await context.Response.WriteAsync("Before Filter \n");
+                await next(context);
+                await context.Response.WriteAsync("\nAfter Filter");
+
+                buffer.Position = 0;
+                await buffer.CopyToAsync(body);
+            }
+            finally
+            {
+                context.Response.Body = body;
+            }
+        }
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+              .UseKestrel()
+              .UseStartup<Startup>()
+              .Build();
+
+            host.Run();
+        }
+    }
+}
