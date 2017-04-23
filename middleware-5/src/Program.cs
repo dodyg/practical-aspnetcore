8321870bce69c58d7ef4dbda13b5ba92f5bc828f
Dody Gunawinata <dodyg@silverkeytech.com>
2017-04-23 23:48:08 +0200

    Add sample about nested app.Map

---
+++ middleware-5/src/Program.cs
@@ -0,0 +1,44 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace HelloWorldWithMiddleware
+{
+    public class Startup
+    {
+        public void Configure(IApplicationBuilder app)
+        {
+            app.Map("/hello", (IApplicationBuilder pp) => {
+                //nested
+                app.Map("/world", (IApplicationBuilder ppa) => ppa.Run(context => 
+                    context.Response.WriteAsync($"Path: {context.Request.Path} - Path Base: {context.Request.PathBase}")));
+                
+                pp.Run(context => 
+                    context.Response.WriteAsync($"Path: {context.Request.Path} - Path Base: {context.Request.PathBase}"));
+            });
+            
+            app.Run(context =>
+            { 
+                context.Response.Headers.Add("content-type", "text/html");
+                return context.Response.WriteAsync(@"
+                   <a href=""/hello"">/hello</a> <a href=""/hello/world"">/hello/world</a>
+                ");
+            });
+        }
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+              .UseKestrel()
+              .UseStartup<Startup>()
+              .Build();
+
+            host.Run();
+        }
+    }
+}
