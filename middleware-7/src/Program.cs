2183f43020b40a0219816c2cea477330e7070647
Dody Gunawinata <dodyg@silverkeytech.com>
2017-04-24 23:32:50 +0200

    Use MapMiddleware and MapWhenMiddleware directly

---
+++ middleware-7/src/Program.cs
@@ -0,0 +1,55 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Builder.Extensions;
+
+namespace HelloWorldWithMiddleware
+{
+    public class Startup
+    {
+        public void Configure(IApplicationBuilder app)
+        {
+            //Use MapMiddleWare and MapWhenMiddlware directly
+            var whenOption = new MapWhenOptions
+            {
+                Branch =  
+                    context => 
+                        context.Response.WriteAsync($"Path: {context.Request.Path} - Path Base: {context.Request.PathBase}"),
+                Predicate = context => context.Request.Path.Value.Contains("hello")
+            };
+            app.UseMiddleware<MapWhenMiddleware>(whenOption);
+        
+            var mapOption = new MapOptions
+            {
+                 Branch =  
+                    context => 
+                        context.Response.WriteAsync($"Path: {context.Request.Path} - Path Base: {context.Request.PathBase}"),
+                 PathMatch = "/greetings"
+            };
+            app.UseMiddleware<MapMiddleware>(mapOption);
+
+            app.Run(context =>
+            { 
+                context.Response.Headers.Add("content-type", "text/html");
+                return context.Response.WriteAsync(@"
+                   <a href=""/hello"">/hello</a> <a href=""/greetings"">/greetings</a>
+                ");
+            });
+        }
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+              .UseKestrel()
+              .UseStartup<Startup>()
+              .Build();
+
+            host.Run();
+        }
+    }
+}
