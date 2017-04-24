d79c2c8fe0dd215ba18c58caf121bedd794c9264
Dody Gunawinata <dodyg@silverkeytech.com>
2017-04-24 23:52:08 +0200

    Request.Connection property

---
+++ hello-world-startup-basic-connection-info/src/Program.cs
@@ -0,0 +1,50 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace StartupBasic 
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env, ILoggerFactory logger)
+        {
+            //These are two services available at constructor
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //This is the only service available at ConfigureServices
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory logger)
+        {
+            //These are the three default services available at Configure
+           
+            app.Run(context =>
+            {
+                var connection = context.Connection;
+                var str = string.Empty;
+                str += $"Local IP:Port => {connection.LocalIpAddress}:{connection.LocalPort}\n";
+                str += $"Remote IP:Port => {connection.RemoteIpAddress}:{connection.RemotePort}\n";
+                str += $"ClientCertificate : {connection.ClientCertificate?.FriendlyName}\n";
+
+                return context.Response.WriteAsync($"{str}");
+            });
+        }
+    }
+    
+   public class Program
+    {
+        public static void Main(string[] args)
+        {
+              var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
+        }
+    }
+}
