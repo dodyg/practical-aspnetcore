c2fdafa8a357abe4c59bd10fd14d98e93a9498d3
Dody Gunawinata <dodyg@silverkeytech.com>
2017-06-03 11:33:12 +0200

    Provide UseExceptionHandler

---
+++ diagnostics-3/src/Program.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Text.Encodings.Web;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Features;
+
+namespace StartupBasic 
+{
+   public class Program
+   {
+        public static void Main(string[] args)
+        {
+              var host = new WebHostBuilder()
+                .UseKestrel()
+                .Configure(app =>
+                {
+                    app.UseExceptionHandler(errorApp =>
+                    {
+                        errorApp.Run(async context =>
+                        {
+                            context.Response.StatusCode = 500;
+                            context.Response.ContentType = "text/html";
+
+                            var feature = context.Features.Get<IExceptionHandlerFeature>();
+
+                            if (feature != null)
+                            {
+                                await context.Response.WriteAsync($"<h1>Custom Error Page</h1> {HtmlEncoder.Default.Encode(feature.Error.Message)}");
+                                await context.Response.WriteAsync($"<hr />{HtmlEncoder.Default.Encode(feature.Error.Source)}");
+                            }
+                        });
+                    });
+
+                    //trigger exception
+                    app.Run(context => throw new Exception("Hello World Exception"));
+                })
+                .Build();
+
+            host.Run();
+        }
+    }
+}
