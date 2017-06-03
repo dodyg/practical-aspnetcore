b04d3b6743e164b103fa3497136845234267f72c
Dody Gunawinata <dodyg@silverkeytech.com>
2017-06-03 11:41:33 +0200

    Use a specific custom error page

--- diagnostics-4/src/Program.cs
+++ diagnostics-4/src/Program.cs
@@ -1,9 +1,10 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.DependencyInjection;
 using System;
+using System.Text.Encodings.Web;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Features;
 
 namespace StartupBasic 
 {
@@ -15,7 +16,25 @@ namespace StartupBasic
                 .UseKestrel()
                 .Configure(app =>
                 {
-                    app.UseDeveloperExceptionPage(); //Don't use this in production
+                    app.UseExceptionHandler("/GlobalError");
+
+                    app.Map("/GlobalError", errorApp =>
+                    {
+                        errorApp.Run(async context =>
+                        {
+                            context.Response.StatusCode = 500;
+                            context.Response.ContentType = "text/html";
+
+                            var feature = context.Features.Get<IExceptionHandlerFeature>();
+
+                            if (feature != null)
+                            {
+                                await context.Response.WriteAsync($"<h1>Custom Error Page</h1> {HtmlEncoder.Default.Encode(feature.Error.Message)}");
+                                await context.Response.WriteAsync($"<hr />{HtmlEncoder.Default.Encode(feature.Error.Source)}");
+                            }
+                        });
+                    });
+                    
                     app.Run(context => throw new ApplicationException("Hello World Exception"));
                 })
                 .Build();
