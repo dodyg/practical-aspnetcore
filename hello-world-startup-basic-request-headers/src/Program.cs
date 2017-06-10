fae4b2ac9475fa28461b6e6b4ad84dcbdf82d38c
Dody Gunawinata <dodyg@silverkeytech.com>
2017-06-10 19:51:46 +0200

    ...

---
+++ hello-world-startup-basic-request-headers/src/Program.cs
@@ -0,0 +1,49 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace StartupBasic 
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env, ILoggerFactory logger)
+        {
+            //These are two services available at constructor
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //This is the only service available at ConfigureServices
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory logger)
+        {
+            app.Run(async context =>
+            {
+                context.Response.Headers.Add("content-type", "text/html");
+                await context.Response.WriteAsync("<h1>Request Headers</h1>");
+                await context.Response.WriteAsync("<ul>");
+                foreach(var h in context.Request.Headers)         
+                {
+                    await context.Response.WriteAsync($"<li>{h.Key} : {h.Value}</li>");
+                }       
+                await context.Response.WriteAsync("</ul>");
+            });
+        }
+    }
+    
+   public class Program
+    {
+        public static void Main(string[] args)
+        {
+              var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
+        }
+    }
+}
