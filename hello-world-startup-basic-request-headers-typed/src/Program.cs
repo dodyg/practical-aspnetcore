13dde2a190171985f11c008d48edefaf3bfef403
Dody Gunawinata <dodyg@silverkeytech.com>
2017-06-15 20:29:50 +0200

    Use type safe acceses to HTTP Request Headers

---
+++ hello-world-startup-basic-request-headers-typed/src/Program.cs
@@ -0,0 +1,51 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
+using System.Linq;
+
+namespace StartupBasic 
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env, ILoggerFactory logger)
+        {
+            //These are two services available at constructor
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //This is the only service available at ConfigureServices
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory logger)
+        {
+            //These are the three default services available at Configure
+            
+            app.Run(context =>
+            {
+                var typedHeaders = context.Request.GetTypedHeaders();
+
+                return context.Response.WriteAsync($@"
+There are more common HTTP headers properties available in HttpContext.Request.GetTypedHeaders()              
+Accept: {typedHeaders.Accept[0]}
+Accept Language : {typedHeaders.AcceptLanguage.FirstOrDefault()?.Value}                
+                ");
+            });
+        }
+    }
+    
+   public class Program
+    {
+        public static void Main(string[] args)
+        {
+              var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
+        }
+    }
+}
