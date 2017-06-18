80ea67582057da7425be60ce0d4f9a0aea84b34f
Dody Gunawinata <dodyg@silverkeytech.com>
2017-06-18 13:43:50 +0200

    Add browse directory

---
+++ serve-static-files-2/src/Program.cs
@@ -0,0 +1,30 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using System.IO;
+
+namespace ServeStaticFiles 
+{
+    public class Startup
+    {
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseStaticFiles();
+            app.UseDirectoryBrowser();
+        }
+    }
+    
+   public class Program
+    {
+        public static void Main(string[] args)
+        {
+              var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
+        }
+    }
+}
