4df3b1b5c392ce7b2ce0fe81c4d749510f6c5bb5
Dody Gunawinata <dodyg@silverkeytech.com>
2017-07-07 21:06:11 +0200

    Rough broadcast sample implemented

--- web-sockets-3/src/Program.cs
+++ web-sockets-3/src/Program.cs
@@ -8,11 +8,86 @@ using System;
 using System.Threading;
 using System.Text;
 using System.IO;
+using System.Collections.Concurrent;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Collections.Generic;
 
 namespace StartupBasic
 {
+    public class ConnectionManager
+    {
+        ConcurrentDictionary<string, WebSocket> _sockets = new ConcurrentDictionary<string, WebSocket>();
+        
+        public string AddSocket(WebSocket socket){
+           var id = Guid.NewGuid().ToString();
+            
+            if (!_sockets.TryAdd(id, socket))
+                throw new Exception($"Problem in adding socket with Id {id}");
+        
+            return id;
+        }
+
+        public WebSocket GetSocketById(string id) => _sockets.FirstOrDefault(x => x.Key == id).Value;
+
+        public string GetId(WebSocket socket) => _sockets.FirstOrDefault(p => p.Value == socket).Key;
+
+        public List<(WebSocket socket, string id)> Other(string id) => _sockets.Where(x => x.Key != id).Select(x => (socket: x.Value, id: x.Key)).ToList();
+
+        public ConcurrentDictionary<string, WebSocket> GetAll() => _sockets;
+
+        public async Task RemoveAsync(string id)
+        {
+            if (_sockets.TryRemove(id, out WebSocket socket))
+            {
+                await socket.CloseAsync(closeStatus: WebSocketCloseStatus.NormalClosure, 
+                    statusDescription: "Closed by the Server", cancellationToken: CancellationToken.None);
+            }
+        }
+    }
+
     public class Startup
     {
+        async Task ReceiveAsync(ILogger log, WebSocket socket, string socketId, Func<string, Task> responseHandler)
+        {
+            var bufferSize = new byte[4];
+            var receiveBuffer = new ArraySegment<byte>(bufferSize);
+            WebSocketReceiveResult result;
+
+            while (socket.State == WebSocketState.Open)
+            {
+                using (var ms = new MemoryStream())
+                {
+                    do
+                    {
+                        result = await socket.ReceiveAsync(receiveBuffer, CancellationToken.None);
+                        if (result.MessageType != WebSocketMessageType.Text)
+                            throw new Exception("Unexpected Message");
+
+                        ms.Write(receiveBuffer.Array, receiveBuffer.Offset, result.Count);
+
+                        receiveBuffer = new ArraySegment<byte>(bufferSize);
+                    }
+                    while (!result.EndOfMessage && !result.CloseStatus.HasValue);
+
+                    ms.Seek(0, SeekOrigin.Begin);
+
+                    string clientRequest = string.Empty;
+                    using (var reader = new StreamReader(ms, Encoding.UTF8))
+                    {
+                        clientRequest = await reader.ReadToEndAsync();
+                    }
+
+                    log.LogDebug($"Socket Id {socketId} : Receive: {clientRequest}");
+
+                    await responseHandler(clientRequest);
+
+                    if (result.CloseStatus.HasValue)
+                        break;
+                }
+            }
+        }
+
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory logger)
         {
             logger.AddConsole((str, level) =>
@@ -21,9 +96,13 @@ namespace StartupBasic
                 return !str.Contains("Microsoft.AspNetCore") && level >= LogLevel.Trace;
             });
 
+            int count = 0;
+
             var log = logger.CreateLogger("");
 
             app.UseWebSockets();
+
+            var cm = new ConnectionManager();
 
             app.Use(async (context, next) =>
             {
@@ -35,46 +114,23 @@ namespace StartupBasic
                 }
 
                 var socket = await context.WebSockets.AcceptWebSocketAsync();
-                var bufferSize = new byte[4];
-                var receiveBuffer = new ArraySegment<byte>(bufferSize);
-                WebSocketReceiveResult result;
+                var socketId = cm.AddSocket(socket);
 
-                while (socket.State == WebSocketState.Open)
+                await ReceiveAsync(log, socket, socketId, async (clientRequest) => 
                 {
-                    using (var ms = new MemoryStream())
+                    var serverReply = Encoding.UTF8.GetBytes($"Echo {++count} {clientRequest}");
+                    var replyBuffer = new ArraySegment<byte>(serverReply);
+                    await socket.SendAsync(replyBuffer, WebSocketMessageType.Text, true, CancellationToken.None);
+
+                    var broadcastReply = Encoding.UTF8.GetBytes($"Broadcast {count} {clientRequest}");
+                    var broadcastBuffer = new ArraySegment<byte>(broadcastReply);
+
+                    foreach(var (s, sid) in cm.Other(socketId))
                     {
-                        int count = 0;
-                        do
-                        {
-                            result = await socket.ReceiveAsync(receiveBuffer, CancellationToken.None);
-                            if (result.MessageType != WebSocketMessageType.Text)
-                                throw new Exception("Unexpected Message");
-
-                            ms.Write(receiveBuffer.Array, receiveBuffer.Offset, result.Count);
-
-                            receiveBuffer = new ArraySegment<byte>(bufferSize);
-                            log.LogDebug($"Reading incoming data with buffer(size {bufferSize.Length}) {++count} times");
-                        }
-                        while (!result.EndOfMessage && !result.CloseStatus.HasValue);
-
-                        ms.Seek(0, SeekOrigin.Begin);
-
-                        string clientRequest = string.Empty;
-                        using (var reader = new StreamReader(ms, Encoding.UTF8))
-                        {
-                            clientRequest = await reader.ReadToEndAsync();
-                        }
-
-                        log.LogDebug($"Receive: {clientRequest}");
-
-                        var serverReply = Encoding.UTF8.GetBytes("Echo " + clientRequest);
-                        var replyBuffer = new ArraySegment<byte>(serverReply);
-                        await socket.SendAsync(replyBuffer, WebSocketMessageType.Text, true, CancellationToken.None);
-
-                        if (result.CloseStatus.HasValue)
-                            break;
+                        await s.SendAsync(broadcastBuffer, WebSocketMessageType.Text, true, CancellationToken.None);
+                        log.LogDebug($"Broadcasting to : {sid}");
                     }
-                }
+                });
             });
 
             app.Run(async context =>
@@ -86,15 +142,17 @@ namespace StartupBasic
         <script src=""https://code.jquery.com/jquery-3.2.1.min.js"" integrity=""sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="" crossorigin=""anonymous""></script>
     </head>
     <body>
-        <h1>Web Socket</h1>
+        <h1>Web Socket (please open this page at 2 tabs at least)</h1>
         <input type=""text"" length=""50"" id=""msg"" value=""hello world""/> <button type=""button"" id=""send"">Send</button>
         <br/>
+        <ul id=""responses""></ul>
         <script>
             $(function(){
                 var url = ""ws://localhost:5000"";
                 var socket = new WebSocket(url);
                 var send = $(""#send"");
                 var msg = $(""#msg"");
+                var responses = $(""#responses"");
 
                 socket.onopen = function(e){
                     send.click(function(){
@@ -104,7 +162,7 @@ namespace StartupBasic
 
                 socket.onmessage = function(e){
                     var response = e.data;
-                    alert(response.trim());
+                    responses.append(`<li>${e.data.trim()}</li>`);
                 };
             });
         </script>
