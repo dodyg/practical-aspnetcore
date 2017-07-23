4c7796eac1258408248019968003fc4e3728a18e
Dody Gunawinata <dodyg@silverkeytech.com>
2017-07-23 11:14:47 +0200

    Switch to netcoreapp1.1 and improve code

--- projects/routing/src/Program.cs
+++ projects/routing/src/Program.cs
@@ -4,7 +4,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace Routing 
+namespace Routing
 {
     public class Startup
     {
@@ -18,21 +18,41 @@ namespace Routing
             var defaultHandler = new RouteHandler(context =>
             {
                 var routeValues = context.GetRouteData().Values;
+                context.Response.Headers.Add("Content-Type", "text/html");
                 return context.Response.WriteAsync(
-                    $"Hello world! Route values: [{string.Join(", ", routeValues)}] - Path [{context.Request.Path}]. context.GetRouteData() returns nothing regardless what kind of path you are requesting, e.g. '/hello-x'");
+                    $@"
+                    <html><head><body>
+                    <h1>Routing</h1>
+                    Click on the following links to see the changes
+                    <ul>
+                        <li><a href=""/try"">/try</a></li>
+                        <li><a href=""/do/33"">/do/33</a></li>
+                        <li><a href=""/values/11/again"">/values/11/again</a></li>
+                    </ul> 
+                    <br/>
+                    Path: {context.Request.Path}. 
+                    <br/>
+                    <br/>
+                    Route values from context.GetRouteData().Values: {string.Join(", ", routeValues)}
+                    <br/>
+                    Note:
+                    <br/>
+                    context.GetRouteData() returns nothing regardless what kind of path you are requesting, e.g. '/hello-x'
+                    </body></html>
+                    ");
             });
             app.UseRouter(defaultHandler);
         }
     }
-    
-   public class Program
+
+    public class Program
     {
         public static void Main(string[] args)
         {
-              var host = new WebHostBuilder()
-                .UseKestrel()
-                .UseStartup<Startup>()
-                .Build();
+            var host = new WebHostBuilder()
+              .UseKestrel()
+              .UseStartup<Startup>()
+              .Build();
 
             host.Run();
         }
