b51d0188bb4ec3484bb63154e349ffb32b4dc3dc
Lukasz Olender <lukasz.olender@ac-project.net>
2017-09-16 13:46:43 +0200

    Added example for razor pages with inline and separate codebehind files.

---
+++ projects/aspnet-core-2/razor-pages-basic/Pages/SeparateCodebehindFile.cshtml.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+
+namespace RazorPagesBasic.Pages
+{
+    public class SeparateCodebehindFileModel : PageModel
+    {
+        public string Title => "Page with separate codebehind file model";
+        public string Message { get; private set; }
+
+        public void OnGet()
+        {
+            Message = $"Generated at { DateTime.Now.ToLongTimeString() }.";
+        }
+    }
+}
