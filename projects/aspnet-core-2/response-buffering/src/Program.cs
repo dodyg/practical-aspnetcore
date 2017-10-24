304fe560f38adcb310f9fd36055842758752134c
Dody Gunawinata <dodyg@silverkeytech.com>
2017-10-24 09:06:17 +0200

    Add response buffering

---
+++ projects/aspnet-core-2/response-buffering/src/Program.cs
@@ -0,0 +1,46 @@
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace StartupBasic 
+{
+    public class Startup
+    {
+        public void Configure(IApplicationBuilder app)
+        {
+            //Comment this code out and you will see the whole program will stop working 
+            app.UseResponseBuffering();
+
+            //These are the four default services available at Configure
+            app.Run(async context =>
+            {
+                context.Response.ContentType = "application/json";
+                await context.Response.WriteAsync(Newtonsoft.Json.JsonConvert.SerializeObject(new { Name = "Annie", Status = "Amore"}));
+                
+                //Without buffering this would have generated exception
+                context.Response.Headers.Clear();
+                context.Response.ContentType = "text/plain";
+                
+                context.Response.Body.SetLength(0);
+                await context.Response.WriteAsync("hello world");
+            });
+        }
+    }
+    
+   public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BuildWebHost(args).Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .UseEnvironment("Development")
+                .Build();
+    }
+}
