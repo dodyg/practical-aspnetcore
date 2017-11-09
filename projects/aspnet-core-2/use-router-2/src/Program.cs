74cf9648cf59c55417f4f5f49ca432ca120d018f
Dody Gunawinata <dodyg@silverkeytech.com>
2017-11-09 08:33:25 +0200

    A second sample for UseRouter()

---
+++ projects/aspnet-core-2/use-router-2/src/Program.cs
@@ -0,0 +1,55 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Routing;
+
+namespace StartupBasic 
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env, ILoggerFactory logger, IConfiguration configuration)
+        {
+            //These are three services available at constructor
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddRouting();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory logger, IConfiguration configuration)
+        {
+            app.UseRouter(r =>{
+                r.MapGet("", async (request, response, route) => {
+                    response.Headers.Add("Content-Type", "text/html");
+                    await response.WriteAsync($"Hello world <br><a href=\"good-morning/good morning\">Greet good morning</a>");
+                });
+
+                r.MapGet("good-morning/{greeting}", async (request, response, route) => {
+                    response.Headers.Add("Content-Type", "text/html");
+                    var greeting = route.Values["greeting"];
+                    await response.WriteAsync($"{greeting}");  
+                });
+
+            });
+        }
+    }
+    
+   public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BuildWebHost(args).Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .UseEnvironment("Development")
+                .Build();
+    }
+}
