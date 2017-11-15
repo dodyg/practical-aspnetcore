835b39cddd487ea136be0a9931d3bee8be1b4041
Dody Gunawinata <dodyg@silverkeytech.com>
2017-11-15 13:37:23 +0200

    Add device type detection

---
+++ projects/aspnet-core-2/device-detection/src/Program.cs
@@ -0,0 +1,53 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore;
+using Microsoft.Extensions.Configuration;
+using Wangkanai.Detection;
+
+namespace StartupBasic 
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env, ILoggerFactory logger, IConfiguration configuration)
+        {
+            //These are three services available at constructor
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDetection()
+                .AddDevice(); 
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory logger, IConfiguration configuration)
+        {
+            //These are the four default services available at Configure
+            app.Run(context =>
+            {
+                var device = context.RequestServices.GetService<IDeviceResolver>();
+
+                return context.Response.WriteAsync($@"
+                Useragent : {device.UserAgent}
+                Device Type: {device.Device.Type}
+                ");
+            });
+        }
+    }
+    
+   public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BuildWebHost(args).Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .UseEnvironment("Development")
+                .Build();
+    }
+}
