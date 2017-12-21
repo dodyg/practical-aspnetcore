719d61027d1d462e5cb714371705ece9b17b3818
Dody Gunawinata <dodyg@silverkeytech.com>
2017-12-21 17:16:46 +0200

    Initial commit for signalr example

---
+++ projects/signalr-2/src/Program.cs
@@ -0,0 +1,19 @@
+using System;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+
+namespace Signalr2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BuildWebHost(args).Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .Build();
+    }
+}
