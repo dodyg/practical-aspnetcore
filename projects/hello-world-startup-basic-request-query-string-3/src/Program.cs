c6d081ee920e40542e50cae3da1d717cf5bbf840
Dody Gunawinata <dodyg@silverkeytech.com>
2018-01-20 23:22:03 +0200

    Add iterating all query string

---
+++ projects/hello-world-startup-basic-request-query-string-3/src/Program.cs
@@ -0,0 +1,55 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Primitives;
+
+namespace StartupBasic 
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env, ILoggerFactory logger)
+        {
+            //These are two services available at constructor
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //This is the only service available at ConfigureServices
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory logger)
+        {
+            //These are the three default services available at Configure
+            app.Run(async context =>
+            {
+                context.Response.Headers.Add("content-type", "text/html");
+
+                await context.Response.WriteAsync("<html><body>");
+                await context.Response.WriteAsync("<h1>All query string</h1>");
+                await context.Response.WriteAsync(@"<a href=""?message=hello&message=world&message=again&isTrue=1&morning=good"">click this link to add query string</a><br/><br/>");
+                await context.Response.WriteAsync("<ul>");
+                foreach(var v in context.Request.Query)
+                {
+                    await context.Response.WriteAsync($"<li>{v.Key} - {v.Value}</li>");
+                }
+                await context.Response.WriteAsync("</ul>");
+                await context.Response.WriteAsync("</body></html>");
+            });
+        }
+    }
+    
+   public class Program
+    {
+        public static void Main(string[] args)
+        {
+              var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
+        }
+    }
+}
