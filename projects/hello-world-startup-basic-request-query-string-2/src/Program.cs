55a862a5bcfea5b55c37c617d01f25074ea92c77
Dody Gunawinata <dodyg@silverkeytech.com>
2018-01-20 23:15:39 +0200

    query string with multiple values

---
+++ projects/hello-world-startup-basic-request-query-string-2/src/Program.cs
@@ -0,0 +1,57 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Primitives;
+
+namespace StartupBasic 
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env, ILoggerFactory logger)
+        {
+            //These are two services available at constructor
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //This is the only service available at ConfigureServices
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory logger)
+        {
+            //These are the three default services available at Configure
+            app.Run(async context =>
+            {
+                context.Response.Headers.Add("content-type", "text/html");
+
+                StringValues queryString = context.Request.Query["message"];
+
+                await context.Response.WriteAsync("<html><body>");
+                await context.Response.WriteAsync("<h1>Query String with multiple values</h1>");
+                await context.Response.WriteAsync(@"<a href=""?message=hello&message=world&message=again"">click this link to add query string</a><br/><br/>");
+                await context.Response.WriteAsync("<ul>");
+                foreach(string v in queryString)
+                {
+                    await context.Response.WriteAsync($"<li>{v}</li>");
+                }
+                await context.Response.WriteAsync("</ul>");
+                await context.Response.WriteAsync("</body></html>");
+            });
+        }
+    }
+    
+   public class Program
+    {
+        public static void Main(string[] args)
+        {
+              var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
+        }
+    }
+}
