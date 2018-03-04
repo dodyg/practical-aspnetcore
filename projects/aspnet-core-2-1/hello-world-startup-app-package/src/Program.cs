c58e64ed655877d18d3d8ccbbc382f945f3b2f7a
Dody Gunawinata <dodyg@silverkeytech.com>
2018-03-04 10:53:21 +0200

    Use new code-based idioms for ASP.NET Core 2.1

--- projects/aspnet-core-2-1/hello-world-startup-app-package/src/Program.cs
+++ projects/aspnet-core-2-1/hello-world-startup-app-package/src/Program.cs
@@ -6,7 +6,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.AspNetCore;
 using Microsoft.Extensions.Configuration;
 
-namespace StartupBasic 
+namespace StartupBasic
 {
     public class Startup
     {
@@ -29,18 +29,17 @@ namespace StartupBasic
             });
         }
     }
-    
-   public class Program
+
+    public class Program
     {
         public static void Main(string[] args)
         {
-            BuildWebHost(args).Run();
+            CreateWebHostBuilder(args).Build().Run();
         }
 
-        public static IWebHost BuildWebHost(string[] args) =>
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
-                .UseEnvironment("Development")
-                .Build();
+                .UseEnvironment("Development");
     }
 }
