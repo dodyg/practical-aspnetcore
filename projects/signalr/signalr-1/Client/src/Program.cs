bedddc1821a13d235a34004b0223d30354f9bff7
Dody Gunawinata <dodyg@silverkeytech.com>
2018-04-19 11:01:57 +0200

    Reworking the signal r samples (WIP)

--- projects/signalr/signalr-1/Client/src/Program.cs
+++ projects/signalr/signalr-1/Client/src/Program.cs
@@ -1,15 +1,18 @@
 ﻿using System;
-using System.IO;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.Logging;
 
-namespace Client
+namespace SignalChat
 {
     public class Startup
     {
@@ -27,11 +30,25 @@ namespace Client
             app.UseStaticFiles();
             app.UseStaticFiles(new StaticFileOptions()
             {
-                FileProvider = new PhysicalFileProvider(
-                    Path.Combine(Directory.GetCurrentDirectory(), @"node_modules")),
+                FileProvider = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), @"node_modules")),
                 RequestPath = new PathString("/node_modules")
             });
             app.UseMvcWithDefaultRoute();
         }
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .UseUrls("http://localhost:5002/")//We have to use this because the signalR server sample will be using the default 5000
+                .UseContentRoot(Path.Combine(Directory.GetCurrentDirectory(), "src"))
+                .UseEnvironment("Development");
     }
 }
---
+++ projects/signalr/signalr-1/Server/src/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.AspNetCore.Http;
+
+namespace SignalChat
+{
+    public class ChatHub : Hub
+    {
+        public async Task Send(string message)
+        {
+            await this.Clients.All.SendAsync("Send", message);
+        }
+    }
+
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("all",
+                    policy => policy.WithOrigins("http://localhost:5002/")
+                                    .AllowAnyHeader()
+                                    .AllowAnyMethod());
+            });
+
+            services.AddSignalR();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.UseDeveloperExceptionPage();
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseCors("all");
+
+            app.UseSignalR(routes =>
+            {
+                routes.MapHub<ChatHub>("/chatHub");
+            });
+
+            app.Run(async context =>
+            {
+                await context.Response.WriteAsync("This site host signalR server that allows remote clients to access thanks to CORS all policy.");
+            });
+        }
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .UseEnvironment("Development");
+    }
+}
