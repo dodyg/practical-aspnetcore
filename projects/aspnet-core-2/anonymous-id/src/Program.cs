a07ed93a3597c9136416d0c5f338446ec27ec703
Dody Gunawinata <dodyg@silverkeytech.com>
2018-04-19 09:30:40 +0200

    Add sample for anonymous id library to keep track of anonymous user

---
+++ projects/aspnet-core-2/anonymous-id/src/Program.cs
@@ -0,0 +1,55 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Routing;
+using ReturnTrue.AspNetCore.Identity.Anonymous;
+
+namespace StartupBasic
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env, ILoggerFactory logger, IConfiguration configuration)
+        {
+            //These are three services available at constructor
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory logger, IConfiguration configuration)
+        {
+            app.UseAnonymousId(new AnonymousIdCookieOptionsBuilder()
+                .SetCustomCookieName("Anoymous_Cookie_Tracker")
+                //.SetCustomCookieRequireSsl(true) //Uncomment this in the case of usign SSL, such as the default setup of .NET Core 2.1 
+                .SetCustomCookieTimeout(120)
+            );
+
+            app.Run(async context =>
+            {
+                IAnonymousIdFeature feature = context.Features.Get<IAnonymousIdFeature>();
+                string anonymousId = feature.AnonymousId;
+
+                await context.Response.WriteAsync($"Hello world with anonymous id {anonymousId}");
+            });
+        }
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BuildWebHost(args).Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .UseEnvironment("Development")
+                .Build();
+    }
+}
