c932de618992feba4fa3f26efb46f9836fb4d2f9
Dody Gunawinata <dodyg@silverkeytech.com>
2018-07-07 12:22:12 +0200

    Trim the project to the most minimalist you should run

--- projects/hello-world-application-environment/src/Program.cs
+++ projects/hello-world-application-environment/src/Program.cs
@@ -3,10 +3,12 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.PlatformAbstractions;
+using Microsoft.AspNetCore;
+using Microsoft.Extensions.Configuration;
 
 namespace HelloWorldWithApplicationEnvironment
 {
+    // https://github.com/aspnet/Announcements/issues/237
     public class Startup
     {
         public Startup(IHostingEnvironment env, ILoggerFactory logger)
@@ -19,32 +21,35 @@ namespace HelloWorldWithApplicationEnvironment
             //This is the only service available at ConfigureServices
         }
 
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory logger)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory logger, IConfiguration configuration)
         {
             //These are the three default services available at Configure
-            
+
             app.Run(async context =>
             {
                 context.Response.Headers.Add("content-type", "text/html");
-                var environment = PlatformServices.Default.Application;
-                await context.Response.WriteAsync($"Application Name: {environment.ApplicationName}<br/>");
-                await context.Response.WriteAsync($"Application Base Path: {environment.ApplicationBasePath}<br/>");
-                await context.Response.WriteAsync($"Target Framework: {environment.RuntimeFramework}<br/>");
-                await context.Response.WriteAsync($"Version: {environment.ApplicationVersion}<br/>");
+                await context.Response.WriteAsync($"Application Name: {System.Reflection.Assembly.GetEntryAssembly().GetName().Name}<br/>");
+                await context.Response.WriteAsync($"Application Base Path: {System.AppContext.BaseDirectory}<br/>");
+
+                System.Reflection.Assembly entryAssembly = System.Reflection.Assembly.GetEntryAssembly();
+                var targetFramework = entryAssembly.GetCustomAttributes(typeof(System.Runtime.Versioning.TargetFrameworkAttribute), true)[0] as System.Runtime.Versioning.TargetFrameworkAttribute;
+                await context.Response.WriteAsync($"Target Framework: {targetFramework.FrameworkName}<br/>");
+
+                await context.Response.WriteAsync($"Version: {System.Reflection.Assembly.GetEntryAssembly().GetName().Version}<br/>");
             });
         }
     }
-    
-   public class Program
+
+    public class Program
     {
         public static void Main(string[] args)
         {
-              var host = new WebHostBuilder()
-                .UseKestrel()
-                .UseStartup<Startup>()
-                .Build();
-
-            host.Run();
+            CreateWebHostBuilder(args).Build().Run();
         }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .UseEnvironment("Development");
     }
 }
