d6a3d3372d6dbb7c1f89a5489ffeccdae7d9cce7
Dody Gunawinata <dodyg@silverkeytech.com>
2018-07-24 16:22:40 +0200

    Add compile task

--- projects/routing-5/src/Program.cs
+++ projects/routing-5/src/Program.cs
@@ -6,7 +6,7 @@ using Microsoft.Extensions.DependencyInjection;
 using System.Collections.Generic;
 using Microsoft.AspNetCore;
 
-namespace Routing5 
+namespace Routing5
 {
     public class Startup
     {
@@ -17,22 +17,23 @@ namespace Routing5
 
         public void Configure(IApplicationBuilder app, IInlineConstraintResolver resolver)
         {
-         var defaultHandler = new RouteHandler (context =>
-            {
-                var routeValues = context.GetRouteData().Values;
-                return context.Response.WriteAsync($"Route values: {string.Join(", ", routeValues)}");
-            });
+            var defaultHandler = new RouteHandler(context =>
+              {
+                  var routeValues = context.GetRouteData().Values;
+                  return context.Response.WriteAsync($"Route values: {string.Join(", ", routeValues)}");
+              });
 
             var routes = new RouteBuilder(app, defaultHandler);
 
-            routes.MapGet("hello", (IApplicationBuilder app2) =>{
+            routes.MapGet("hello", (IApplicationBuilder app2) =>
+            {
                 var routes2 = new RouteBuilder(app2);
                 routes2.MapGet("world", (context) => context.Response.WriteAsync("Hello World"));
-                app2.UseRouter(routes2.Build());
+                return app2.UseRouter(routes2.Build());
             });
 
             routes.MapRoute(
-                name: "Default", 
+                name: "Default",
                 template: "{*path}"
             );
 
@@ -40,7 +41,7 @@ namespace Routing5
             app.UseRouter(routing);
         }
     }
-    
+
     public class Program
     {
         public static void Main(string[] args)
