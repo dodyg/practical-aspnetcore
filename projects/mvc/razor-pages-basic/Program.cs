e0499e78e941f4715f561eb0b7f1214feb62aeb7
Dody Gunawinata <dodyg@silverkeytech.com>
2018-08-16 18:15:46 +0200

    upgrade to 2.1

---
+++ projects/mvc/razor-pages-basic/Program.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+
+namespace RazorPagesBasic
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .UseEnvironment("Development");
+    }
+}
