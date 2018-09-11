68846628567d3bd538312d25849bc333f1cac845
Dody Gunawinata <dodyg@silverkeytech.com>
2018-09-11 11:58:17 +0200

    This sample shows alternative syntax from routing-1

---
+++ projects/mvc/routing-2/src/Program.cs
@@ -0,0 +1,69 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MvcRouting
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env, ILoggerFactory logger, IConfiguration configuration)
+        {
+            //These are three services available at constructor
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvcCore().
+                SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory logger, IConfiguration configuration)
+        {
+            app.UseMvc(routes => {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=HomePage}/{action=Index}");
+            });
+        }
+    }
+
+    public class HomePageController : Controller
+    {
+        public ActionResult Index()
+        {
+            return new ContentResult
+            {
+                Content = @"
+                <html><body>
+                <b>Hello World</b>
+                <br/><br/>
+                The following links call the same controller and action.
+                <ul>
+                    <li><a href=""/"">/</a></li>
+                    <li><a href=""/homepage"">/homepage</a></li>
+                    <li><a href=""/homepage/index"">/homepage/index</a></li>
+                </ul>
+                </body></html>",
+                ContentType = "text/html"
+            };
+        }
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .UseEnvironment("Development");
+    }
+}
