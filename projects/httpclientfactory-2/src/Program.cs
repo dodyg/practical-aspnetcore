a5a953dd6506305269039895dee4173d9f326e4c
Dody Gunawinata <dodyg@silverkeytech.com>
2018-09-15 02:37:06 +0200

    WIP

---
+++ projects/httpclientfactory-2/src/Program.cs
@@ -0,0 +1,52 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Routing;
+using System.Net.Http;
+
+namespace StartupBasic 
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env, ILoggerFactory logger, IConfiguration configuration)
+        {
+            //These are three services available at constructor
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddHttpClient();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory logger, IConfiguration configuration)
+        {
+            app.Run(async context =>
+            {
+                var httpClient = context.RequestServices.GetService<IHttpClientFactory>();
+                var client = httpClient.CreateClient();
+                var result = await client.GetStringAsync("http://scripting.com/rss.xml");
+            
+                context.Response.Headers.Add("Content-Type", "application/rss+xml");
+                await context.Response.WriteAsync(result);
+            });
+        }
+    }
+    
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .UseEnvironment("Development");
+    }
+}
