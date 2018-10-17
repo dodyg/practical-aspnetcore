648432ee327b1a0d1f43b9ce31b90c7c83256199
Dody Gunawinata <dodyg@silverkeytech.com>
2018-10-17 18:22:14 +0200

    Remove extra file

--- projects/mvc/mvc-accept-xml/src/Program.cs
+++ projects/mvc/mvc-accept-xml/src/Program.cs
@@ -4,7 +4,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.AspNetCore.Routing;
 
-namespace MvcAcceptXml 
+namespace MvcAcceptXml
 {
     public class Startup
     {
@@ -18,7 +18,34 @@ namespace MvcAcceptXml
             app.UseMvc();
         }
     }
-    
+
+    public class HelloWorld
+    {
+        public string Hello { get; set; }
+        public string World { get; set; }
+    }
+
+    public class Result
+    {
+        public string Output { get; set; }
+    }
+
+    [Route("")]
+    public class HomeController : Controller
+    {
+        [HttpGet("")]
+        public IActionResult Index()
+        {
+            return this.Content("Make a post with { Hello : \"hello\", World : \"World\" } payload to localhost:5000");
+        }
+
+        [HttpPost("")]
+        public IActionResult Index([FromBody] HelloWorld payload)
+        {
+            return Json(new Result { Output = payload.Hello + " = " + payload.World });
+        }
+    }
+
     public class Program
     {
         public static void Main(string[] args)
