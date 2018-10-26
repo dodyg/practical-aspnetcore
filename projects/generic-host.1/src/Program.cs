f74e0414141e7a473f679dd10d58c3424652dd0e
Dody Gunawinata <dodyg@silverkeytech.com>
2018-10-26 12:28:10 +0200

    Add example for RunConsoleAsync()

---
+++ projects/generic-host.1/src/Program.cs
@@ -0,0 +1,38 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using System.Threading.Tasks;
+using System.Threading;
+using Microsoft.Extensions.Hosting;
+using System;
+
+namespace GenericHostBasic
+{
+    public class HelloWorldService : IHostedService
+    {
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            Console.WriteLine("Hello world");
+            return Task.CompletedTask;
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            Console.WriteLine("Goodbye");
+            return Task.CompletedTask;
+        }
+    }
+
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var host = new HostBuilder()
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddHostedService<HelloWorldService>();
+                });
+
+            await host.RunConsoleAsync();
+        }
+    }
+}
