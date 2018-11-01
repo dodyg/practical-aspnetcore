27335b51b246edfa4c11a0d532d98d8e95586ec0
Dody Gunawinata <dodyg@silverkeytech.com>
2018-11-01 09:51:29 +0200

    Another example on how to run a regularly updating service.

---
+++ projects/generic-host-5/src/Program.cs
@@ -0,0 +1,71 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Threading;
+
+namespace GenericHostBasic
+{
+    public class HelloWorldService : IHostedService, IDisposable
+    {
+        CancellationTokenSource _stoppingCts = new CancellationTokenSource();
+
+        Task _executingTask;
+
+        public HelloWorldService()
+        {
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            _executingTask = ExecuteAsync(_stoppingCts.Token);
+
+            return _executingTask.IsCompleted ? _executingTask : Task.CompletedTask;
+        }
+
+        async Task ExecuteAsync(CancellationToken cancellationToken)
+        {
+            var count = 0;
+            do
+            {
+                Console.WriteLine(count);
+                count++;
+                await Task.Delay(1000, cancellationToken);
+            } while (!cancellationToken.IsCancellationRequested);
+        }
+
+        public async Task StopAsync(CancellationToken cancellationToken)
+        {
+            if (_executingTask == null)
+                return;
+
+            try
+            {
+                _stoppingCts.Cancel();
+            }
+            finally
+            {
+                await Task.WhenAny(_executingTask, Task.Delay(Timeout.Infinite, cancellationToken));
+            }
+        }
+
+        public void Dispose() => _stoppingCts.Cancel();
+    }
+
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var host = new HostBuilder()
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddHostedService<HelloWorldService>();
+                })
+                .UseConsoleLifetime()
+                .Build();
+
+            await host.RunAsync();
+        }
+    }
+}
