65eeb8c70d30f741ac58a155fa59f640909eb321
Dody Gunawinata <dodyg@silverkeytech.com>
2018-12-03 22:07:12 +0200

    WIP model binding sample

---
+++ projects/mvc/model-binding/src/Program.cs
@@ -0,0 +1,60 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Mvc;
+
+namespace StartupBasic
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env, ILoggerFactory logger, IConfiguration configuration)
+        {
+            //These are three services available at constructor
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvcCore().
+                SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory logger, IConfiguration configuration)
+        {
+            app.UseMvc(routes =>
+                routes.MapRoute(
+                    name: "default_route",
+                    template: "{controller}/{action}/{id?}",
+                    defaults: new { controller = "Home", action = "Index" })
+                );
+        }
+    }
+
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return new ContentResult
+            {
+                Content = "<html><body><b>Hello World</b></body></html>",
+                ContentType = "text/html"
+            };
+        }
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .UseEnvironment("Development");
+    }
+}
