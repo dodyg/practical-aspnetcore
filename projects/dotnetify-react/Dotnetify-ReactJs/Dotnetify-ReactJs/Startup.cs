cf711270a597a7a96d50ac9630bc0dbe5d36d411
Sherif Refaat <sherif.refaat.212@gmail.com>
2018-12-12 11:59:03 +0200

    Add dotnetify-reactjs sample.

---
+++ projects/dotnetify-react/Dotnetify-ReactJs/Dotnetify-ReactJs/Startup.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using DotNetify;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace Dotnetify_ReactJs
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddMemoryCache();
+            services.AddSignalR();
+            services.AddDotNetify();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseWebSockets();
+            app.UseSignalR(routes => routes.MapDotNetifyHub());
+            app.UseDotNetify();
+
+            app.UseMvc();
+
+            app.UseStaticFiles();
+
+            app.Map("", (builder) =>
+            {
+                builder.Run(async (httpContext) =>
+                {
+                    using (var reader = new StreamReader(File.OpenRead($"wwwroot/index.html")))
+                    {
+                        var html = reader.ReadToEnd();
+                        await httpContext.Response.WriteAsync(html);
+                    }
+                });
+            });
+        }
+    }
+}
