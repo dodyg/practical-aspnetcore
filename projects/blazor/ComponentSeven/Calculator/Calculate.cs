fcbe1cea62853894e7a58346e0b1a5c7d77e5bcd
Dody Gunawinata <dodyg@silverkeytech.com>
2018-12-15 12:47:05 +0200

    A simple sample that you can just use normal C# classes in your components

---
+++ projects/blazor/ComponentSeven/Calculator/Calculate.cs
@@ -0,0 +1,8 @@
+
+namespace Calculator
+{
+    public class Calculate
+    {
+        public int Double(int number) => number * 2;
+    }
+}
---
+++ projects/blazor/ComponentSeven/Program.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Blazor.Builder;
+using Microsoft.AspNetCore.Blazor.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+namespace ComponentSeven
+{
+    public class Startup
+    {
+        public void Configure(IBlazorApplicationBuilder app)
+        {
+            app.AddComponent<App>("app");
+        }
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IWebAssemblyHostBuilder CreateHostBuilder(string[] args) =>
+            BlazorWebAssemblyHost.CreateDefaultBuilder()
+                .UseBlazorStartup<Startup>();
+    }
+}
