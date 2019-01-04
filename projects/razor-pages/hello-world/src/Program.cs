ed81237125eed3cc4b49e5d2a37c5e082d4fd0d1
Dody Gunawinata <dodyg@silverkeytech.com>
2019-01-04 18:26:28 +0200

    Add entry to razor page section for hello world.

---
+++ projects/razor-pages/hello-world/src/Program.cs
@@ -0,0 +1,43 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Mvc;
+
+namespace StartupBasic
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env, ILoggerFactory logger, IConfiguration configuration)
+        {
+            //These are three services available at constructor
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().
+                SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory logger, IConfiguration configuration)
+        {
+            app.UseMvc();
+        }
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .UseEnvironment("Development");
+    }
+}
