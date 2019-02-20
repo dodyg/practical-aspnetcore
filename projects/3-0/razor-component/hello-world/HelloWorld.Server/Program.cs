32ae28ceb665200cc5fab37ed3b87deb048e2723
Dody Gunawinata <dodyg@silverkeytech.com>
2019-02-20 08:21:08 +0200

    Add hello world

---
+++ projects/3-0/razor-component/hello-world/HelloWorld.Server/Program.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace HelloWorld.Server
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                    webBuilder.UseEnvironment(Microsoft.AspNetCore.Hosting.EnvironmentName.Development);
+                });
+    }
+}
