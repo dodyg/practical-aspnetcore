f5f56a8ed0d746d6020569f1dabf35a1eb04919b
Dody Gunawinata <dodyg@silverkeytech.com>
2019-02-20 08:45:59 +0200

    Reduce the sample

--- projects/3-0/razor-component/hello-world/HelloWorld.Server/Program.cs
+++ projects/3-0/razor-component/hello-world/HelloWorld.Server/Program.cs
@@ -1,16 +1,35 @@
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
+using aspnetHosting = Microsoft.AspNetCore.Hosting;
 
 namespace HelloWorld.Server
 {
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddRazorComponents<App.Startup>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, aspnetHosting.IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseStaticFiles();
+            app.UseRazorComponents<App.Startup>();
+        }
+    }
+
     public class Program
     {
         public static void Main(string[] args)
@@ -23,7 +42,7 @@ namespace HelloWorld.Server
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
-                    webBuilder.UseEnvironment(Microsoft.AspNetCore.Hosting.EnvironmentName.Development);
+                    webBuilder.UseEnvironment(aspnetHosting.EnvironmentName.Development);
                 });
     }
 }
