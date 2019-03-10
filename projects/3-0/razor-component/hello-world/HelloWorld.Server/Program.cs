73b5baf55a4dc691f29c0ebdb0a3b71396c48375
Dody Gunawinata <dodyg@silverkeytech.com>
2019-03-10 08:19:03 +0200

    Remove the extra project .App

--- projects/3-0/razor-component/hello-world/HelloWorld.Server/Program.cs
+++ projects/3-0/razor-component/hello-world/HelloWorld.Server/Program.cs
@@ -4,29 +4,38 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.DependencyInjection;
-using aspnetHosting = Microsoft.AspNetCore.Hosting;
+using HelloWorld.Server.Components;
 
 namespace HelloWorld.Server
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddRazorComponents<App.Startup>();
+            services.AddRazorComponents();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, aspnetHosting.IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
+            else
+            {
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
 
+            app.UseHttpsRedirection();
             app.UseStaticFiles();
-            app.UseRazorComponents<App.Startup>();
+
+            app.UseRouting(routes =>
+            {
+                routes.MapRazorPages();
+                routes.MapComponentHub<App>("app");
+            });
         }
     }
 
@@ -42,7 +51,7 @@ namespace HelloWorld.Server
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
-                    webBuilder.UseEnvironment(aspnetHosting.EnvironmentName.Development);
+                    webBuilder.UseEnvironment(Environments.Development);
                 });
     }
 }
