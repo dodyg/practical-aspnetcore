ca0cd9f59d20faa9e764ab33c2743cdef47e387e
Dody Gunawinata <dodyg@silverkeytech.com>
2019-03-27 11:51:03 +0200

    Add sample to configure MVC Routes

---
+++ projects/3-0/new-routing-2/README.md
@@ -0,0 +1,3 @@
+# New Routing - enable just MVC 
+
+This example shows how to enable *just* MVC routes in your app using the method `app.UseRouting`. Razor Pages won't work at all in this sample.
---
+++ projects/3-0/new-routing-2/src/Program.cs
@@ -0,0 +1,48 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NewRouting
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseRouting(route =>
+            {
+                route.MapControllers();
+            });
+        }
+    }
+
+    [Route("")]
+    public class HomeController : Controller
+    {
+        public IActionResult Index() => Content("Hello World. Razor Pages won't work in this sample.");
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>().
+                    UseEnvironment(Environments.Development);
+                });
+    }
+}
