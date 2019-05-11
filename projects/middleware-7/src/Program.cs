16df204eda384a9d165e47be0f95faab5e86d946
Dody Gunawinata <dodyg@silverkeytech.com>
2019-05-11 12:15:13 +0200

    Clarify the sample

--- projects/middleware-7/src/Program.cs
+++ projects/middleware-7/src/Program.cs
@@ -15,24 +15,26 @@ namespace HelloWorldWithMiddleware
             //Use MapMiddleWare and MapWhenMiddlware directly
             var whenOption = new MapWhenOptions
             {
-                Branch =  
-                    context => 
-                        context.Response.WriteAsync($"Path: {context.Request.Path} - Path Base: {context.Request.PathBase}"),
+                Branch =
+                    context =>
+                        context.Response.WriteAsync($"MapWhenMiddleware| Path: {context.Request.Path} - Path Base: {context.Request.PathBase}"),
                 Predicate = context => context.Request.Path.Value.Contains("hello")
             };
+
             app.UseMiddleware<MapWhenMiddleware>(whenOption);
-        
+
             var mapOption = new MapOptions
             {
-                 Branch =  
-                    context => 
-                        context.Response.WriteAsync($"Path: {context.Request.Path} - Path Base: {context.Request.PathBase}"),
-                 PathMatch = "/greetings"
+                Branch =
+                    context =>
+                        context.Response.WriteAsync($"MapMiddleware| Path: {context.Request.Path} - Path Base: {context.Request.PathBase}"),
+                PathMatch = "/greetings"
             };
+
             app.UseMiddleware<MapMiddleware>(mapOption);
 
             app.Run(context =>
-            { 
+            {
                 context.Response.Headers.Add("content-type", "text/html");
                 return context.Response.WriteAsync(@"
                    <a href=""/hello"">/hello</a> <a href=""/greetings"">/greetings</a>
