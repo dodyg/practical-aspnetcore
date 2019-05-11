077c865e7746ab92e7b54ed9aa6e93232179f4d9
Dody Gunawinata <dodyg@silverkeytech.com>
2019-05-11 13:51:51 +0200

    Fix the middlere sample only to write at one place

--- projects/hello-world-with-middleware/src/Program.cs
+++ projects/hello-world-with-middleware/src/Program.cs
@@ -2,8 +2,9 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore;
+using System.Threading.Tasks;
 
-namespace HelloWorldWithMiddleware 
+namespace HelloWorldWithMiddleware
 {
     public class Startup
     {
@@ -12,25 +13,28 @@ namespace HelloWorldWithMiddleware
             //The order of these things are important. 
             app.Use(async (context, next) =>
             {
-                await context.Response.WriteAsync("[1] ----- \n");//1
+                context.Items["Content"] += "[1] ----- \n";//1
                 await next.Invoke();
-                await context.Response.WriteAsync("[5] =====\n");
+                context.Items["Content"] += "[5] =====\n";//5
+
+                await context.Response.WriteAsync(context.Items["Content"] as string);
             });
 
             app.Use(async (context, next) =>
             {
-                await context.Response.WriteAsync("[2] Hello world \n");//2
+                context.Items["Content"] += "[2] Hello world \n";//2
                 await next.Invoke();
-                await context.Response.WriteAsync("[4]  \n");//4
+                context.Items["Content"] += "[4]  \n";//4
             });
 
-            app.Run(async context =>
+            app.Run(context =>
             {
-                await context.Response.WriteAsync("[3] ----- \n");//3
+                context.Items["Content"] += "[3] ----- \n";//3
+                return Task.CompletedTask;
             });
         }
     }
-    
+
     public class Program
     {
         public static void Main(string[] args)
