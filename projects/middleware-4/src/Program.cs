6609776ab4b589320fcdaec9fae4dfe2878b843c
Dody Gunawinata <dodyg@silverkeytech.com>
2019-05-11 13:37:47 +0200

    Improve sample

--- projects/middleware-4/src/Program.cs
+++ projects/middleware-4/src/Program.cs
@@ -11,11 +11,11 @@ namespace HelloWorldWithMiddleware
     {
         public void Configure(IApplicationBuilder app)
         {
-            app.Map("/hello", (IApplicationBuilder pp) => pp.Run(context => context.Response.WriteAsync("Hello")));
-            app.Map("/world", (IApplicationBuilder pp) => pp.Run(context => context.Response.WriteAsync("Hello")));
-            
+            app.Map("/hello", (IApplicationBuilder pp) => pp.Run(context => context.Response.WriteAsync("/hello path")));
+            app.Map("/world", (IApplicationBuilder pp) => pp.Run(context => context.Response.WriteAsync("/world path")));
+
             app.Run(context =>
-            { 
+            {
                 context.Response.Headers.Add("content-type", "text/html");
                 return context.Response.WriteAsync(@"
                    <a href=""/hello"">hello</a> <a href=""/world"">world</a>
