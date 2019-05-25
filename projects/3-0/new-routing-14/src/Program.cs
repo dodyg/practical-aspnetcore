212bc950af31744ce19611f3dba6d0a31d06a937
Dody Gunawinata <dodyg@silverkeytech.com>
2019-05-25 16:34:33 +0200

    Add sample for IEndpointFeature

---
+++ projects/3-0/new-routing-14/src/Program.cs
@@ -0,0 +1,57 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.AspNetCore.Connections;
+using System.Buffers;
+using System.Threading.Tasks;
+using Microsoft.Extensions.FileProviders;
+using System.IO;
+using Microsoft.AspNetCore.Http.Endpoints;
+using System;
+using Microsoft.AspNetCore.Http.Features;
+
+namespace NewRouting
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostEnvironment environment)
+        {
+            app.UseRouting();
+
+            app.UseEndpoints(routes =>
+            {
+                routes.MapGet("/", async context =>
+                {
+                    var feature = context.Features.Get<IEndpointFeature>();
+                    await context.Response.WriteAsync($"Endpoint Name {feature.Endpoint.DisplayName}");
+                });
+            });
+        }
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>().
+                    UseEnvironment(Environments.Development);
+                });
+    }
+}
