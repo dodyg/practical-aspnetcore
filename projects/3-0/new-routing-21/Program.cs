1e1616553730382d187723377da1ce053a2fbb9f
Dody Gunawinata <dodyg@silverkeytech.com>
2019-06-04 09:36:25 +0200

    Add sample about the new MapHealthChecks

---
+++ projects/3-0/new-routing-21/Program.cs
@@ -0,0 +1,64 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.AspNetCore.Connections;
+using System.Buffers;
+using System.Threading.Tasks;
+using Microsoft.Extensions.FileProviders;
+using System.IO;
+using Microsoft.AspNetCore.Http.Endpoints;
+using System;
+using Microsoft.AspNetCore.Http.Features;
+
+namespace NewRouting
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddHealthChecks();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostEnvironment environment)
+        {
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapHealthChecks("/WhatsUp");
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync(@"
+                    <html>
+                        <body>
+                            <h1>Health Check</h1>
+                            The health check service checks on this url <a href=""/WhatsUp"">/WhatsUp</a>. 
+                        </body>
+                    </html> 
+                    ");
+                });
+            });
+        }
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>().
+                    UseEnvironment(Environments.Development);
+                });
+    }
+}
---
+++ projects/3-0/new-routing-21/README.md
@@ -0,0 +1,3 @@
+# A new way to map health check
+        
+Use `IEndpointRouteBuilder.MapHealthChecks` to configure health check instead of `IApplicationBuilder.UseHealthChecks`.
