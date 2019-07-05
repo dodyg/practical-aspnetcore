d4119ad696455af15b108ae348e83e7f2ae8389f
Dody Gunawinata <dodyg@silverkeytech.com>
2019-07-05 16:13:46 +0200

    WIP - form validation sample

---
+++ projects/blazor/DataBindingTwo/Code/Profile.cs
@@ -0,0 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+using System;
+
+namespace DataBindingTwo.Code
+{
+    public class Profile
+    {
+        [Required]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Required]
+        public string Gender { get; set; }
+    }
+}
---
+++ projects/blazor/DataBindingTwo/Program.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Components.Builder;
+using Microsoft.AspNetCore.Blazor.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+namespace DataBindingTwo
+{
+    public class Startup
+    {
+        public void Configure(IComponentsApplicationBuilder app)
+        {
+            app.AddComponent<App>("app");
+        }
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IWebAssemblyHostBuilder CreateHostBuilder(string[] args) =>
+            BlazorWebAssemblyHost.CreateDefaultBuilder()
+                .UseBlazorStartup<Startup>();
+    }
+}
