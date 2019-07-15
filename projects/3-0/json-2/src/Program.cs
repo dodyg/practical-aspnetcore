3a4d5b25e77a2b4311fd14195678e43c974f68a2
Dody Gunawinata <dodyg@silverkeytech.com>
2019-07-15 11:28:24 +0200

    JsonSerializerOptions sample

---
+++ projects/3-0/json-2/src/Program.cs
@@ -0,0 +1,93 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using Microsoft.Net.Http.Headers;
+using System;
+using System.Collections.Generic;
+
+namespace JsonSample
+{
+    public class Person
+    {
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public bool IsMarried { get; set; }
+
+        public DateTimeOffset CurrentTime { get; set; }
+
+        public bool? IsWorking { get; set; }
+
+        public Dictionary<string, bool> Characters { get; set; }
+    }
+
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseRouting();
+            app.UseEndpoints(route =>
+            {
+                route.MapGet("/", async context =>
+                {
+                    var payload = new Person
+                    // We won't assign any value to the property IsWorking. 
+                    {
+                        Name = "Annie",
+                        Age = 33,
+                        IsMarried = false,
+                        CurrentTime = DateTimeOffset.UtcNow,
+                        Characters = new Dictionary<string, bool>
+                        {
+                            {"Funny" , true},
+                            {"Feisty" , true},
+                            {"Brilliant" , true},
+                            {"FOMA", false}
+                        }
+                    };
+
+
+                    var options = new JsonSerializerOptions
+                    {
+                        WriteIndented = true,
+                        IgnoreNullValues = true,
+                        PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+                        DictionaryKeyPolicy = JsonNamingPolicy.CamelCase
+                    };
+
+                    var serialized = JsonSerializer.ToString(payload, options);
+                    context.Response.Headers.Add(HeaderNames.ContentType, "application/json");
+                    await context.Response.WriteAsync(serialized);
+                });
+            });
+        }
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>().
+                    UseEnvironment(Environments.Development);
+                });
+    }
+}
