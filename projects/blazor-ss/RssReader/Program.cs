0f664325168f94d03522c6abdbfee56c0e7d8c3e
Dody Gunawinata <dodyg@silverkeytech.com>
2019-07-24 21:10:12 +0200

    Update to preview 7

--- projects/blazor-ss/RssReader/Program.cs
+++ projects/blazor-ss/RssReader/Program.cs
@@ -27,10 +27,8 @@ namespace RssReader
             else
             {
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
             app.UseStaticFiles();
 
             app.UseRouting();
