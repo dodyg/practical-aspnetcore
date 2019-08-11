4ba50972e2c546043a588b6df114d843d29e98d1
Dody Gunawinata <dodyg@silverkeytech.com>
2019-08-11 15:19:18 +0200

    Use snakecase implementation from newtonsoft.json

--- projects/3-0/json-7/README.md
+++ projects/3-0/json-7/README.md
@@ -2,4 +2,4 @@
 
 Create a custom naming policy that generate JSON property names in snake_case.
 
-Code for converting CamelCase property to snake_case is obtained from this [Gist](https://gist.github.com/vkobel/d7302c0076c64c95ef4b).
+Code for converting CamelCase property to snake_case is obtained from [Newtonsoft.Json](https://github.com/JamesNK/Newtonsoft.Json/blob/cdf10151d507d497a3f9a71d36d544b199f73435/Src/Newtonsoft.Json/Utilities/StringUtils.cs).
--- projects/3-0/json-7/src/Program.cs
+++ projects/3-0/json-7/src/Program.cs
@@ -7,14 +7,97 @@ using Microsoft.Net.Http.Headers;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 
 namespace JsonSample
 {
+    // Implementation from https://github.com/JamesNK/Newtonsoft.Json/blob/cdf10151d507d497a3f9a71d36d544b199f73435/Src/Newtonsoft.Json/Utilities/StringUtils.cs
+    internal static class StringUtils
+    {
+        internal enum SnakeCaseState
+        {
+            Start,
+            Lower,
+            Upper,
+            NewWord
+        }
+
+        public static string ToSnakeCase(string s)
+        {
+            if (string.IsNullOrEmpty(s))
+            {
+                return s;
+            }
+
+            StringBuilder sb = new StringBuilder();
+            SnakeCaseState state = SnakeCaseState.Start;
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] == ' ')
+                {
+                    if (state != SnakeCaseState.Start)
+                    {
+                        state = SnakeCaseState.NewWord;
+                    }
+                }
+                else if (char.IsUpper(s[i]))
+                {
+                    switch (state)
+                    {
+                        case SnakeCaseState.Upper:
+                            bool hasNext = (i + 1 < s.Length);
+                            if (i > 0 && hasNext)
+                            {
+                                char nextChar = s[i + 1];
+                                if (!char.IsUpper(nextChar) && nextChar != '_')
+                                {
+                                    sb.Append('_');
+                                }
+                            }
+                            break;
+                        case SnakeCaseState.Lower:
+                        case SnakeCaseState.NewWord:
+                            sb.Append('_');
+                            break;
+                    }
+
+                    char c;
+#if HAVE_CHAR_TO_LOWER_WITH_CULTURE
+                    c = char.ToLower(s[i], CultureInfo.InvariantCulture);
+#else
+                    c = char.ToLowerInvariant(s[i]);
+#endif
+                    sb.Append(c);
+
+                    state = SnakeCaseState.Upper;
+                }
+                else if (s[i] == '_')
+                {
+                    sb.Append('_');
+                    state = SnakeCaseState.Start;
+                }
+                else
+                {
+                    if (state == SnakeCaseState.NewWord)
+                    {
+                        sb.Append('_');
+                    }
+
+                    sb.Append(s[i]);
+                    state = SnakeCaseState.Lower;
+                }
+            }
+
+            return sb.ToString();
+        }
+    }
+
     public class SnakeCaseNamingPolicy : JsonNamingPolicy
     {
         public override string ConvertName(string name)
         {
-            return string.Concat(name.Select((x, i) => i > 0 && char.IsUpper(x) ? "_" + x.ToString() : x.ToString())).ToLower();
+            return StringUtils.ToSnakeCase(name);
         }
     }
 
