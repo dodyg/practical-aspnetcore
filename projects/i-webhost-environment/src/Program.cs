a569a0c5f84afca83e3ac177d49a65153115bad9
Dody Gunawinata <dodyg@silverkeytech.com>
2019-11-30 15:10:43 +0200

    Switchi from IHostEnvironment to IWebHostEnvironment

--- projects/i-webhost-environment/src/Program.cs
+++ projects/i-webhost-environment/src/Program.cs
@@ -1,25 +1,13 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.AspNetCore;
+using Microsoft.Extensions.Hosting;
 
-namespace StartupBasic 
+namespace PracticalAspNetCore
 {
     public class Startup
     {
-        public Startup(IHostingEnvironment env, ILoggerFactory logger)
-        {
-            //These are two services available at constructor
-        }
-
-        public void ConfigureServices(IServiceCollection services)
-        {
-            //This is the only service available at ConfigureServices
-        }
-
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory logger)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             //These are the three default services available at Configure
 
@@ -30,8 +18,8 @@ namespace StartupBasic
             var webRootPath = $"<tr><td>WebRootPath</td><td>{env.WebRootPath}</td></tr>";
             var webRootFileProvider = $"<tr><td>WebRootFileProvider</td><td>{env.WebRootFileProvider}</td></tr>";
 
-            var content = $"<html><body><table><tbody><h1>Complete list of IHostingEnvironment properties</h1>{applicationName}{contentRootPath}{contentRootFileProvider}{environmentName}{webRootPath}{webRootFileProvider}</tbody></table></body></html>";
-            
+            var content = $"<html><body><table><tbody><h1>Complete list of IWebHostEnvironment properties</h1>{applicationName}{contentRootPath}{contentRootFileProvider}{environmentName}{webRootPath}{webRootFileProvider}</tbody></table></body></html>";
+
             app.Run(context =>
             {
                 context.Response.ContentType = "text/html";
@@ -39,7 +27,7 @@ namespace StartupBasic
             });
         }
     }
-    
+
     public class Program
     {
         public static void Main(string[] args)
