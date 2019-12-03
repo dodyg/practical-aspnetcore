7c6ce758b94eebe83519366d939866c95c07f4b4
Dody Gunawinata <dodyg@silverkeytech.com>
2019-12-03 15:15:42 +0200

    Upgraded

--- projects/diagnostics/diagnostics-5/src/Program.cs
+++ projects/diagnostics/diagnostics-5/src/Program.cs
@@ -1,45 +1,42 @@
-using System;
-using System.Linq;
-using System.Net;
-using System.Text;
-using System.Text.Encodings.Web;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Http.Features;
-using Microsoft.AspNetCore;
+using Microsoft.Extensions.Hosting;
 
 namespace PracticalAspNetCore
 {
+    public class Startup
+    {
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseStatusCodePagesWithRedirects("/error?status={0}");
+
+            app.Map("/error", errorApp =>
+            {
+                errorApp.Run(async context =>
+                {
+                    await context.Response.WriteAsync($"This is a redirected error message status {context.Request.Query["status"]}");
+                });
+            });
+
+            app.Run(context =>
+            {
+                context.Response.StatusCode = 500;//change this as necessary
+                return Task.CompletedTask;
+            });
+        }
+    }
+
     public class Program
     {
-        public static void Main(string[] args)
-        {
+        public static void Main(string[] args) =>
             CreateHostBuilder(args).Build().Run();
-        }
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .Configure(app =>
-                {
-                    app.UseStatusCodePagesWithRedirects("/error?status={0}");
-
-                    app.Map("/error", errorApp =>
-                    {
-                        errorApp.Run(async context =>
-                        {
-                            await context.Response.WriteAsync($"This is a redirected error message status {context.Request.Query["status"]}");
-                        });
-                    });
-
-                    app.Run(context =>
-                    {
-                        context.Response.StatusCode = 500;//change this as necessary
-                        return Task.CompletedTask;
-                    });
-                })
-                .UseEnvironment("Development");
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                    webBuilder.UseStartup<Startup>()
+                );
     }
 }
