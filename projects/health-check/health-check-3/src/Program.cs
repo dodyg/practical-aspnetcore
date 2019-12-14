a00902433809ac42eeaac5a637ca7a55e8038cb6
Dody Gunawinata <dodyg@silverkeytech.com>
2019-12-14 14:32:56 +0200

    upgraded

--- projects/health-check/health-check-3/src/Program.cs
+++ projects/health-check/health-check-3/src/Program.cs
@@ -1,40 +1,33 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.AspNetCore;
-using Microsoft.Extensions.Configuration;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Routing;
-using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.Extensions.Diagnostics.HealthChecks;
 using System;
 using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
 
 namespace PracticalAspNetCore
 {
     public class Startup
     {
-        public Startup(IHostingEnvironment env, ILoggerFactory logger)
-        {
-        }
-
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddHealthChecks()
                 .AddCheck<AlwaysBadHealthCheck>("Bad");
 
-            services
-                .AddMvc()
-                .SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddMvc();
         }
 
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory logger, IConfiguration configuration)
+        public void Configure(IApplicationBuilder app)
         {
-            app.UseHealthChecks("/IsUp");
-            app.UseMvcWithDefaultRoute();
+            app.UseRouting();
+            app.UseEndpoints(route =>
+            {
+                route.MapHealthChecks("/IsUp");
+                route.MapDefaultControllerRoute();
+            });
         }
     }
 
