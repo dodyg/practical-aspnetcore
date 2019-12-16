d2ba010d30ae972383339a7712fcc38ca2b1631c
Dody Gunawinata <dodyg@silverkeytech.com>
2019-12-16 15:45:40 +0200

    Upgraded

--- projects/httpclientfactory/httpclientfactory-4/src/Program.cs
+++ projects/httpclientfactory/httpclientfactory-4/src/Program.cs
@@ -1,23 +1,15 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.AspNetCore;
-using Microsoft.Extensions.Configuration;
-using Microsoft.AspNetCore.Routing;
 using System.Net.Http;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
 
-namespace StartupBasic
+namespace PracticalAspNetCore
 {
     public class Startup
     {
-        public Startup(IHostingEnvironment env, ILoggerFactory logger, IConfiguration configuration)
-        {
-            //These are three services available at constructor
-        }
-
         public void ConfigureServices(IServiceCollection services)
         {
             // The order is important. If you switch it, it won't work.
@@ -25,7 +17,7 @@ namespace StartupBasic
             services.AddHttpClient<IRss, RssReader>();
         }
 
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory logger, IConfiguration configuration)
+        public void Configure(IApplicationBuilder app)
         {
             app.Run(async context =>
             {
