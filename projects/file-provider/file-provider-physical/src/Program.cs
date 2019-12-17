e57582e8b317d51115bebb5dba3265e1f44d593f
Dody Gunawinata <dodyg@silverkeytech.com>
2019-12-17 16:49:48 +0200

    Upgraded

--- projects/file-provider/file-provider-physical/src/Program.cs
+++ projects/file-provider/file-provider-physical/src/Program.cs
@@ -1,41 +1,28 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.DependencyInjection;
-using System.IO;
 using Microsoft.Extensions.FileProviders;
-using Microsoft.AspNetCore;
+using Microsoft.Extensions.Hosting;
 
-namespace FileProviderPhysical 
+namespace PracticalAspNetCore
 {
     public class Startup
     {
-        public Startup(IHostingEnvironment env, ILoggerFactory logger)
-        {
-            //These are two services available at constructor
-        }
-
-        public void ConfigureServices(IServiceCollection services)
-        {
-            //This is the only service available at ConfigureServices
-        }
-
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory logger)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             //These are the three default services available at Configure
-            
+
             app.Run(async context =>
             {
-                context.Response.Headers.Add("content-type", "text/html");
+                context.Response.Headers.Add("Content-Type", "text/html");
 
                 var contentRoot = env.ContentRootFileProvider;
                 var contentPhysical = env.ContentRootFileProvider as PhysicalFileProvider;
 
-                await context.Response.WriteAsync("<h1>Content Root</h1>");                
+                await context.Response.WriteAsync("<h1>Content Root</h1>");
                 await context.Response.WriteAsync($"{contentPhysical.Root}");
                 await context.Response.WriteAsync($"<ul>");
-                foreach(var f in contentRoot.GetDirectoryContents(""))
+                foreach (var f in contentRoot.GetDirectoryContents(""))
                 {
                     if (f.IsDirectory)
                         await context.Response.WriteAsync($"<li>{f.Name} - Directory</li>");
@@ -47,10 +34,10 @@ namespace FileProviderPhysical
                 var webRoot = env.WebRootFileProvider;
                 var webPhysical = env.WebRootFileProvider as PhysicalFileProvider;
 
-                await context.Response.WriteAsync("<h1>Web Root</h1>");                
+                await context.Response.WriteAsync("<h1>Web Root</h1>");
                 await context.Response.WriteAsync($"{webPhysical.Root}");
                 await context.Response.WriteAsync($"<ul>");
-                foreach(var f in webRoot.GetDirectoryContents(""))
+                foreach (var f in webRoot.GetDirectoryContents(""))
                 {
                     if (f.IsDirectory)
                         await context.Response.WriteAsync($"<li>{f.Name} - Directory</li>");
@@ -61,7 +48,7 @@ namespace FileProviderPhysical
             });
         }
     }
-    
+
     public class Program
     {
         public static void Main(string[] args)
