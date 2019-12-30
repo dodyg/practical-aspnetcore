f19e6b79d11f415f7a9151793ac9c3e14df3e227
Dody Gunawinata <dodyg@silverkeytech.com>
2019-12-30 14:32:37 +0200

    Upgrade

--- projects/syndications/syndication-1/src/Program.cs
+++ projects/syndications/syndication-1/src/Program.cs
@@ -1,22 +1,18 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.AspNetCore;
-using Microsoft.Extensions.Configuration;
 using Microsoft.SyndicationFeed;
 using Microsoft.SyndicationFeed.Rss;
-using System.Net.Http;
 using System.Xml;
 using System.Collections.Generic;
 using System.Text;
+using Microsoft.Extensions.Hosting;
 
 namespace PracticalAspNetCore
 {
     public class Startup
     {
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory logger, IConfiguration configuration)
+        public void Configure(IApplicationBuilder app)
         {
             //These are the four default services available at Configure
             app.Run(async context =>
