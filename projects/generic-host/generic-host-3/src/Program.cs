e24695670f8926324f8c30e015b0633691aab659
Dody Gunawinata <dodyg@silverkeytech.com>
2020-01-16 15:09:02 +0200

    upgraded

--- projects/generic-host/generic-host-3/src/Program.cs
+++ projects/generic-host/generic-host-3/src/Program.cs
@@ -4,50 +4,72 @@ using System.Threading.Tasks;
 using System.Threading;
 using Microsoft.Extensions.Hosting;
 using System;
+using Microsoft.Extensions.Logging;
 
 namespace PracticalAspNetCore
 {
     public class HelloWorldService : IHostedService
     {
+        readonly ILogger _log;
+
+        public HelloWorldService(ILogger<HelloWorldService> logger)
+        {
+            _log = logger;
+        }
+
         public Task StartAsync(CancellationToken cancellationToken)
         {
-            Console.WriteLine("Hello world 1");
+            _log.LogDebug("Hello world 1");
             return Task.CompletedTask;
         }
 
         public Task StopAsync(CancellationToken cancellationToken)
         {
-            Console.WriteLine("Goodbye 1");
+            _log.LogDebug("Goodbye 1");
             return Task.CompletedTask;
         }
     }
 
     public class HelloWorldService2 : IHostedService
     {
+        readonly ILogger _log;
+
+        public HelloWorldService2(ILogger<HelloWorldService2> logger)
+        {
+            _log = logger;
+        }
+
         public Task StartAsync(CancellationToken cancellationToken)
         {
-            Console.WriteLine("Hello world 2");
+            _log.LogDebug("Hello world 2");
             return Task.CompletedTask;
         }
 
         public Task StopAsync(CancellationToken cancellationToken)
         {
-            Console.WriteLine("Goodbye 2");
+            _log.LogDebug("Goodbye 2");
             return Task.CompletedTask;
         }
     }
 
     public class HelloWorldService3 : IHostedService
     {
+        readonly ILogger _log;
+
+        public HelloWorldService3(ILogger<HelloWorldService3> logger)
+        {
+            _log = logger;
+        }
+
         public Task StartAsync(CancellationToken cancellationToken)
         {
-            Console.WriteLine("Hello world 3");
+            _log.LogDebug("Hello world 3");
             return Task.CompletedTask;
         }
 
         public Task StopAsync(CancellationToken cancellationToken)
         {
-            Console.WriteLine("Goodbye 3");
+            _log.LogDebug("Goodbye 3");
             return Task.CompletedTask;
         }
     }
@@ -62,7 +84,16 @@ namespace PracticalAspNetCore
                     services.AddHostedService<HelloWorldService>();
                     services.AddHostedService<HelloWorldService2>();
                     services.AddHostedService<HelloWorldService3>();
-                });
+                })
+                .ConfigureLogging(logging =>
+                {
+                    logging.ClearProviders();
+                    logging.AddConsole();
+                    logging.AddFilter((provider, category, logLevel) =>
+                    {
+                        return !category.Contains("Microsoft");
+                    });
+                }); ;
 
             await host.RunConsoleAsync();
         }
