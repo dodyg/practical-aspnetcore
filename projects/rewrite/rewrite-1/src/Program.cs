ce5a0e20d873e841c238851082566f9fb76c179e
Dody Gunawinata <dodyg@silverkeytech.com>
2020-02-14 20:36:53 +0800

    Upgrade to 3.1 and use Routing

--- projects/rewrite/rewrite-1/src/Program.cs
+++ projects/rewrite/rewrite-1/src/Program.cs
@@ -1,41 +1,37 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.AspNetCore.Rewrite;
 using Microsoft.AspNetCore.Routing;
-using Microsoft.AspNetCore;
+using Microsoft.Extensions.Hosting;
 
 namespace PracticalAspNetCore
 {
     public class Startup
     {
-        public Startup(IHostingEnvironment env, ILoggerFactory logger)
-        {
-        }
-
         public void ConfigureServices(IServiceCollection services)
         {
-            //This is the only service available at ConfigureServices
             services.AddRouting();
         }
 
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory logger)
+        public void Configure(IApplicationBuilder app)
         {
+            app.UseRouting();
+
             var options = new RewriteOptions()
-               .AddRedirect("/$", "/"); //redirect when path ends with /
+                .AddRedirect("/$", "/"); //redirect when path ends with /
 
             app.UseRewriter(options);
 
-            var routes = new RouteBuilder(app);
-            routes.MapGet("", (context) =>
+            app.UseEndpoints(endpoints =>
             {
-                context.Response.Headers.Add("content-type", "text/html");
-                return context.Response.WriteAsync($"Always display this page when path ends with / e.g. <a href=\"/hello-world/\">/hello-world/</a> or <a href=\"/welcome/everybody/inthis/train/\">/welcome/everybody/inthis/train/</a>.");
+                endpoints.MapGet("/", (context) =>
+                {
+                    context.Response.Headers.Add("content-type", "text/html");
+                    return context.Response.WriteAsync($"Always display this page when path ends with / e.g. <a href=\"/hello-world/\">/hello-world/</a> or <a href=\"/welcome/everybody/inthis/train/\">/welcome/everybody/inthis/train/</a>.");
+                });
             });
-
-            app.UseRouter(routes.Build());
         }
     }
 
