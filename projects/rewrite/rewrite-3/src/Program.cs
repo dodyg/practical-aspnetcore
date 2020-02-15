144236bd973e8a530f0bd6c97f571565ea965c94
Dody Gunawinata <dodyg@silverkeytech.com>
2020-02-15 09:48:12 +0800

    Upgrade to 3.1

--- projects/rewrite/rewrite-3/src/Program.cs
+++ projects/rewrite/rewrite-3/src/Program.cs
@@ -1,50 +1,40 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.AspNetCore.Rewrite;
 using Microsoft.AspNetCore.Routing;
-using Microsoft.AspNetCore;
+using Microsoft.Extensions.Hosting;
 
 namespace PracticalAspNetCore
 {
     public class Startup
     {
-        public Startup(IHostingEnvironment env, ILoggerFactory logger)
+        public void Configure(IApplicationBuilder app)
         {
-        }
+            app.UseRouting();
 
-        public void ConfigureServices(IServiceCollection services)
-        {
-            //This is the only service available at ConfigureServices
-            services.AddRouting();
-        }
-
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory logger)
-        {
             var options = new RewriteOptions()
-               .AddRewrite("([/_0-9a-z-]+)+(.*)$", "/?path=$1&ext=$2", skipRemainingRules: false); //redirect any path that ends with .html 
+               .AddRewrite("([/_0-9a-z-]+)+(.*)$", "/?path=$1&ext=$2", skipRemainingRules: false);  
 
             app.UseRewriter(options);
 
-            var routes = new RouteBuilder(app);
-            routes.MapGet("", (context) =>
+            app.UseEndpoints(endpoints =>
             {
-                context.Response.Headers.Add("content-type", "text/html");
-                var path = context.Request.Query["Path"];
-                var ext = context.Request.Query["Ext"];
-                return context.Response.WriteAsync($@"
-                <h1>REWRITE</h1>
-                Always display this page when path ends with an extension (e.g. .html or .aspx) and capture the their values. 
-                For example <a href=""/hello-world.html"">/hello-world.html</a> or <a href=""/welcome/everybody/inthis/train.aspx"">/welcome/everybody/inthis/train.aspx</a>.
-                <br/><br/>
-                Query String ""path"" = {path}<br/>
-                Query String ""ext"" = {ext}<br/>
-                ");
+                endpoints.MapGet("/", (context) =>
+                {
+                    context.Response.Headers.Add("content-type", "text/html");
+                    var path = context.Request.Query["Path"];
+                    var ext = context.Request.Query["Ext"];
+                    return context.Response.WriteAsync($@"
+                    <h1>REWRITE</h1>
+                    Always display this page when path ends with an extension (e.g. .html or .aspx) and capture the their values. 
+                    For example <a href=""/hello-world.html"">/hello-world.html</a> or <a href=""/welcome/everybody/inthis/train.aspx"">/welcome/everybody/inthis/train.aspx</a>.
+                    <br/><br/>
+                    Query String ""path"" = {path}<br/>
+                    Query String ""ext"" = {ext}<br/>
+                    ");
+                });
             });
-
-            app.UseRouter(routes.Build());
         }
     }
 
