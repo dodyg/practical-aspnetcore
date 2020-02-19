b8cd13f32b83e0b0c9c6e20c8358e91efc385777
Dody Gunawinata <dodyg@silverkeytech.com>
2020-02-19 11:03:07 +0800

    Fix doc

--- projects/mvc/localization/mvc-localization-4/src/Program.cs
+++ projects/mvc/localization/mvc-localization-4/src/Program.cs
@@ -73,7 +73,7 @@ namespace MvcLocalization
                     This means that we do not have to fully qualify the name of the resource file based on the following naming rules <br/><br/>
                     <i>Resources are named for the full type name of their class minus the assembly name. For example, a French resource in a project whose main assembly is LocalizationWebsite.Web.dll for the class LocalizationWebsite.Web.Startup would be named Startup.fr.resx. A resource for the class LocalizationWebsite.Web.Controllers.HomeController would be named Controllers.HomeController.fr.resx. If your targeted class's namespace isn't the same as the assembly name you will need the full type name. For example, in the sample project a resource for the type ExtraNamespace.Tools would be named ExtraNamespace.Tools.fr.resx.
                     </i>
-                    <a href=""https://docs.microsoft.com/en-us/aspnet/core/fundamentals/localization?view=aspnetcore-2.1"">Doc</a>
+                    <a href=""https://docs.microsoft.com/en-us/aspnet/core/fundamentals/localization?view=aspnetcore-3.1"">Doc</a>
                 </p>
                 <p>
                     <a href=""/about/index"">About</a>
