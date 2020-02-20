6b4d7594738c0bdd95fde88947fe84a023d69aaa
Dody Gunawinata <dodyg@silverkeytech.com>
2020-02-20 11:56:16 +0800

    Upgrading 3.1.

--- projects/mvc/routing/routing-2/src/Program.cs
+++ projects/mvc/routing/routing-2/src/Program.cs
@@ -1,38 +1,29 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.AspNetCore;
-using Microsoft.Extensions.Configuration;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Hosting;
 
 namespace PracticalAspNetCore
 {
     public class Startup
     {
-        public Startup(IHostingEnvironment env, ILoggerFactory logger, IConfiguration configuration)
-        {
-            //These are three services available at constructor
-        }
-
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvcCore().
-                SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddMvc();
         }
 
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory logger, IConfiguration configuration)
+        public void Configure(IApplicationBuilder app)
         {
-            app.UseMvc(routes => {
-                routes.MapRoute(
-                    name: "default",
-                    template: "{controller=HomePage}/{action=Index}");
+            app.UseRouting();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapDefaultControllerRoute();
             });
         }
     }
 
-    public class HomePageController : Controller
+    public class HomeController : Controller
     {
         public ActionResult Index()
         {
@@ -45,8 +36,8 @@ namespace PracticalAspNetCore
                 The following links call the same controller and action.
                 <ul>
                     <li><a href=""/"">/</a></li>
-                    <li><a href=""/homepage"">/homepage</a></li>
-                    <li><a href=""/homepage/index"">/homepage/index</a></li>
+                    <li><a href=""/home"">/home</a></li>
+                    <li><a href=""/home/index"">/home/index</a></li>
                 </ul>
                 </body></html>",
                 ContentType = "text/html"
