465e7f09bf9a63932413bbd60f73c2bb781703f8
Dody Gunawinata <dodyg@silverkeytech.com>
2020-02-29 15:11:40 +0200

    Upgrade to 3.1

--- projects/web-utilities/web-utilities-reason-phrases/src/Program.cs
+++ projects/web-utilities/web-utilities-reason-phrases/src/Program.cs
@@ -1,28 +1,14 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.AspNetCore;
-using Microsoft.Extensions.Configuration;
-using System.Collections.Generic;
 using Microsoft.AspNetCore.WebUtilities;
+using Microsoft.Extensions.Hosting;
 
 namespace PracticalAspNetCore
 {
     public class Startup
     {
-        public Startup(IHostingEnvironment env, ILoggerFactory logger)
-        {
-            //These are two services available at constructor
-        }
-
-        public void ConfigureServices(IServiceCollection services)
-        {
-            //This is the only service available at ConfigureServices
-        }
-
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory logger, IConfiguration configuration)
+        public void Configure(IApplicationBuilder app)
         {
             app.Run(context =>
             {
