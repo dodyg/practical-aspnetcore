5e9bd7a1114e25661e0707378f9a881d074d3cd8
Dody Gunawinata <dodyg@silverkeytech.com>
2020-03-02 01:20:18 +0200

    Upgraded to 3.1.

--- projects/features/features-connection/src/Program.cs
+++ projects/features/features-connection/src/Program.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Hosting.Server.Features;
 using Microsoft.AspNetCore.Http.Features;
 using Microsoft.AspNetCore;
+using Microsoft.Extensions.Hosting;
 
 namespace Features.Connection 
 {
