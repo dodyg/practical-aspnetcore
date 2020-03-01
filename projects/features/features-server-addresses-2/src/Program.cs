b1da304e3200314e5f210ca946a3bed5e398baa3
Dody Gunawinata <dodyg@silverkeytech.com>
2020-03-02 01:47:00 +0200

    Upgrade to 3.1

--- projects/features/features-server-addresses-2/src/Program.cs
+++ projects/features/features-server-addresses-2/src/Program.cs
@@ -7,8 +7,9 @@ using Microsoft.AspNetCore.Hosting.Server;
 using System.Threading.Tasks;
 using System.Collections.Generic;
 using System.Linq;
+using Microsoft.Extensions.Hosting;
 
-namespace HelloWorldWithReload
+namespace PracticalAspNetCore
 {
     public class Startup
     {
