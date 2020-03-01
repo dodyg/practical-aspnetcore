4d2987c65d264df4e14199836d112095300f60bb
Dody Gunawinata <dodyg@silverkeytech.com>
2020-03-02 01:45:53 +0200

    Upgrade to 3.1

--- projects/features/features-server-addresses/src/Program.cs
+++ projects/features/features-server-addresses/src/Program.cs
@@ -3,8 +3,9 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Hosting.Server.Features;
 using Microsoft.AspNetCore;
+using Microsoft.Extensions.Hosting;
 
-namespace HelloWorldWithReload 
+namespace PracticalAspNetCore 
 {
     public class Startup
     {
