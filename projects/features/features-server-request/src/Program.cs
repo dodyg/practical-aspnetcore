aedc2e6328d269d728e905230792f518a52bf39f
Dody Gunawinata <dodyg@silverkeytech.com>
2020-03-02 01:52:07 +0200

    Upgrade to 3.1

--- projects/features/features-server-request/src/Program.cs
+++ projects/features/features-server-request/src/Program.cs
@@ -2,9 +2,9 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Http.Features;
-using Microsoft.AspNetCore;
+using Microsoft.Extensions.Hosting;
 
-namespace Features.ServerRequest 
+namespace PracticalAspNetCore 
 {
     public class Startup
     {
