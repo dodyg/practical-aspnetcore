e1fd1492bcc8dbf5124625655bf8d33bcd735914
Dody Gunawinata <dodyg@silverkeytech.com>
2020-03-07 19:55:31 +0200

    Upgrade to 3.1

--- projects/request/form-values/src/Program.cs
+++ projects/request/form-values/src/Program.cs
@@ -6,73 +6,63 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.AspNetCore.Routing;
 using System.IO;
 using Microsoft.AspNetCore;
+using Microsoft.Extensions.Hosting;
 
 namespace PracticalAspNetCore
 {
     public class Startup
     {
-        public Startup(IHostingEnvironment env, ILoggerFactory logger)
+        public void Configure(IApplicationBuilder app, IHostEnvironment env)
         {
-            //These are two services available at constructor
-        }
-
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddRouting();
-        }
-
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory logger)
-        {
-            //These are the three default services available at Configure
-            var routerBuilder = new RouteBuilder(app);
-
-            routerBuilder.MapGet("", async context =>
+            app.UseRouting();
+            app.UseEndpoints(endpoints =>
             {
-                context.Response.Headers.Add("content-type", "text/html");
-
-                var body = $@"
-               <h1>Upload File</h1>
-               <form action=""Upload"" method=""post"" enctype=""multipart/form-data"">
-                    <input type=""text"" name=""name"" placeholder=""Name"" /><br/><br/>
-                    <input type=""password"" name=""password"" placeholder=""Password"" /><br/><br/>
-                    <textarea name=""description"" placeholder=""Description""></textarea><br/><br/>
-                    <select name=""gender"">
-                        <option value=""M"">Male</option>
-                        <option value=""F"">Female</option>
-                    </select><br/><br/>
-                    <select name=""hobbies"" multiple=""multiple"">
-                        <option>Writing Novel</option>
-                        <option>Watching Old Movies</option>
-                        <option>Swimming</option>
-                    </select><br/><br/>
-                    <input type=""checkbox"" name=""married"" value=""Yes""> Married? <br/><br/>
-                    <input type=""checkbox"" name=""adventurous"" value=""Yes""> Adventurous? <br/><br/>
-                    <input type=""hidden"" name=""secretHiddenValue"" value=""We are secretive"" />
-                    <input type=""submit"" value=""Upload"" />
-               </form>
-               ";
-
-                await context.Response.WriteAsync(body);
-            });
-
-            routerBuilder.MapPost("Upload", async context =>
-            {
-                context.Response.Headers.Add("content-type", "text/html");
-
-                if (context.Request.HasFormContentType)
+                endpoints.MapGet("", async context =>
                 {
-                    var form = await context.Request.ReadFormAsync();
+                    context.Response.Headers.Add("content-type", "text/html");
 
-                    foreach (var v in form.Keys)
+                    var body = $@"
+                <h1>Upload File</h1>
+                <form action=""Upload"" method=""post"" enctype=""multipart/form-data"">
+                        <input type=""text"" name=""name"" placeholder=""Name"" /><br/><br/>
+                        <input type=""password"" name=""password"" placeholder=""Password"" /><br/><br/>
+                        <textarea name=""description"" placeholder=""Description""></textarea><br/><br/>
+                        <select name=""gender"">
+                            <option value=""M"">Male</option>
+                            <option value=""F"">Female</option>
+                        </select><br/><br/>
+                        <select name=""hobbies"" multiple=""multiple"">
+                            <option>Writing Novel</option>
+                            <option>Watching Old Movies</option>
+                            <option>Swimming</option>
+                        </select><br/><br/>
+                        <input type=""checkbox"" name=""married"" value=""Yes""> Married? <br/><br/>
+                        <input type=""checkbox"" name=""adventurous"" value=""Yes""> Adventurous? <br/><br/>
+                        <input type=""hidden"" name=""secretHiddenValue"" value=""We are secretive"" />
+                        <input type=""submit"" value=""Upload"" />
+                </form>
+                ";
+
+                    await context.Response.WriteAsync(body);
+                });
+
+                endpoints.MapPost("Upload", async context =>
+                {
+                    context.Response.Headers.Add("content-type", "text/html");
+
+                    if (context.Request.HasFormContentType)
                     {
-                        await context.Response.WriteAsync($"{v} = {form[v]} <br/>");
+                        var form = await context.Request.ReadFormAsync();
+
+                        foreach (var v in form.Keys)
+                        {
+                            await context.Response.WriteAsync($"{v} = {form[v]} <br/>");
+                        }
+
                     }
-
-                }
-                await context.Response.WriteAsync("");
+                    await context.Response.WriteAsync("");
+                });
             });
-
-            app.UseRouter(routerBuilder.Build());
         }
 
         public static byte[] ReadFully(Stream input)
