d876b99ef75cd81e3348a3ddc26fd720d055234b
Dody Gunawinata <dodyg@silverkeytech.com>
2020-03-07 20:16:43 +0200

    Upgrade to 3.1

--- projects/request/query-string-3/src/Program.cs
+++ projects/request/query-string-3/src/Program.cs
@@ -1,28 +1,14 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Primitives;
-using Microsoft.AspNetCore;
+using Microsoft.Extensions.Hosting;
 
 namespace PracticalAspNetCore
 {
     public class Startup
     {
-        public Startup(IHostingEnvironment env, ILoggerFactory logger)
+        public void Configure(IApplicationBuilder app)
         {
-            //These are two services available at constructor
-        }
-
-        public void ConfigureServices(IServiceCollection services)
-        {
-            //This is the only service available at ConfigureServices
-        }
-
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory logger)
-        {
-            //These are the three default services available at Configure
             app.Run(async context =>
             {
                 context.Response.Headers.Add("content-type", "text/html");
