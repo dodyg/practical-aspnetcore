449cb07209a20940223f54aec50e1fb26e9388d7
Dody Gunawinata <dodyg@silverkeytech.com>
2020-03-07 20:15:48 +0200

    Upgrade to 3.1

--- projects/request/query-string-2/src/Program.cs
+++ projects/request/query-string-2/src/Program.cs
@@ -5,27 +5,18 @@ using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Primitives;
 using Microsoft.AspNetCore;
+using Microsoft.Extensions.Hosting;
 
 namespace PracticalAspNetCore
 {
     public class Startup
     {
-        public Startup(IHostingEnvironment env, ILoggerFactory logger)
-        {
-            //These are two services available at constructor
-        }
-
-        public void ConfigureServices(IServiceCollection services)
-        {
-            //This is the only service available at ConfigureServices
-        }
-
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory logger)
+        public void Configure(IApplicationBuilder app)
         {
             //These are the three default services available at Configure
             app.Run(async context =>
             {
-                context.Response.Headers.Add("content-type", "text/html");
+                context.Response.Headers.Add("Content-Type", "text/html");
 
                 StringValues queryString = context.Request.Query["message"];
 
