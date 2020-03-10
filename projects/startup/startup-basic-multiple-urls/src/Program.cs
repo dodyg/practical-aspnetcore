72d09652744e6fc2c75afebaec5e4702ec976d47
Dody Gunawinata <dodyg@silverkeytech.com>
2020-03-10 13:31:20 +0200

    Upgrade to 3.1

--- projects/startup/startup-basic-multiple-urls/src/Program.cs
+++ projects/startup/startup-basic-multiple-urls/src/Program.cs
@@ -1,28 +1,14 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.AspNetCore;
+using Microsoft.Extensions.Hosting;
 
-namespace PracticalAspNetCoreUrls
+namespace PracticalAspNetCore
 {
     public class Startup
     {
-        public Startup(IHostingEnvironment env, ILoggerFactory logger)
+        public void Configure(IApplicationBuilder app)
         {
-            //These are two services available at constructor
-        }
-
-        public void ConfigureServices(IServiceCollection services)
-        {
-            //This is the only service available at ConfigureServices
-        }
-
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory logger)
-        {
-            //These are the three default services available at Configure
-
             app.Run(context =>
             {
                 return context.Response.WriteAsync("Hello world responding on http://localhost:1000 and http://localhost:5000");
@@ -37,10 +23,11 @@ namespace PracticalAspNetCoreUrls
             CreateHostBuilder(args).Build().Run();
         }
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>()
-                .UseUrls("http://*:1000", "http://*:5000")
-                .UseEnvironment("Development");
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                    webBuilder.UseStartup<Startup>()
+                    .UseUrls("http://*:1000", "http://*:5000")
+                );
     }
 }
