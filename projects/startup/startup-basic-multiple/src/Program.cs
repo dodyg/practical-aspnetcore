25b268a4d2478cecc91999813bdc97d831b13e19
Dody Gunawinata <dodyg@silverkeytech.com>
2020-03-10 12:49:33 +0200

    Upgrade to 3.1

--- projects/startup/startup-basic-multiple/src/Program.cs
+++ projects/startup/startup-basic-multiple/src/Program.cs
@@ -1,28 +1,14 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.AspNetCore;
+using Microsoft.Extensions.Hosting;
 
 namespace PracticalAspNetCore
 {
     public class Startup1
     {
-        public Startup1(IHostingEnvironment env, ILoggerFactory logger)
+        public void Configure(IApplicationBuilder app)
         {
-            //These are two services available at constructor
-        }
-
-        public void ConfigureServices(IServiceCollection services)
-        {
-            //This is the only service available at ConfigureServices
-        }
-
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory logger)
-        {
-            //These are the three default services available at Configure
-
             app.Run(context =>
             {
                 return context.Response.WriteAsync("Hello world from Startup1");
@@ -32,20 +18,8 @@ namespace PracticalAspNetCore
 
     public class Startup2
     {
-        public Startup2(IHostingEnvironment env, ILoggerFactory logger)
+        public void Configure(IApplicationBuilder app)
         {
-            //These are two services available at constructor
-        }
-
-        public void ConfigureServices(IServiceCollection services)
-        {
-            //This is the only service available at ConfigureServices
-        }
-
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory logger)
-        {
-            //These are the three default services available at Configure
-
             app.Run(context =>
             {
                 return context.Response.WriteAsync("Hello world Startup2");
@@ -53,26 +27,24 @@ namespace PracticalAspNetCore
         }
     }
 
-    public class Program
+   public class Program
     {
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
         }
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args)
-        {
-            var hostBuilder = WebHost.CreateDefaultBuilder(args).UseEnvironment("Development");
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                     //This is a dumb way of doing it. You can use command line argument, etc to switch your startup 
+                     var startupNumber = 1; //CHANGE THIS to 2 if you want to use Startup2
 
-            //This is a dumb way of doing it. You can use command line argument, etc to switch your startup 
-            const int startupNumber = 1; //CHANGE THIS to 2 if you want to use Startup2
-
-            if (startupNumber == 1)
-                hostBuilder.UseStartup<Startup1>();
-            else if (startupNumber == 2)
-                hostBuilder.UseStartup<Startup2>();
-
-            return hostBuilder;
-        }
+                     if (startupNumber == 1)
+                        webBuilder.UseStartup<Startup1>();
+                     else if (startupNumber == 2)
+                        webBuilder.UseStartup<Startup2>();
+                });
     }
 }
