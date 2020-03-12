797cd26a90d6ed97eddb82ec577ce065ec8a5ec5
Dody Gunawinata <dodyg@silverkeytech.com>
2020-03-12 11:43:01 +0200

    Clean up

--- projects/application-environment/src/Program.cs
+++ projects/application-environment/src/Program.cs
@@ -1,10 +1,6 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.AspNetCore;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 
 namespace PracticalAspNetCore
