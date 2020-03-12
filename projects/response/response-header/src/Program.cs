fa195e92e8abd5f563ef6f755cbedb63800a5886
Dody Gunawinata <dodyg@silverkeytech.com>
2020-03-12 14:48:18 +0200

    Upgrade to 3.1

--- projects/response/response-header/src/Program.cs
+++ projects/response/response-header/src/Program.cs
@@ -5,25 +5,14 @@ using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using Microsoft.AspNetCore;
+using Microsoft.Extensions.Hosting;
 
-namespace StartupWithHeader 
+namespace PracticalAspNetCore
 {
     public class Startup
     {
-        public Startup(IHostingEnvironment env, ILoggerFactory logger)
+        public void Configure(IApplicationBuilder app)
         {
-            //These are two services available at constructor
-        }
-
-        public void ConfigureServices(IServiceCollection services)
-        {
-            //This is the only service available at ConfigureServices
-        }
-
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory logger)
-        {
-            //These are the three default services available at Configure
-            
             app.Run(async context =>
             {
                 context.Response.Headers.Add("content-type", "text/html");
