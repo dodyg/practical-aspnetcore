3e9033a52cfb5d18632192312581c66ee62eaaf3
Dody Gunawinata <dodyg@silverkeytech.com>
2020-03-12 14:31:43 +0200

    Upgrade to 3.1

--- projects/logging/logging-2/src/Program.cs
+++ projects/logging/logging-2/src/Program.cs
@@ -2,28 +2,15 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.AspNetCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
 
 namespace PracticalAspNetCore
 {
     public class Startup
     {
-        public Startup(IHostingEnvironment env, ILoggerFactory logger)
+        public void Configure(IApplicationBuilder app, IConfiguration configuration, ILogger<Startup> log)
         {
-            //These are two services available at constructor
-        }
-
-        public void ConfigureServices(IServiceCollection services)
-        {
-            //This is the only service available at ConfigureServices
-        }
-
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory logger, IConfiguration configuration)
-        {
-            //These are the four default services available at Configure
-            var log = logger.CreateLogger("AppLogger");
             app.Run(context =>
             {
                 log.LogInformation("This is a information message");
@@ -40,16 +27,18 @@ namespace PracticalAspNetCore
             CreateHostBuilder(args).Build().Run();
         }
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>()
-                .ConfigureLogging(builder =>
-                {
-                    // Trace, Debug, Information, Warning, Error, Critical, None
-                    builder.AddFilter("Microsoft", LogLevel.Warning); //Only show Warning log and above from anything that contains Microsoft.
-                    builder.AddFilter("AppLogger", LogLevel.Trace);//Pretty much show everything from AppLogger
-                    builder.AddConsole();
-                })
-                .UseEnvironment("Development");
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                    webBuilder
+                    .ConfigureLogging(builder =>
+                    {
+                        // Trace, Debug, Information, Warning, Error, Critical, None
+                        builder.AddFilter("Microsoft", LogLevel.Warning); //Only show Warning log and above from anything that contains Microsoft.
+                        builder.AddFilter("AppLogger", LogLevel.Trace);//Pretty much show everything from AppLogger
+                        builder.AddConsole();
+                    })
+                    .UseStartup<Startup>()
+                );
     }
 }
