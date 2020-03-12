3de5443fa66ec184b2c4264ea5f00d0d04a64141
Dody Gunawinata <dodyg@silverkeytech.com>
2020-03-12 16:09:18 +0200

    Upgraded to 3.1

--- projects/device-detection/src/Program.cs
+++ projects/device-detection/src/Program.cs
@@ -3,26 +3,21 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.AspNetCore;
 using Microsoft.Extensions.Configuration;
 using Wangkanai.Detection;
+using Microsoft.Extensions.Hosting;
 
 namespace PracticalAspNetCore
 {
     public class Startup
     {
-        public Startup(IHostingEnvironment env, ILoggerFactory logger, IConfiguration configuration)
-        {
-            //These are three services available at constructor
-        }
-
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDetectionCore()
                 .AddDevice();
         }
 
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory logger, IConfiguration configuration)
+        public void Configure(IApplicationBuilder app)
         {
             //These are the four default services available at Configure
             app.Run(context =>
