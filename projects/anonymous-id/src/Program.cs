8bfcb1a1b44605ec4fea9b6c518f66a8f3d2513c
Dody Gunawinata <dodyg@silverkeytech.com>
2020-03-12 12:36:39 +0200

    Upgrade 3.1

--- projects/anonymous-id/src/Program.cs
+++ projects/anonymous-id/src/Program.cs
@@ -1,27 +1,14 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.AspNetCore;
-using Microsoft.Extensions.Configuration;
-using Microsoft.AspNetCore.Routing;
 using ReturnTrue.AspNetCore.Identity.Anonymous;
+using Microsoft.Extensions.Hosting;
 
 namespace PracticalAspNetCore
 {
     public class Startup
     {
-        public Startup(IHostingEnvironment env, ILoggerFactory logger, IConfiguration configuration)
-        {
-            //These are three services available at constructor
-        }
-
-        public void ConfigureServices(IServiceCollection services)
-        {
-        }
-
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory logger, IConfiguration configuration)
+        public void Configure(IApplicationBuilder app)
         {
             app.UseAnonymousId(new AnonymousIdCookieOptionsBuilder()
                 .SetCustomCookieName("Anoymous_Cookie_Tracker")
