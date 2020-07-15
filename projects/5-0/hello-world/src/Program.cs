342374ec634f8734ddfe66f41d686697d9a8df19
Dody Gunawinata <dodyg@silverkeytech.com>
2020-07-15 17:05:06 +0200

    Make hello world in C# 9

--- projects/5-0/hello-world/src/Program.cs
+++ projects/5-0/hello-world/src/Program.cs
@@ -3,28 +3,16 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Hosting;
 
-namespace PracticalAspNetCore
-{
-    public class Startup
-    {
-        public void Configure(IApplicationBuilder app)
-        {
-            app.Run(async context =>
+CreateHostBuilder(args).Build().Run();
+
+IHostBuilder CreateHostBuilder(string[] args) =>
+    Host.CreateDefaultBuilder(args)
+        .ConfigureWebHostDefaults(webBuilder =>
+            webBuilder.Configure(app =>
             {
-                await context.Response.WriteAsync("Hello world .NET 5");
-            });
-        }
-    }
+                app.Run(async context => {
+                    await context.Response.WriteAsync("Hello world .NET 5");
+                });
+            })
+        );
 
-    public class Program
-    {
-        public static void Main(string[] args) =>
-            CreateHostBuilder(args).Build().Run();
-
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                    webBuilder.UseStartup<Startup>()
-                );
-    }
-}
