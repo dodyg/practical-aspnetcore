078f5a56c99b90f6bbf2c06dca3cb419ea9e3542
Dody Gunawinata <dodyg@silverkeytech.com>
2020-07-20 03:43:35 +0200

    Edit is working sort of

--- projects/5-0/sfa/wiki/Program.cs
+++ projects/5-0/sfa/wiki/Program.cs
@@ -1,51 +1,167 @@
 using System;
 using System.Globalization;
 using System.IO;
-using System.Threading.Tasks;
 using LiteDB;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Scriban;
 using Microsoft.Extensions.DependencyInjection;
+using HtmlBuilders;
+using Microsoft.Extensions.Primitives;
+using System.Threading.Tasks;
+using System.Net;
+using System.Collections.Generic;
+using System.Linq;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddSingleton<Wiki>();
 var app = builder.Build();
 
+DateTimeOffset Timestamp() => DateTimeOffset.UtcNow;
+
 app.MapGet("/", async context =>
 {
-  await context.Response.WriteAsync(BuildPage(title:"Welcome To Irtysh Wiki"));
+  await context.Response.WriteAsync(BuildPage(title: "Welcome To Irtysh Wiki"));
+});
+
+app.MapGet("/edit", async context =>
+{
+  var wiki = context.RequestServices.GetService<Wiki>()!;
+  var pageName = context.Request.Query["pageName"];
+
+  var (isFound, page) = wiki.LoadPage(pageName);
+  if (!isFound)
+  {
+    context.Response.StatusCode = (int) HttpStatusCode.NotFound;
+    return;
+  }
+
+  await context.Response.WriteAsync(BuildPage(pageName, () =>
+    {
+      return new []{ 
+        BuildForm(new PageInput(page!.Id, pageName, page.Content), path: $"{pageName}")
+      };
+    }));
 });
 
 app.MapGet("/{pageName}", async context =>
 {
-  var wiki = context.RequestServices.GetService<Wiki>();
+  var wiki = context.RequestServices.GetService<Wiki>()!;
   var pageName = context.Request.RouteValues["pageName"] as string ?? "";
 
   var (isFound, page) = wiki.LoadPage(pageName);
 
   if (isFound)
   {
-    await context.Response.WriteAsync($"Found {page!.Name}");
+    await context.Response.WriteAsync(BuildPage(pageName, () =>
+    {
+      return new[] 
+      { 
+        Markdig.Markdown.ToHtml(page!.Content),
+        HtmlTags.A.Href($"/edit?pageName={pageName}").Append("Edit").ToHtmlString()
+      };
+    }));
   }
-  else {
-    await context.Response.WriteAsync(BuildPage(pageName));
+  else
+  {
+    await context.Response.WriteAsync(BuildPage(pageName, () =>
+    {
+      return new[] { 
+        BuildForm(new PageInput(null, pageName, string.Empty), path: pageName)
+        };
+    }));
   }
 });
 
-string BuildPage(string title)
+app.MapPost("/{pageName}", context =>
 {
-  var head = Template.Parse("<title>{{ title }}</title>").Render(new { title });
+  var id = context.Request.Form["Id"];
+  var name = context.Request.Form["Name"];
+  var content = context.Request.Form["Content"];
 
-  var body = Template.Parse("<h1>{{ page_name }}</h1>")
-    .Render(new 
-    { 
-      PageName = CultureInfo.CurrentCulture.TextInfo.ToTitleCase(title.Replace('-', ' ')) 
+  var properName = name.ToString().Replace(' ', '-').ToLower();
+
+  var page = new Page
+  {
+    Name = name,
+    Content = content,
+    LastModified = Timestamp()
+  };
+
+  if (!StringValues.IsNullOrEmpty(id))
+    page.Id = Convert.ToInt32(id);
+
+  var wiki = context.RequestServices.GetService<Wiki>()!;  
+  var (isOK, p, ex) = wiki.SavePage(page);
+
+  if (!isOK)
+    Console.WriteLine($"Error {ex.Message}");
+
+  var pageName = context.Request.RouteValues["pageName"] as string ?? "";
+  context.Response.Redirect($"/{pageName}");  
+  return Task.CompletedTask;
+});
+
+string BuildForm(PageInput input, string path)
+{
+  var nameField = HtmlTags.Div.Class("field")
+    .Append(HtmlTags.Label.Class("label").Append(nameof(input.Name)))
+    .Append(HtmlTags.Div.Class("control")
+      .Append(HtmlTags.Input.Text.Class("input").Name("Name").Value(input.Name))
+    );
+
+  var contentField = HtmlTags.Div.Class("field")
+    .Append(HtmlTags.Label.Class("label").Append(nameof(input.Content)))
+    .Append(HtmlTags.Div.Class("control")
+      .Append(HtmlTags.Textarea.Name("Content").Class("textarea").Append(input.Content))
+    );
+
+  var submit = HtmlTags.Button.Class("button").Append("Submit");
+
+  var form = HtmlTags.Form
+                  .Attribute("method", "post")
+                  .Attribute("action", $"/{path}")
+                .Append(nameField)
+                .Append(contentField);
+
+  if (input.Id.HasValue)
+  {
+    HtmlTag id = HtmlTags.Input.Hidden.Name("Id").Value(input.Id.ToString());
+    form = form.Append(id);
+  }
+  
+  form = form.Append(submit);
+
+  return form.ToHtmlString();
+}
+
+string KebabToNormalCase(string txt) => CultureInfo.CurrentCulture.TextInfo.ToTitleCase(txt.Replace('-', ' '));
+
+string BuildPage(string title, Func<IEnumerable<string>>? buildContent = null)
+{
+  var head = Template.Parse(@"
+    <meta charset=""utf-8"">
+    <meta name=""viewport"" content=""width=device-width, initial-scale=1"">
+    <title>{{ title }}</title>
+    <link rel=""stylesheet"" href=""https://cdn.jsdelivr.net/npm/bulma@0.9.0/css/bulma.min.css"">
+  ").Render(new { title });
+
+  var body = Template.Parse(@"
+    <div class=""container"">
+      <h1 class=""title is-1"">{{ page_name }}</h1>
+
+      {{ content }}
+    </div>
+    ")
+    .Render(new
+    {
+      PageName = KebabToNormalCase(title),
+      Content = string.Join("<br/>", buildContent?.Invoke() ?? new [] { "" })
     });
-    
+
   var page = @"
-    <html>
+    <!DOCTYPE html>
       <head>
         {{ head }}
       </head>
@@ -61,15 +177,15 @@ string BuildPage(string title)
 
 await app.RunAsync();
 
-class Wiki 
+class Wiki
 {
   const string PageCollectionName = "Pages";
 
   readonly IWebHostEnvironment _env;
-  
+
   public Wiki(IWebHostEnvironment env)
   {
-      _env = env;
+    _env = env;
   }
 
   string GetDbPath() => Path.Combine(_env!.ContentRootPath, "wiki.db");
@@ -80,6 +196,8 @@ class Wiki
 
     var coll = db.GetCollection<Page>(PageCollectionName);
 
+    coll.EnsureIndex(x => x.Name);
+
     var p = coll.Query()
             .Where(x => x.Name.Equals(path, StringComparison.CurrentCultureIgnoreCase))
             .FirstOrDefault();
@@ -89,14 +207,39 @@ class Wiki
 
     return (true, p);
   }
+
+  public (bool isOK, Page? page, Exception ex) SavePage(Page page)
+  {
+    using var db = new LiteDatabase(GetDbPath());
+    var coll = db.GetCollection<Page>(PageCollectionName);
+    coll.EnsureIndex(x => x.Name);
+
+    if (page.Id == default(int))
+    {
+      Console.WriteLine("Insert");
+      coll.Insert(page);
+    }
+    else
+    {
+      Console.WriteLine("Update");
+      coll.Update(page);
+    }
+
+    return (true, page, null);
+  }
 }
 
-public record Page 
+
+
+public record Page
 {
+  public int Id { get; set; }
+
   public string Name { get; set; }
 
   public string Content { get; set; }
 
   public DateTimeOffset LastModified { get; set; }
-} 
+}
 
+public record PageInput (int? Id, string Name, string Content);
