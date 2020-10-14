f14490c9499433cd5a210589d26a900775635c09
Antti K. Koskela <posti@koskila.net>
2020-10-14 23:29:49 +0300

    Adds a simple StartUp file to the function

---
+++ projects/azure-functions/StartupExample/StartupExample/StartUp.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Hosting;
+using StartupExample;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+[assembly: WebJobsStartup(typeof(StartUp))]
+namespace StartupExample
+{
+    class StartUp : IWebJobsStartup
+    {
+        public void Configure(IWebJobsBuilder builder)
+        {
+        }
+    }
+}
