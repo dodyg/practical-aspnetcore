f0b79e9076079651c0b5d1725ae02c65a030297c
Antti K. Koskela <posti@koskila.net>
2020-10-20 21:45:43 +0300

    Adds a Data layer project with an initial EF model

---
+++ projects/azure-functions/DbContextExample/Data/ApplicationDbContext.cs
@@ -0,0 +1,27 @@
+﻿using DataLayer.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataLayer
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Entity> Entities { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public ApplicationDbContext() : base()
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options) => options.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=mydb;Trusted_Connection=True;MultipleActiveResultSets=True");
+    }
+}
