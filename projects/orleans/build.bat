52c06562fa6498a80b583abe7c7c179c7b825069
Dody Gunawinata <dodyg@silverkeytech.com>
2021-02-01 10:31:01 +0200

    Upgrade samples to Orleans 3.4

---
+++ projects/orleans/build.bat
@@ -0,0 +1,15 @@
+dotnet build hello-world\client\
+dotnet build hello-world\silo\
+dotnet build hello-world-2\client
+dotnet build hello-world-2\silo
+dotnet build hello-world-3
+dotnet build hello-world-4
+dotnet build http-client
+dotnet build reminder
+dotnet build rss-reader
+dotnet build rss-reader-2
+dotnet build rss-reader-3
+dotnet build rss-reader-4
+dotnet build rss-reader-5
+dotnet build rss-reader-6
+dotnet build timer
