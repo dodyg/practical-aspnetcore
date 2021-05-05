f9abdd28c50b2f89e2d1d738a124ec9450b9a284
Dody Gunawinata <dodyg@silverkeytech.com>
2021-05-05 10:00:42 +0200

    Fix accidental code

--- projects/net6/hello-world/src/Program.cs
+++ projects/net6/hello-world/src/Program.cs
@@ -4,7 +4,6 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Hosting;
 
 WebApplication app = WebApplication.Create();
-
 app.Run(async context =>
 {
     // Duplicate the code below and write more messages. Save and refresh your browser to see the result.
