68abd1c1d99f502cd7a8539f15077ddc85cbc8b2
Dody Gunawinata <dodyg@silverkeytech.com>
2021-05-27 15:04:25 +0200

    Use GetDebugView()

--- projects/net6/web-application-7/src/Program.cs
+++ projects/net6/web-application-7/src/Program.cs
@@ -1,15 +1,11 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
-using System.Linq;
 
 var app = WebApplication.Create();
 app.Run(async (context) =>
 {
-    foreach(var e in app.Configuration.AsEnumerable())
-    {
-        await context.Response.WriteAsync($"{e.Key} = {e.Value}\n");
-    }
+    await context.Response.WriteAsync((app.Configuration as IConfigurationRoot).GetDebugView());
 });
 
 await app.RunAsync();
