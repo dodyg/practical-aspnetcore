5988811e625702999749c1fc9d98a14a5198a268
Dody Gunawinata <dodyg@silverkeytech.com>
2021-07-15 12:05:01 +0200

    Update to preview 6 official

--- projects/net6/build.bat
+++ projects/net6/build.bat
@@ -3,9 +3,11 @@ dotnet build hello-world
 dotnet build map
 dotnet build map-2
 dotnet build map-3
+dotnet build map-4
+dotnet build map-methods
+dotnet build map-open-api
 dotnet build map-post
 dotnet build map-post-2
-dotnet build map-methods
 dotnet build web-application
 dotnet build web-application-2
 dotnet build web-application-3
@@ -17,3 +19,14 @@ dotnet build web-application-8
 dotnet build web-application-builder
 dotnet build web-application-builder-2
 dotnet build yarp/basic-demo/
+dotnet build json/json-12
+dotnet build json/json-13
+dotnet build json/json-14
+dotnet build json/json-15
+dotnet build json/json-16
+dotnet build json/json-17
+dotnet build json/json-18
+dotnet build json/json-19
+dotnet build json/json-20
+dotnet build json/json-21
+dotnet build json/json-22
