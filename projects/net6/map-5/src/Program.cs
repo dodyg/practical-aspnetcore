bd7b3a3b5f289d0751b3e9fc0dfc93e198f388b2
Dody Gunawinata <dodyg@silverkeytech.com>
2021-07-27 13:13:00 +0200

    Add sample about HttpRequest injection

---
+++ projects/net6/map-5/src/Program.cs
@@ -0,0 +1,30 @@
+var app = WebApplication.Create();
+
+string Plaintext(HttpRequest request)
+{
+    var name = request.Query["name"];
+    if (!string.IsNullOrWhiteSpace(name))
+        return "hello " + name;
+    else
+        return "hello";
+}
+
+// return Mime type: text/html
+app.MapGet("/hello", Plaintext);
+
+app.MapGet("/", async context =>
+{
+    await context.Response.WriteAsync(
+$@"
+    <html>
+        <body>
+            <ul>
+                <li><a href=""/hello?name=anne"">Greet with name</a></li>
+                <li><a href=""/hello"">Just greet</a></li>
+            </ul>
+        </body>
+    </html>
+");
+});
+
+app.Run();
