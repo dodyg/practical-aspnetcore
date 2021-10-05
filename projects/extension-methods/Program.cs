debcd93233d5809f159120853bf609a80a7cf398
Lucas Mundim <lucas.mundim044@gmail.com>
2021-10-05 17:09:29 -0300

    add extension methods sample; update sdk to 5.0.401 as its the latest release of .NET 5

---
+++ projects/extension-methods/Program.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Hosting;
+using System;
+
+namespace PracticalAspNetCore
+{
+    public static class DateTimeExtensions
+    {
+        public static DateTime NextDay(this DateTime dateTime)
+        {
+            return dateTime.AddDays(1);
+        }
+
+        public static DateTime PreviousDay(this DateTime dateTime) => dateTime.AddDays(-1);
+    }
+
+    public class Startup
+    {
+        public void Configure(IApplicationBuilder app)
+        {
+            app.Run(async context =>
+            {
+                // Duplicate the code below and write more messages. Save and refresh your browser to see the result.
+                var today = new DateTime(2001, 5, 25);
+
+                await context.Response.WriteAsync($"Say Today is {today.ToShortDateString()}. So the tomorrow is {today.NextDay().ToShortDateString()} and yesterday was {today.PreviousDay().ToShortDateString()}");
+            });
+        }
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args) =>
+            CreateHostBuilder(args).Build().Run();
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                    webBuilder.UseStartup<Startup>()
+                );
+    }
+}
