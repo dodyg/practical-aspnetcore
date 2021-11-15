c96dc6ef7c73bb8da4363f6d48c88befff131091
Dody Gunawinata <dodyg@silverkeytech.com>
2021-11-15 15:25:09 +0200

    ...

--- projects/net6/csharp-10/src/Models.cs
+++ projects/net6/csharp-10/src/Models.cs
@@ -2,3 +2,10 @@ namespace SK.Framework;
 public record class Person(string FirstName, string LastName);
 
 public record struct Id(int Value);
+
+public class Demo
+{
+    public string Name { get; set; }
+
+    public int Version { get; set; }
+}
