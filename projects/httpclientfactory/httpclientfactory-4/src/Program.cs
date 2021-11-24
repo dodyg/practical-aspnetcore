ccb4f32c29c68cb6d7ae99ad14f931edabb1aa82
Dody Gunawinata <dodyg@silverkeytech.com>
2021-11-24 11:47:20 +0200

    Upgrade to .NET 6

--- projects/httpclientfactory/httpclientfactory-4/src/Program.cs
+++ projects/httpclientfactory/httpclientfactory-4/src/Program.cs
@@ -1,51 +1,33 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.DependencyInjection;
-using System.Net.Http;
-using System.Threading.Tasks;
-using Microsoft.Extensions.Hosting;
+var builder = WebApplication.CreateBuilder();
+builder.Services.AddTransient<RssReader>();
+builder.Services.AddHttpClient<IRss, RssReader>();
+       
+var app = builder.Build();
 
-namespace PracticalAspNetCore
+app.Run(async context =>
 {
-    public class Startup
+    var rss = context.RequestServices.GetService<IRss>();
+    var result = await rss.Get("http://scripting.com/rss.xml");
+
+    context.Response.Headers.Add("Content-Type", "application/rss+xml");
+    await context.Response.WriteAsync(result);
+});
+
+app.Run();
+
+public interface IRss
+{
+    Task<string> Get(string url);
+}
+
+public class RssReader : IRss
+{
+    readonly HttpClient _client;
+
+    public RssReader(HttpClient client)
     {
-        public void ConfigureServices(IServiceCollection services)
-        {
-            // The order is important. If you switch it, it won't work.
-            services.AddTransient<RssReader>();
-            services.AddHttpClient<IRss, RssReader>();
-        }
-
-        public void Configure(IApplicationBuilder app)
-        {
-            app.Run(async context =>
-            {
-                var rss = context.RequestServices.GetService<IRss>();
-                var result = await rss.Get("http://scripting.com/rss.xml");
-
-                context.Response.Headers.Add("Content-Type", "application/rss+xml");
-                await context.Response.WriteAsync(result);
-            });
-        }
+        _client = client;
     }
 
-    public interface IRss
-    {
-        Task<string> Get(string url);
-    }
-
-    public class RssReader : IRss
-    {
-        readonly HttpClient _client;
-
-        public RssReader(HttpClient client)
-        {
-            _client = client;
-        }
-
-        public Task<string> Get(string url) => _client.GetStringAsync(url);
-    }
-
-
+    public Task<string> Get(string url) => _client.GetStringAsync(url);
 }
