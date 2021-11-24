6d809e71b95e837d345f1eb9e5340bcd0217a60c
Dody Gunawinata <dodyg@silverkeytech.com>
2021-11-24 11:39:00 +0200

    Converted to .NET 6

--- projects/httpclientfactory/httpclientfactory-1/src/Program.cs
+++ projects/httpclientfactory/httpclientfactory-1/src/Program.cs
@@ -1,33 +1,16 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.DependencyInjection;
-using System.Net.Http;
-using Microsoft.Extensions.Hosting;
+var builder = WebApplication.CreateBuilder();
+builder.Services.AddHttpClient();
 
-namespace PracticalAspNetCore
+var app = builder.Build();
+
+app.Run(async context =>
 {
-    public class Startup
-    {
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddHttpClient();
-        }
+    var httpClient = context.RequestServices.GetService<IHttpClientFactory>();
+    var client = httpClient.CreateClient();
+    var result = await client.GetStringAsync("http://scripting.com/rss.xml");
 
-        public void Configure(IApplicationBuilder app)
-        {
-            app.Run(async context =>
-            {
-                var httpClient = context.RequestServices.GetService<IHttpClientFactory>();
-                var client = httpClient.CreateClient();
-                var result = await client.GetStringAsync("http://scripting.com/rss.xml");
+    context.Response.Headers.Add("Content-Type", "application/rss+xml");
+    await context.Response.WriteAsync(result);
+});
 
-                context.Response.Headers.Add("Content-Type", "application/rss+xml");
-                await context.Response.WriteAsync(result);
-            });
-        }
-    }
-
-
-
-}
+app.Run();
