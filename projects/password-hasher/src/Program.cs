4e7ef8c730e651a2db251e9a14e6e29be07a501c
Dody Gunawinata <dodyg@silverkeytech.com>
2021-11-24 11:20:36 +0200

    Upgrade to minimal APIs

--- projects/password-hasher/src/Program.cs
+++ projects/password-hasher/src/Program.cs
@@ -1,37 +1,24 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.Extensions.Hosting;
 
-namespace PracticalAspNetCore 
-{
-    public class User
+var app = WebApplication.Create();
+app.Run(context =>
     {
+        var password = context.Request.Query["password"];
 
-    }
+        if (string.IsNullOrWhiteSpace(password))
+            password = "123456789";
 
-    public class Startup
-    {
-        public void Configure(IApplicationBuilder app)
-        {
-            app.Run(context =>
-            {
-                var password = context.Request.Query["password"];
+        var usr = new User();
+        var hasher = new PasswordHasher<User>();
+        var hashedPassword = hasher.HashPassword(usr, password);
 
-                if (string.IsNullOrWhiteSpace(password))
-                    password = "123456789";
+        var isPasswordMatch = hasher.VerifyHashedPassword(usr, hashedPassword, password);
 
-                var usr = new User();
-                var hasher = new PasswordHasher<User>();
-                var hashedPassword = hasher.HashPassword(usr, password);
+        return context.Response.WriteAsync($"Append ?password at url to test your own password hashing.\nPassword : {password} => Hashed : {hashedPassword} \nPassword Matched : {isPasswordMatch}");
+    });
 
-                var isPasswordMatch = hasher.VerifyHashedPassword(usr, hashedPassword, password);
+app.Run();
 
-                return context.Response.WriteAsync($"Append ?password at url to test your own password hashing.\nPassword : {password} => Hashed : {hashedPassword} \nPassword Matched : {isPasswordMatch}");
-            });
-        }
-    }
+public record User();
+
     
-
-}
