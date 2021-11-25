3893be3ada08baed0ee1590a4547e1b39c432f2a
Dody Gunawinata <dodyg@silverkeytech.com>
2021-11-25 03:47:28 +0200

    Format

--- projects/password-hasher/src/Program.cs
+++ projects/password-hasher/src/Program.cs
@@ -2,20 +2,20 @@ using Microsoft.AspNetCore.Identity;
 
 var app = WebApplication.Create();
 app.Run(context =>
-    {
-        var password = context.Request.Query["password"];
+{
+    var password = context.Request.Query["password"];
 
-        if (string.IsNullOrWhiteSpace(password))
-            password = "123456789";
+    if (string.IsNullOrWhiteSpace(password))
+        password = "123456789";
 
-        var usr = new User();
-        var hasher = new PasswordHasher<User>();
-        var hashedPassword = hasher.HashPassword(usr, password);
+    var usr = new User();
+    var hasher = new PasswordHasher<User>();
+    var hashedPassword = hasher.HashPassword(usr, password);
 
-        var isPasswordMatch = hasher.VerifyHashedPassword(usr, hashedPassword, password);
+    var isPasswordMatch = hasher.VerifyHashedPassword(usr, hashedPassword, password);
 
-        return context.Response.WriteAsync($"Append ?password at url to test your own password hashing.\nPassword : {password} => Hashed : {hashedPassword} \nPassword Matched : {isPasswordMatch}");
-    });
+    return context.Response.WriteAsync($"Append ?password at url to test your own password hashing.\nPassword : {password} => Hashed : {hashedPassword} \nPassword Matched : {isPasswordMatch}");
+});
 
 app.Run();
 
