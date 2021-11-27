041a43a8bfe29f7a99fa4be02b5471cdef247e02
Dody Gunawinata <dodyg@silverkeytech.com>
2021-11-27 12:27:51 +0200

    Upgradet to .NET 6

--- projects/grpc/grpc/client/src/Program.cs
+++ projects/grpc/grpc/client/src/Program.cs
@@ -1,35 +1,19 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.Hosting;
-using System.Threading.Tasks;
-using System;
 using Grpc.Net.Client;
-using System.Net.Http;
 
-namespace GrpcServer
+var app = WebApplication.Create();
+
+app.Run(async context =>
 {
-    public class Startup
+    var channel = GrpcChannel.ForAddress("https://localhost:5500");
+    var client = new Billboard.Board.BoardClient(channel);
+    var reply = await client.ShowMessageAsync(new Billboard.MessageRequest
     {
-        public void Configure(IApplicationBuilder app)
-        {
-            //Make sure that the grpc-server is run 
-            app.Run(async context =>
-            {
-                //We need this switch because we are connecting to an unsecure server. If the server runs on SSL, there's no need for this switch.
-                AppContext.SetSwitch("System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
-                var channel = GrpcChannel.ForAddress("http://localhost:5500");
-                var client = new Billboard.Board.BoardClient(channel);
-                var reply = await client.ShowMessageAsync(new Billboard.MessageRequest
-                {
-                    Message = "Good morning people of the world",
-                    Sender = "Dody Gunawinata"
-                });
+        Message = "Good morning people of the world",
+        Sender = "Dody Gunawinata"
+    });
 
-                var displayDate = new DateTime(reply.DisplayTime);
-                await context.Response.WriteAsync($"This server sends a gRPC request to a server and get the following result: Received message on {displayDate} from {reply.ReceiveFrom}");
-            });
-        }
-    }
+    var displayDate = new DateTime(reply.DisplayTime);
+    await context.Response.WriteAsync($"This server sends a gRPC request to a server and get the following result: Received message on {displayDate} from {reply.ReceiveFrom}");
+});
 
-}
+app.Run();
--- projects/grpc/grpc/server/src/Program.cs
+++ projects/grpc/grpc/server/src/Program.cs
@@ -1,73 +1,38 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore;
-using Microsoft.Extensions.Hosting;
-using System.Threading.Tasks;
 using Grpc.Core;
-using System;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.AspNetCore.Server.Kestrel.Core;
 
-namespace GrpcServer
+var builder = WebApplication.CreateBuilder();
+builder.Services.AddGrpc();
+builder.WebHost.ConfigureKestrel(k =>
 {
-    public class Startup
+    k.ConfigureEndpointDefaults(options =>
     {
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddGrpc();
-        }
+        options.Protocols = HttpProtocols.Http2;
+    });
 
-        public void Configure(IApplicationBuilder app)
-        {
-            app.UseRouting();
+    k.ListenLocalhost(5500, o => o.UseHttps());
+});
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGrpcService<BillboardService>();
-                endpoints.MapGet("/", context =>
-                {
-                    return context.Response.WriteAsync("This server contains a gRPC service");
-                });
-            });
-        }
-    }
+var app = builder.Build();
 
-    public class BillboardService : Billboard.Board.BoardBase
+app.MapGrpcService<BillboardService>();
+app.MapGet("/", context =>
+{
+    return context.Response.WriteAsync("This server contains a gRPC service");
+});
+
+app.Run();
+
+
+public class BillboardService : Billboard.Board.BoardBase
+{
+    public override Task<Billboard.MessageReply> ShowMessage(Billboard.MessageRequest request, ServerCallContext context)
     {
-        public override Task<Billboard.MessageReply> ShowMessage(Billboard.MessageRequest request, ServerCallContext context)
+        var now = DateTime.UtcNow;
+        return Task.FromResult(new Billboard.MessageReply
         {
-            var now = DateTime.UtcNow;
-            return Task.FromResult(new Billboard.MessageReply
-            {
-                DisplayTime = now.Ticks,
-                ReceiveFrom = request.Sender
-            });
-        }
-    }
-
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
-
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>().
-                    ConfigureKestrel(k =>
-                    {
-                        k.ConfigureEndpointDefaults(options =>
-                        {
-                            options.Protocols = HttpProtocols.Http2;
-                        });
-
-                        k.ListenLocalhost(5500);
-                    }).
-                    UseEnvironment(Environments.Development);
-                });
+            DisplayTime = now.Ticks,
+            ReceiveFrom = request.Sender
+        });
     }
 }
