d212df05c444b32629864caa509869e4bd0d6856
Dody Gunawinata <dodyg@silverkeytech.com>
2021-11-27 12:52:56 +0200

    Upgrade to .NET 6

--- projects/grpc/grpc-3/client/src/Program.cs
+++ projects/grpc/grpc-3/client/src/Program.cs
@@ -1,20 +1,6 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.Hosting;
-using System;
-using System.Net.Http;
 using Grpc.Net.Client;
-using Grpc.Core;
-using System.Threading;
-using System.Collections.Generic;
-using System.Threading.Tasks;
 
-namespace GrpcServer
-{
-    public class Startup
-    {
-        List<string> Fortunes { get; set; } = new List<string>()
+var fortunes = new List<string>()
         {
             "The fortune you seek is in another cookie.",
             "A closed mouth gathers no feet.",
@@ -28,51 +14,47 @@ namespace GrpcServer
             "An alien of some sort will be appearing to you shortly."
         };
 
-        public void Configure(IApplicationBuilder app)
+var app = WebApplication.Create();
+
+//Make sure that the grpc-server is run 
+app.Run(async context =>
+{
+    var channel = GrpcChannel.ForAddress("https://localhost:5500"); //check the values at /server project
+    var client = new Billboard.Board.BoardClient(channel);
+
+    context.Response.Headers["Content-Type"] = "text/event-stream";
+
+    using var tokenSource = new CancellationTokenSource();
+    CancellationToken token = tokenSource.Token;
+
+    using var stream = client.ShowMessage(cancellationToken: token);
+
+    foreach (var f in fortunes)
+    {
+        if (token.IsCancellationRequested)
+            break;
+
+        await stream.RequestStream.WriteAsync(new Billboard.MessageRequest
         {
-            //Make sure that the grpc-server is run 
-            app.Run(async context =>
-            {
-                //We need this switch because we are connecting to an unsecure server. If the server runs on SSL, there's no need for this switch.
-                AppContext.SetSwitch("System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
-                var channel = GrpcChannel.ForAddress("http://localhost:5500"); //check the values at /server project
-                var client = new Billboard.Board.BoardClient(channel);
+            FortuneCookie = f
+        });
 
-                context.Response.Headers["Content-Type"] = "text/event-stream";
+        await context.Response.WriteAsync($"Sending \"{f}\" \n");
+        await context.Response.Body.FlushAsync();
 
-                using var tokenSource = new CancellationTokenSource();
-                CancellationToken token = tokenSource.Token;
-
-                using var stream = client.ShowMessage(cancellationToken: token);
-
-                foreach (var f in Fortunes)
-                {
-                    if (token.IsCancellationRequested)
-                        break;
-
-                    await stream.RequestStream.WriteAsync(new Billboard.MessageRequest
-                    {
-                        FortuneCookie = f
-                    });
-
-                    await context.Response.WriteAsync($"Sending \"{f}\" \n");
-                    await context.Response.Body.FlushAsync();
-
-                    await Task.Delay(1000); //1 second
-                }
-
-                await stream.RequestStream.CompleteAsync();
-
-                var response = await stream.ResponseAsync;
-
-                await context.Response.WriteAsync("\n\n");
-
-                foreach (var r in response.Fortunes)
-                {
-                    await context.Response.WriteAsync($"Reply \"{r.Message}\". Original cookied received on {new DateTime(r.ReceivedTime)}. \n");
-                }
-            });
-        }
+        await Task.Delay(1000); //1 second
     }
 
-}
+    await stream.RequestStream.CompleteAsync();
+
+    var response = await stream.ResponseAsync;
+
+    await context.Response.WriteAsync("\n\n");
+
+    foreach (var r in response.Fortunes)
+    {
+        await context.Response.WriteAsync($"Reply \"{r.Message}\". Original cookied received on {new DateTime(r.ReceivedTime)}. \n");
+    }
+});
+
+app.Run();
--- projects/grpc/grpc-3/server/src/Program.cs
+++ projects/grpc/grpc-3/server/src/Program.cs
@@ -1,101 +1,60 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.Hosting;
-using System.Threading.Tasks;
 using Grpc.Core;
-using System;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.AspNetCore.Server.Kestrel.Core;
 using Billboard;
-using System.Linq;
-using System.Collections.Generic;
-using System.Threading;
 
-namespace GrpcServer
+var builder = WebApplication.CreateBuilder();
+builder.Services.AddGrpc();
+
+builder.WebHost.ConfigureKestrel(k =>
 {
-    public class Startup
+    k.ConfigureEndpointDefaults(options =>
     {
-        public void ConfigureServices(IServiceCollection services)
+        options.Protocols = HttpProtocols.Http2;
+    });
+
+    k.ListenLocalhost(5500, o => o.UseHttps());
+});
+
+var app = builder.Build();
+app.MapGrpcService<BillboardService>();
+app.MapGet("/", () => "This server contains a gRPC service");
+
+app.Run();
+
+public class ReceivedFortune
+{
+    public string Message { get; set; }
+
+    public DateTimeOffset Received { get; set; } = DateTimeOffset.UtcNow;
+}
+
+public class BillboardService : Billboard.Board.BoardBase
+{
+    public override async Task<MessageReply> ShowMessage(IAsyncStreamReader<MessageRequest> requestStream, ServerCallContext context)
+    {
+        var fortunes = new List<ReceivedFortune>();
+
+        using var tokenSource = new CancellationTokenSource();
+        CancellationToken token = tokenSource.Token;
+
+        await foreach (var request in requestStream.ReadAllAsync(token))
         {
-            services.AddGrpc();
+            var inBed = request.FortuneCookie[0..^1] + " in bed.";
+
+            fortunes.Add(new ReceivedFortune { Message = inBed });
         }
 
-        public void Configure(IApplicationBuilder app)
-        {
-            app.UseRouting();
+        var reply = new MessageReply();
 
-            app.UseEndpoints(endpoints =>
+        foreach (var f in fortunes)
+        {
+            reply.Fortunes.Add(new TranslatedFortune
             {
-                endpoints.MapGrpcService<BillboardService>();
-                endpoints.MapGet("/", context =>
-                {
-                    return context.Response.WriteAsync("This server contains a gRPC service");
-                });
+                Message = f.Message,
+                ReceivedTime = f.Received.Ticks
             });
         }
-    }
 
-    public class ReceivedFortune
-    {
-        public string Message { get; set; }
-
-        public DateTimeOffset Received { get; set; } = DateTimeOffset.UtcNow;
-    }
-
-    public class BillboardService : Billboard.Board.BoardBase
-    {
-        public override async Task<MessageReply> ShowMessage(IAsyncStreamReader<MessageRequest> requestStream, ServerCallContext context)
-        {
-            var fortunes = new List<ReceivedFortune>();
-
-            using var tokenSource = new CancellationTokenSource();
-            CancellationToken token = tokenSource.Token;
-
-            await foreach (var request in requestStream.ReadAllAsync(token))
-            {
-                var inBed = request.FortuneCookie[0..^1] + " in bed.";
-
-                fortunes.Add(new ReceivedFortune { Message = inBed });
-            }
-
-            var reply = new MessageReply();
-
-            foreach (var f in fortunes)
-            {
-                reply.Fortunes.Add(new TranslatedFortune
-                {
-                    Message = f.Message,
-                    ReceivedTime = f.Received.Ticks
-                });
-            }
-
-            return reply;
-        }
-    }
-
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
-
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>().
-                    ConfigureKestrel(k =>
-                    {
-                        k.ConfigureEndpointDefaults(options =>
-                        {
-                            options.Protocols = HttpProtocols.Http2;
-                        });
-
-                        k.ListenLocalhost(5500);
-                    }).
-                    UseEnvironment(Environments.Development);
-                });
+        return reply;
     }
 }
