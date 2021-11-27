779c6504198b715bece44ceb78f1cff451280109
Dody Gunawinata <dodyg@silverkeytech.com>
2021-11-27 12:36:57 +0200

    Upgrade to .NET 6

--- projects/grpc/grpc-2/client/src/Program.cs
+++ projects/grpc/grpc-2/client/src/Program.cs
@@ -1,46 +1,30 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.Hosting;
-using System;
-using System.Net.Http;
 using Grpc.Net.Client;
 using Grpc.Core;
-using System.Threading;
 
-namespace GrpcServer
+var app = WebApplication.Create();
+
+app.Run(async context =>
 {
-    public class Startup
+    var channel = GrpcChannel.ForAddress("https://localhost:5500");
+    var client = new Billboard.Board.BoardClient(channel);
+    var result = client.ShowMessage(new Billboard.MessageRequest
     {
-        public void Configure(IApplicationBuilder app)
-        {
-            //Make sure that the grpc-server is run 
-            app.Run(async context =>
-            {
-                //We need this switch because we are connecting to an unsecure server. If the server runs on SSL, there's no need for this switch.
-                AppContext.SetSwitch("System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
-                var channel = GrpcChannel.ForAddress("http://localhost:5500");
-                var client = new Billboard.Board.BoardClient(channel);
-                var result = client.ShowMessage(new Billboard.MessageRequest
-                {
-                    Name = "Johny"
-                });
+        Name = "Johny"
+    });
 
-                context.Response.Headers["Content-Type"] = "text/event-stream";
+    context.Response.Headers["Content-Type"] = "text/event-stream";
 
-                using var tokenSource = new CancellationTokenSource();
-                CancellationToken token = tokenSource.Token;
+    using var tokenSource = new CancellationTokenSource();
+    CancellationToken token = tokenSource.Token;
 
-                var streamReader = result.ResponseStream;
+    var streamReader = result.ResponseStream;
 
-                await foreach (var reply in streamReader.ReadAllAsync(token))
-                {
-                    var displayDate = new DateTime(reply.DisplayTime);
-                    await context.Response.WriteAsync($"Received \"{reply.Message}\" on {displayDate.ToLongTimeString()} \n");
-                    await context.Response.Body.FlushAsync();
-                }
-            });
-        }
+    await foreach (var reply in streamReader.ReadAllAsync(token))
+    {
+        var displayDate = new DateTime(reply.DisplayTime);
+        await context.Response.WriteAsync($"Received \"{reply.Message}\" on {displayDate.ToLongTimeString()} \n");
+        await context.Response.Body.FlushAsync();
     }
+});
 
-}
+app.Run();
--- projects/grpc/grpc-2/server/src/Program.cs
+++ projects/grpc/grpc-2/server/src/Program.cs
@@ -1,80 +1,42 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.Hosting;
-using System.Threading.Tasks;
 using Grpc.Core;
-using System;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.AspNetCore.Server.Kestrel.Core;
 using Billboard;
-using System.Linq;
 
-namespace GrpcServer
+var builder = WebApplication.CreateBuilder();
+builder.Services.AddGrpc();
+
+builder.WebHost.ConfigureKestrel(k =>
 {
-    public class Startup
+    k.ConfigureEndpointDefaults(options =>
     {
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddGrpc();
-        }
+        options.Protocols = HttpProtocols.Http2;
+    });
 
-        public void Configure(IApplicationBuilder app)
-        {
-            app.UseRouting();
+    k.ListenLocalhost(5500, o => o.UseHttps());
+});
 
-            app.UseEndpoints(endpoints =>
+var app = builder.Build();
+
+app.MapGrpcService<BillboardService>();
+app.MapGet("/", () => "This server contains a gRPC service");
+
+app.Run();
+
+public class BillboardService : Billboard.Board.BoardBase
+{
+    public override async Task ShowMessage(MessageRequest request, IServerStreamWriter<MessageReply> responseStream, ServerCallContext context)
+    {
+        foreach (var x in Enumerable.Range(1, 10))
+        {
+            var now = DateTime.UtcNow;
+
+            await responseStream.WriteAsync(new Billboard.MessageReply
             {
-                endpoints.MapGrpcService<BillboardService>();
-                endpoints.MapGet("/", context =>
-                {
-                    return context.Response.WriteAsync("This server contains a gRPC service");
-                });
+                DisplayTime = now.Ticks,
+                Message = $"Hello {request.Name}"
             });
+
+            await Task.Delay(5000);
         }
-    }
-
-    public class BillboardService : Billboard.Board.BoardBase
-    {
-        public override async Task ShowMessage(MessageRequest request, IServerStreamWriter<MessageReply> responseStream, ServerCallContext context)
-        {
-            foreach (var x in Enumerable.Range(1, 10))
-            {
-                var now = DateTime.UtcNow;
-
-                await responseStream.WriteAsync(new Billboard.MessageReply
-                {
-                    DisplayTime = now.Ticks,
-                    Message = $"Hello {request.Name}"
-                });
-
-                await Task.Delay(5000);
-            }
-        }
-    }
-
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
-
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>().
-                    ConfigureKestrel(k =>
-                    {
-                        k.ConfigureEndpointDefaults(options =>
-                        {
-                            options.Protocols = HttpProtocols.Http2;
-                        });
-
-                        k.ListenLocalhost(5500);
-                    }).
-                    UseEnvironment(Environments.Development);
-                });
     }
 }
