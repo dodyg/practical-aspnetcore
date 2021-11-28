a1133abef53c2a7306b0fb30f8466801035c118d
Dody Gunawinata <dodyg@silverkeytech.com>
2021-11-28 11:14:17 +0200

    Upgrade to .NET 6

--- projects/grpc/grpc-5/client/src/Program.cs
+++ projects/grpc/grpc-5/client/src/Program.cs
@@ -1,34 +1,21 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.Hosting;
-using System;
 using Grpc.Net.Client;
 
-namespace GrpcServer
+var app = WebApplication.Create();
+
+//Make sure that the grpc-server is run 
+app.Run(async context =>
 {
-    public class Startup
+    var channel = GrpcChannel.ForAddress("https://localhost:5500"); //check the values at /server project
+    var client = new Billboard.Board.BoardClient(channel);
+    var reply = await client.ShowMessageAsync(new Billboard.MessageRequest
     {
-        public void Configure(IApplicationBuilder app)
-        {
-            //Make sure that the grpc-server is run 
-            app.Run(async context =>
-            {
-                //We need this switch because we are connecting to an unsecure server. If the server runs on SSL, there's no need for this switch.
-                AppContext.SetSwitch("System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
-                var channel = GrpcChannel.ForAddress("http://localhost:5500"); //check the values at /server project
-                var client = new Billboard.Board.BoardClient(channel);
-                var reply = await client.ShowMessageAsync(new Billboard.MessageRequest
-                {
-                    Message = "Hello World",
-                    Sender = "Dody Gunawinata",
-                    Type = Billboard.MessageRequest.Types.MessageType.LongForm
-                });
+        Message = "Hello World",
+        Sender = "Dody Gunawinata",
+        Type = Billboard.MessageRequest.Types.MessageType.LongForm
+    });
 
-                var displayDate = new DateTime(reply.ReceivedTime);
-                await context.Response.WriteAsync($"We sent a message to a gRPC server and  received  the following reply '{reply.Message}' on {displayDate} ");
-            });
-        }
-    }
+    var displayDate = new DateTime(reply.ReceivedTime);
+    await context.Response.WriteAsync($"We sent a message to a gRPC server and  received  the following reply '{reply.Message}' on {displayDate} ");
+});
 
-}
+app.Run();
--- projects/grpc/grpc-5/server/src/Program.cs
+++ projects/grpc/grpc-5/server/src/Program.cs
@@ -1,73 +1,30 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.Hosting;
-using System.Threading.Tasks;
 using Grpc.Core;
-using System;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.AspNetCore.Server.Kestrel.Core;
 
-namespace GrpcServer
+var builder = WebApplication.CreateBuilder();
+builder.Services.AddGrpc();
+builder.WebHost.ConfigureKestrel(k =>
 {
-    public class Startup
+    k.ConfigureEndpointDefaults(options => options.Protocols = HttpProtocols.Http2);
+    k.ListenLocalhost(5500, o => o.UseHttps());
+});
+
+var app = builder.Build();
+app.MapGrpcService<BillboardService>();
+app.MapGet("/", () => "This server contains a gRPC service");
+
+app.Run();
+
+public class BillboardService : Billboard.Board.BoardBase
+{
+    public override Task<Billboard.MessageReply> ShowMessage(Billboard.MessageRequest request, ServerCallContext context)
     {
-        public void ConfigureServices(IServiceCollection services)
+        var message = $"Your {request.Type} with the following content `{request.Message}` has arrived well.";
+        var now = DateTime.UtcNow;
+        return Task.FromResult(new Billboard.MessageReply
         {
-            services.AddGrpc();
-        }
-
-        public void Configure(IApplicationBuilder app)
-        {
-            app.UseRouting();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGrpcService<BillboardService>();
-                endpoints.MapGet("/", context =>
-                {
-                    return context.Response.WriteAsync("This server contains a gRPC service");
-                });
-            });
-        }
-    }
-
-    public class BillboardService : Billboard.Board.BoardBase
-    {
-        public override Task<Billboard.MessageReply> ShowMessage(Billboard.MessageRequest request, ServerCallContext context)
-        {
-            var message = $"Your {request.Type} with the following content `{request.Message}` has arrived well.";
-            var now = DateTime.UtcNow;
-            return Task.FromResult(new Billboard.MessageReply
-            {
-                ReceivedTime = now.Ticks,
-                Message = message
-            });
-        }
-    }
-
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
-
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>().
-                    ConfigureKestrel(k =>
-                    {
-                        k.ConfigureEndpointDefaults(options =>
-                        {
-                            options.Protocols = HttpProtocols.Http2;
-                        });
-
-                        k.ListenLocalhost(5500);
-                    }).
-                    UseEnvironment(Environments.Development);
-                });
+            ReceivedTime = now.Ticks,
+            Message = message
+        });
     }
 }
