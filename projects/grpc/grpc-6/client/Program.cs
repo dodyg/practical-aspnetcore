c8d949a33b9296b8d4d6b6af12134ab75caa2426
Dody Gunawinata <dodyg@silverkeytech.com>
2021-11-28 17:15:06 +0200

    Fix the path definition file

---
+++ projects/grpc/grpc-6/client/Program.cs
@@ -0,0 +1,20 @@
+using Grpc.Net.Client;
+
+var app = WebApplication.Create();
+
+app.Run(async context =>
+{
+    var channel = GrpcChannel.ForAddress("http://localhost:5500"); //check the values at /server project
+    var client = new Billboard.Board.BoardClient(channel);
+    var request = new Billboard.MessageRequest();
+    request.Capabilities.Add(new Billboard.MessageRequest.Types.SuperPower { Name = "Flying", Level = 1 });
+    request.Capabilities.Add(new Billboard.MessageRequest.Types.SuperPower { Name = "Invisibility", Level = 10 });
+    request.Capabilities.Add(new Billboard.MessageRequest.Types.SuperPower { Name = "Speed", Level = 5 });
+
+    var reply = await client.ShowMessageAsync(request);
+
+    var displayDate = new DateTime(reply.ReceivedTime);
+    await context.Response.WriteAsync($"We sent a message to a gRPC server and  received  the following reply \n'\n{reply.Message}' \non {displayDate} ");
+});
+
+app.Run();
