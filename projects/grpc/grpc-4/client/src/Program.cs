84449215db8f8b9bdbc5c808af04f7eaa863ab84
Dody Gunawinata <dodyg@silverkeytech.com>
2021-11-28 11:03:30 +0200

    Upgrade to .NET 6

--- projects/grpc/grpc-4/client/src/Program.cs
+++ projects/grpc/grpc-4/client/src/Program.cs
@@ -1,62 +1,42 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.Hosting;
-using System;
-using System.Net.Http;
 using Grpc.Net.Client;
-using Grpc.Core;
-using System.Threading;
-using System.Collections.Generic;
-using System.Threading.Tasks;
 
-namespace GrpcServer
+var app = WebApplication.Create();
+app.Run(async context =>
 {
-    public class Startup
+    var channel = GrpcChannel.ForAddress("https://localhost:5500"); //check the values at /server project
+    var client = new Billboard.Board.BoardClient(channel);
+
+    context.Response.Headers["Content-Type"] = "text/event-stream";
+
+    using var tokenSource = new CancellationTokenSource();
+    CancellationToken token = tokenSource.Token;
+
+    using var stream = client.ShowMessage(cancellationToken: token);
+
+    bool response = true;
+    int inc = 1;
+    do
     {
-        public void Configure(IApplicationBuilder app)
+        try
         {
-            //Make sure that the grpc-server is run 
-            app.Run(async context =>
+            var delay = checked(1000 * inc);
+
+            await stream.RequestStream.WriteAsync(new Billboard.MessageRequest { Ping = "Ping", DelayTime = delay });
+            inc++;
+            await context.Response.WriteAsync($"Send ping on {DateTimeOffset.UtcNow} \n");
+            response = await stream.ResponseStream.MoveNext(token);
+            if (response)
             {
-                //We need this switch because we are connecting to an unsecure server. If the server runs on SSL, there's no need for this switch.
-                AppContext.SetSwitch("System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
-                var channel = GrpcChannel.ForAddress("http://localhost:5500"); //check the values at /server project
-                var client = new Billboard.Board.BoardClient(channel);
-
-                context.Response.Headers["Content-Type"] = "text/event-stream";
-
-                using var tokenSource = new CancellationTokenSource();
-                CancellationToken token = tokenSource.Token;
-
-                using var stream = client.ShowMessage(cancellationToken: token);
-
-                bool response = true;
-                int inc = 1;
-                do
-                {
-                    try
-                    {
-                        var delay = checked(1000 * inc);
-
-                        await stream.RequestStream.WriteAsync(new Billboard.MessageRequest { Ping = "Ping", DelayTime = delay });
-                        inc++;
-                        await context.Response.WriteAsync($"Send ping on {DateTimeOffset.UtcNow} \n");
-                        response = await stream.ResponseStream.MoveNext(token);
-                        if (response)
-                        {
-                            var result = stream.ResponseStream.Current;
-                            await context.Response.WriteAsync($"Receive {result.Pong} on {DateTimeOffset.UtcNow} \n\n");
-                        }
-                    }
-                    catch (System.OverflowException)
-                    {
-                        inc = 1;
-                    }
-                }
-                while (response);
-            });
+                var result = stream.ResponseStream.Current;
+                await context.Response.WriteAsync($"Receive {result.Pong} on {DateTimeOffset.UtcNow} \n\n");
+            }
+        }
+        catch (System.OverflowException)
+        {
+            inc = 1;
         }
     }
+    while (response);
+});
 
-}
+app.Run();
--- projects/grpc/grpc-4/server/src/Program.cs
+++ projects/grpc/grpc-4/server/src/Program.cs
@@ -1,85 +1,44 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.Hosting;
-using System.Threading.Tasks;
-using Grpc.Core;
-using System;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.AspNetCore.Server.Kestrel.Core;
 using Billboard;
-using System.Linq;
-using System.Collections.Generic;
-using System.Threading;
+using Grpc.Core;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
 
-namespace GrpcServer
+var builder = WebApplication.CreateBuilder();
+builder.Services.AddGrpc();
+
+builder.WebHost.ConfigureKestrel(k =>
 {
-    public class Startup
+    k.ConfigureEndpointDefaults(options => options.Protocols = HttpProtocols.Http2);
+    k.ListenLocalhost(5500, o => o.UseHttps());
+});
+
+var app = builder.Build();
+
+app.MapGrpcService<BillboardService>();
+app.MapGet("/", context =>
+{
+    return context.Response.WriteAsync("This server contains a gRPC service");
+});
+
+app.Run();
+
+public class ReceivedFortune
+{
+    public string Message { get; set; }
+
+    public DateTimeOffset Received { get; set; } = DateTimeOffset.UtcNow;
+}
+
+public class BillboardService : Billboard.Board.BoardBase
+{
+    public override async Task ShowMessage(IAsyncStreamReader<MessageRequest> requestStream, IServerStreamWriter<MessageReply> responseStream, ServerCallContext context)
     {
-        public void ConfigureServices(IServiceCollection services)
+        using var tokenSource = new CancellationTokenSource();
+        CancellationToken token = tokenSource.Token;
+
+        await foreach (var request in requestStream.ReadAllAsync(token))
         {
-            services.AddGrpc();
+            await responseStream.WriteAsync(new MessageReply { Pong = "pong" });
+            await Task.Delay(request.DelayTime);
         }
-
-        public void Configure(IApplicationBuilder app)
-        {
-            app.UseRouting();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGrpcService<BillboardService>();
-                endpoints.MapGet("/", context =>
-                {
-                    return context.Response.WriteAsync("This server contains a gRPC service");
-                });
-            });
-        }
-    }
-
-    public class ReceivedFortune
-    {
-        public string Message { get; set; }
-
-        public DateTimeOffset Received { get; set; } = DateTimeOffset.UtcNow;
-    }
-
-    public class BillboardService : Billboard.Board.BoardBase
-    {
-        public override async Task ShowMessage(IAsyncStreamReader<MessageRequest> requestStream, IServerStreamWriter<MessageReply> responseStream, ServerCallContext context)
-        {
-            using var tokenSource = new CancellationTokenSource();
-            CancellationToken token = tokenSource.Token;
-
-            await foreach (var request in requestStream.ReadAllAsync(token))
-            {
-                await responseStream.WriteAsync(new MessageReply { Pong = "pong" });
-                await Task.Delay(request.DelayTime);
-            }
-        }
-    }
-
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
-
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>().
-                    ConfigureKestrel(k =>
-                    {
-                        k.ConfigureEndpointDefaults(options =>
-                        {
-                            options.Protocols = HttpProtocols.Http2;
-                        });
-
-                        k.ListenLocalhost(5500);
-                    }).
-                    UseEnvironment(Environments.Development);
-                });
     }
 }
