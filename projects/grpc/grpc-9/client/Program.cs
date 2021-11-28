5d2be805f2384ddf6684e6f634078b7c42cdd49f
Dody Gunawinata <dodyg@silverkeytech.com>
2021-11-28 17:48:22 +0200

    Upgrade to .NET 6

--- projects/grpc/grpc-9/client/Program.cs
+++ projects/grpc/grpc-9/client/Program.cs
@@ -1,71 +1,55 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.Hosting;
-using System;
-using System.Net.Http;
 using Grpc.Net.Client;
 using Grpc.Core;
-using System.Threading;
-using System.IO;
 
-namespace GrpcServer
+var app = WebApplication.Create();
+
+app.UseStaticFiles();
+
+//Make sure that the grpc-server is run 
+app.Run(async context =>
 {
-    public class Startup
+    var channel = GrpcChannel.ForAddress("https://localhost:5500"); //check the values at /server project
+    var client = new Billboard.Board.BoardClient(channel);
+    var result = client.ShowMessage(new Google.Protobuf.WellKnownTypes.Empty());
+
+    context.Response.Headers["Content-Type"] = "text/html";
+
+    await context.Response.WriteAsync("<html><body><h1>Kitty Streaming</h1>");
+
+    using var tokenSource = new CancellationTokenSource();
+    CancellationToken token = tokenSource.Token;
+
+    var streamReader = result.ResponseStream;
+
+    //get metdata
+    string path = null;
+    await streamReader.MoveNext(token);
+    var initial = streamReader.Current;
+    if (initial.ImageCase == Billboard.MessageReply.ImageOneofCase.MetaData)
     {
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            app.UseStaticFiles();
-
-            //Make sure that the grpc-server is run 
-            app.Run(async context =>
-            {
-                //We need this switch because we are connecting to an unsecure server. If the server runs on SSL, there's no need for this switch.
-                AppContext.SetSwitch("System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
-
-                var channel = GrpcChannel.ForAddress("http://localhost:5500"); //check the values at /server project
-                var client = new Billboard.Board.BoardClient(channel);
-                var result = client.ShowMessage(new Google.Protobuf.WellKnownTypes.Empty());
-
-                context.Response.Headers["Content-Type"] = "text/html";
-
-                await context.Response.WriteAsync("<html><body><h1>Kitty Streaming</h1>");
-
-                using var tokenSource = new CancellationTokenSource();
-                CancellationToken token = tokenSource.Token;
-
-                var streamReader = result.ResponseStream;
-
-                //get metdata
-                string path = null;
-                await streamReader.MoveNext(token);
-                var initial = streamReader.Current;
-                if (initial.ImageCase == Billboard.MessageReply.ImageOneofCase.MetaData)
-                {
-                    path = Path.Combine(env.WebRootPath, initial.MetaData.FileName);
-                }
-
-                if (path == null)
-                    throw new ApplicationException("Metadata is missing from the server");
-
-                using FileStream file = new FileStream(path, FileMode.Create);
-
-                int position = 0;
-                await foreach (var data in streamReader.ReadAllAsync(token))
-                {
-                    var chunk = data.Chunk;
-                    await file.WriteAsync(chunk.Data.ToByteArray(), 0, chunk.Length);
-                    position += chunk.Length;
-                    await context.Response.WriteAsync(position + " ");
-                }
-
-                file.Close();
-
-                await context.Response.WriteAsync($"<img src=\"{initial.MetaData.FileName}\"/>");
-
-                await context.Response.WriteAsync("</body></html>");
-            });
-        }
+        var env = context.RequestServices.GetService<IWebHostEnvironment>();
+        path = Path.Combine(env.WebRootPath, initial.MetaData.FileName);
     }
 
-}
+    if (path == null)
+        throw new ApplicationException("Metadata is missing from the server");
+
+    using FileStream file = new FileStream(path, FileMode.Create);
+
+    int position = 0;
+    await foreach (var data in streamReader.ReadAllAsync(token))
+    {
+        var chunk = data.Chunk;
+        await file.WriteAsync(chunk.Data.ToByteArray(), 0, chunk.Length);
+        position += chunk.Length;
+        await context.Response.WriteAsync(position + " ");
+    }
+
+    file.Close();
+
+    await context.Response.WriteAsync($"<img src=\"{initial.MetaData.FileName}\"/>");
+
+    await context.Response.WriteAsync("</body></html>");
+});
+
+app.Run();
--- projects/grpc/grpc-9/server/Program.cs
+++ projects/grpc/grpc-9/server/Program.cs
@@ -1,16 +1,6 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.Hosting;
-using System.Threading.Tasks;
 using Grpc.Core;
-using System;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.AspNetCore.Server.Kestrel.Core;
 using Billboard;
-using System.Linq;
-using System.IO;
-using System.Threading;
 using Google.Protobuf;
 
 var builder = WebApplication.CreateBuilder();
@@ -24,102 +14,55 @@ builder.WebHost.ConfigureKestrel(k =>
 
 var app = builder.Build();
 
-namespace GrpcServer
+app.MapGrpcService<BillboardService>();
+app.MapGet("/", () => "This server contains a gRPC service");
+app.Run();
+
+public class BillboardService : Billboard.Board.BoardBase
 {
-    public class Startup
+    IHostEnvironment _env;
+
+    public BillboardService(IHostEnvironment env)
     {
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddGrpc();
-        }
-
-        public void Configure(IApplicationBuilder app)
-        {
-            app.UseRouting();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGrpcService<BillboardService>();
-                endpoints.MapGet("/", context =>
-                {
-                    return context.Response.WriteAsync("This server contains a gRPC service");
-                });
-            });
-        }
+        _env = env;
     }
 
-    public class BillboardService : Billboard.Board.BoardBase
+    public override async Task ShowMessage(Google.Protobuf.WellKnownTypes.Empty _, IServerStreamWriter<MessageReply> responseStream, ServerCallContext context)
     {
+        using var tokenSource = new CancellationTokenSource();
+        CancellationToken token = tokenSource.Token;
 
-        IHostEnvironment _env;
+        var meta = new MessageReply();
+        var metaData = new ImageMetaData();
+        metaData.FileName = "kitty.jpg";
+        metaData.MimeType = "image/jpeg";
+        meta.MetaData = metaData;
 
-        public BillboardService(IHostEnvironment env)
+        await responseStream.WriteAsync(meta);
+
+        var kitty = Path.Combine(_env.ContentRootPath, "kitty.jpg");
+
+        using var reader = new FileStream(kitty, FileMode.Open);
+
+        int chunkSize = 100;
+        int bytesRead;
+        byte[] buffer = new byte[chunkSize];
+        int position = 0;
+        long length = reader.Length;
+        while ((bytesRead = await reader.ReadAsync(buffer, 0, buffer.Length)) > 0)
         {
-            _env = env;
+            var reply = new MessageReply();
+            var chunk = new ImageChunk();
+            chunk.Length = bytesRead;
+            chunk.Data = ByteString.CopyFrom(buffer);
+            reply.Chunk = chunk;
+
+            await responseStream.WriteAsync(reply);
+
+            position += bytesRead;
+            Console.WriteLine(position);
         }
-
-        public override async Task ShowMessage(Google.Protobuf.WellKnownTypes.Empty _, IServerStreamWriter<MessageReply> responseStream, ServerCallContext context)
-        {
-            using var tokenSource = new CancellationTokenSource();
-            CancellationToken token = tokenSource.Token;
-
-            var meta = new MessageReply();
-            var metaData = new ImageMetaData();
-            metaData.FileName = "kitty.jpg";
-            metaData.MimeType = "image/jpeg";
-            meta.MetaData = metaData;
-
-            await responseStream.WriteAsync(meta);
-
-            var kitty = Path.Combine(_env.ContentRootPath, "kitty.jpg");
-
-            using var reader = new FileStream(kitty, FileMode.Open);
-
-            int chunkSize = 100;
-            int bytesRead;
-            byte[] buffer = new byte[chunkSize];
-            int position = 0;
-            long length = reader.Length;
-            while ((bytesRead = await reader.ReadAsync(buffer, 0, buffer.Length)) > 0)
-            {
-                var reply = new MessageReply();
-                var chunk = new ImageChunk();
-                chunk.Length = bytesRead;
-                chunk.Data = ByteString.CopyFrom(buffer);
-                reply.Chunk = chunk;
-
-                await responseStream.WriteAsync(reply);
-
-                position += bytesRead;
-                Console.WriteLine(position);
-            }
-            reader.Close();
-            Console.WriteLine("Done");
-        }
-    }
-
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
-
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>().
-                    ConfigureKestrel(k =>
-                    {
-                        k.ConfigureEndpointDefaults(options =>
-                        {
-                            options.Protocols = HttpProtocols.Http2;
-                        });
-
-                        k.ListenLocalhost(5500);
-                    }).
-                    UseEnvironment(Environments.Development);
-                });
+        reader.Close();
+        Console.WriteLine("Done");
     }
 }
