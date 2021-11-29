3c0c8c3aad2b6410f261bd61ab02225fd8a96ee2
Dody Gunawinata <dodyg@silverkeytech.com>
2021-11-29 16:37:50 +0200

    Upgrade to  .NET 6

--- projects/blazor/DataBinding/Program.cs
+++ projects/blazor/DataBinding/Program.cs
@@ -1,15 +1,7 @@
 ﻿using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using System.Threading.Tasks;
-namespace DataBinding
-{
-    public class Program
-    {
-        public static async Task Main(string[] args)
-        {
-            var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.RootComponents.Add<App>("app");
+using DataBinding;
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("app");
 
-            await builder.Build().RunAsync();
-        }
-    }
-}
+var app = builder.Build();
+await app.RunAsync();
