15a637256e8bf76f4e58272979e7738b80d59815
Dody Gunawinata <dodyg@silverkeytech.com>
2021-11-29 16:44:39 +0200

    Upgrade to .NET 6

--- projects/blazor/HelloWorld/Program.cs
+++ projects/blazor/HelloWorld/Program.cs
@@ -1,16 +1,7 @@
 ﻿using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using System.Threading.Tasks;
+using HelloWorld;
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("app");
 
-namespace HelloWorld
-{
-    public class Program
-    {
-        public static async Task Main(string[] args)
-        {
-            var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.RootComponents.Add<App>("app");
-
-            await builder.Build().RunAsync();
-        }
-    }
-}
+var app = builder.Build();
+await app.RunAsync();
