b114034aa2f6bb23e944f3ddd4971f5e3dfc6b0f
Dody Gunawinata <dodyg@silverkeytech.com>
2021-11-29 10:49:55 +0200

    Upgrade to .NET 6

--- projects/dependency-injection/dependency-injection-1/src/Program.cs
+++ projects/dependency-injection/dependency-injection-1/src/Program.cs
@@ -1,73 +1,53 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore;
-using Microsoft.Extensions.Hosting;
+var builder = WebApplication.CreateBuilder();
 
-namespace DI.One
+builder.Services.AddSingleton(x => new SingletonDate());
+builder.Services.AddTransient(x => new TransientDate());
+builder.Services.AddScoped(x => new ScopedDate());
+
+var app = builder.Build();
+
+app.Use(async (context, next) =>
 {
-    public class Startup
-    {
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddSingleton(x => new SingletonDate());
-            services.AddTransient(x => new TransientDate());
-            services.AddScoped(x => new ScopedDate());
-        }
+    var single = context.RequestServices.GetService<SingletonDate>();
+    var scoped = context.RequestServices.GetService<ScopedDate>();
+    var transient = context.RequestServices.GetService<TransientDate>();
 
-        public void Configure(IApplicationBuilder app)
-        {
-            //These are the three default services available at Configure
+    await context.Response.WriteAsync("Open this page in two tabs \n");
+    await context.Response.WriteAsync("Keep refreshing and you will see the three different DI behaviors\n");
+    await context.Response.WriteAsync("----------------------------------\n");
+    await context.Response.WriteAsync($"Singleton : {single.Date.ToString("MM/dd/yyyy hh:mm:ss.fff tt")}\n");
+    await context.Response.WriteAsync($"Scoped: {scoped.Date.ToString("MM/dd/yyyy hh:mm:ss.fff tt")}\n");
+    await context.Response.WriteAsync($"Transient: {transient.Date.ToString("MM/dd/yyyy hh:mm:ss.fff tt")}\n");
+    await next.Invoke();
+});
 
-            app.Use(async (context, next) =>
-            {
-                var single = context.RequestServices.GetService<SingletonDate>();
-                var scoped = context.RequestServices.GetService<ScopedDate>();
-                var transient = context.RequestServices.GetService<TransientDate>();
+app.Run(async (context) =>
+{
+    await Task.Delay(100);//delay for 100 ms
 
-                await context.Response.WriteAsync("Open this page in two tabs \n");
-                await context.Response.WriteAsync("Keep refreshing and you will see the three different DI behaviors\n");
-                await context.Response.WriteAsync("----------------------------------\n");
-                await context.Response.WriteAsync($"Singleton : {single.Date.ToString("MM/dd/yyyy hh:mm:ss.fff tt")}\n");
-                await context.Response.WriteAsync($"Scoped: {scoped.Date.ToString("MM/dd/yyyy hh:mm:ss.fff tt")}\n");
-                await context.Response.WriteAsync($"Transient: {transient.Date.ToString("MM/dd/yyyy hh:mm:ss.fff tt")}\n");
-                await next.Invoke();
-            });
+    var single = context.RequestServices.GetService<SingletonDate>();
+    var scoped = context.RequestServices.GetService<ScopedDate>();
+    var transient = context.RequestServices.GetService<TransientDate>();
 
-            app.Run(async (context) =>
-            {
-               await Task.Delay(100);//delay for 100 ms
+    await context.Response.WriteAsync("----------------------------------\n");
+    await context.Response.WriteAsync($"Singleton : {single.Date.ToString("MM/dd/yyyy hh:mm:ss.fff tt")}\n");
+    await context.Response.WriteAsync($"Scoped: {scoped.Date.ToString("MM/dd/yyyy hh:mm:ss.fff tt")}\n");
+    await context.Response.WriteAsync($"Transient: {transient.Date.ToString("MM/dd/yyyy hh:mm:ss.fff tt")}\n");
+});
 
-                var single = context.RequestServices.GetService<SingletonDate>();
-                var scoped = context.RequestServices.GetService<ScopedDate>();
-                var transient = context.RequestServices.GetService<TransientDate>();
-                
-                await context.Response.WriteAsync("----------------------------------\n");
-                await context.Response.WriteAsync($"Singleton : {single.Date.ToString("MM/dd/yyyy hh:mm:ss.fff tt")}\n");
-                await context.Response.WriteAsync($"Scoped: {scoped.Date.ToString("MM/dd/yyyy hh:mm:ss.fff tt")}\n");
-                await context.Response.WriteAsync($"Transient: {transient.Date.ToString("MM/dd/yyyy hh:mm:ss.fff tt")}\n");
-            });
-        }
-    }
+app.Run();
 
-    public class SingletonDate
-    {
-        public DateTime Date { get; set; } = DateTime.Now;
-    }
+public class SingletonDate
+{
+    public DateTime Date { get; set; } = DateTime.Now;
+}
 
-    public class TransientDate
-    {
-        public DateTime Date { get; set; } = DateTime.Now;
-    }
+public class TransientDate
+{
+    public DateTime Date { get; set; } = DateTime.Now;
+}
 
-    public class ScopedDate
-    {
-        public DateTime Date { get; set; } = DateTime.Now;
-    }
-
-
+public class ScopedDate
+{
+    public DateTime Date { get; set; } = DateTime.Now;
 }
