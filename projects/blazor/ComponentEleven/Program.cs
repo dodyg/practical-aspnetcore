0c516f3a88cfba093a9384c396a6ce72d2079741
Dody Gunawinata <dodyg@silverkeytech.com>
2021-11-29 16:23:44 +0200

    Upgrade to .NET 6

--- projects/blazor/ComponentEleven/Program.cs
+++ projects/blazor/ComponentEleven/Program.cs
@@ -1,16 +1,7 @@
 ﻿using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using System.Threading.Tasks;
+using ComponentEleven;
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("app");
 
-namespace ComponentEleven
-{
-    public class Program
-    {
-        public static async Task Main(string[] args)
-        {
-            var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.RootComponents.Add<App>("app");
-
-            await builder.Build().RunAsync();
-        }
-    }
-}
+var app = builder.Build();
+await app.RunAsync();
