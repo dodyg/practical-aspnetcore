95a6f60d524970b49792f309e86eb23551a511e2
Dody Gunawinata <dodyg@silverkeytech.com>
2021-11-29 16:21:12 +0200

    Upgrade to .NET 6

--- projects/blazor/ComponentTen/Program.cs
+++ projects/blazor/ComponentTen/Program.cs
@@ -1,15 +1,8 @@
 ﻿using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using System.Threading.Tasks;
-namespace ComponentTen
-{
-    public class Program
-    {
-        public static async Task Main(string[] args)
-        {
-            var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.RootComponents.Add<App>("app");
+using ComponentTen;
 
-            await builder.Build().RunAsync();
-        }
-    }
-}
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("app");
+
+var app = builder.Build();
+await app.RunAsync();
