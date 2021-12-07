a7d418402bc9ca82c65015eb2a7ae7d9e940659c
Dody Gunawinata <dodyg@silverkeytech.com>
2021-12-07 11:00:12 +0200

    Upgrade to .NET 6

--- projects/file-provider/serve-static-files-5/Program.cs
+++ projects/file-provider/serve-static-files-5/Program.cs
@@ -1,76 +1,49 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Http;
-using System.Collections.Generic;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.StaticFiles;
-using Microsoft.Extensions.Hosting;
 
-namespace PracticalAspNetCore
+var app = WebApplication.Create();
+app.UseStaticFiles();
+app.UseDirectoryBrowser(new DirectoryBrowserOptions
 {
-    public class DirectoryFormatter : IDirectoryFormatter
-    {
-        public async Task GenerateContentAsync(HttpContext context, IEnumerable<Microsoft.Extensions.FileProviders.IFileInfo> contents)
-        {
-            context.Response.ContentType = "text/html";
+    Formatter = new DirectoryFormatter()
+});
 
-            await context.Response.WriteAsync(@"
+app.Run();
+
+public class DirectoryFormatter : IDirectoryFormatter
+{
+    public async Task GenerateContentAsync(HttpContext context, IEnumerable<Microsoft.Extensions.FileProviders.IFileInfo> contents)
+    {
+        context.Response.ContentType = "text/html";
+
+        await context.Response.WriteAsync(@"
 <html>
 <head>
-    <link href=""https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css"" rel=""stylesheet"" integrity=""sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ"" crossorigin=""anonymous"">
+    <link href=""https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"" rel=""stylesheet"" integrity=""sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"" crossorigin=""anonymous"">
 </head>
 <body>
 <div class=""container"">
 ");
-            await context.Response.WriteAsync("\n");
+        await context.Response.WriteAsync("\n");
 
-            foreach (var c in contents)
+        foreach (var c in contents)
+        {
+            await context.Response.WriteAsync($"<div class=\"row justify-content-center\">\n");
+
+            if (c.IsDirectory)
             {
-                await context.Response.WriteAsync($"<div class=\"row justify-content-center\">\n");
-
-                if (c.IsDirectory)
-                {
-                    await context.Response.WriteAsync($"<div class=\"col\"><strong>Directory <a href=\"{c.Name}\">{c.Name}</a></strong></div>\n");
-                }
+                await context.Response.WriteAsync($"<div class=\"col\"><strong>Directory <a href=\"{c.Name}\">{c.Name}</a></strong></div>\n");
+            }
+            else
+            {
+                if (c.Name.Contains(".png") || c.Name.Contains(".jpg"))
+                    await context.Response.WriteAsync($"<div class=\"col\"><img src=\"{c.Name}\" class=\"img-thumbnail\"/></div>\n");
                 else
-                {
-                    if (c.Name.Contains(".png") || c.Name.Contains(".jpg"))
-                        await context.Response.WriteAsync($"<div class=\"col\"><img src=\"{c.Name}\" class=\"img-thumbnail\"/></div>\n");
-                    else
-                        await context.Response.WriteAsync($"<div class=\"col\"><a href=\"{c.Name}\">{c.Name}</a></div>\n");
-                }
-
-                await context.Response.WriteAsync("</div>\n");
+                    await context.Response.WriteAsync($"<div class=\"col\"><a href=\"{c.Name}\">{c.Name}</a></div>\n");
             }
 
-            await context.Response.WriteAsync("\n</div></body></html>");
-        }
-    }
-
-    public class Startup
-    {
-        public void Configure(IApplicationBuilder app)
-        {
-            app.UseStaticFiles();
-            app.UseDirectoryBrowser(new DirectoryBrowserOptions
-            {
-                Formatter = new DirectoryFormatter()
-            });
-        }
-    }
-
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
+            await context.Response.WriteAsync("</div>\n");
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                    webBuilder.UseStartup<Startup>()
-                );
-
+        await context.Response.WriteAsync("\n</div></body></html>");
     }
 }
