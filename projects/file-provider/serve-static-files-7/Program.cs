31fdd17bf270e8daef05c76efd578a95d0870dee
Dody Gunawinata <dodyg@silverkeytech.com>
2021-12-07 11:03:33 +0200

    Upgrade .NET 6

--- projects/file-provider/serve-static-files-7/Program.cs
+++ projects/file-provider/serve-static-files-7/Program.cs
@@ -1,39 +1,20 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.DependencyInjection;
-using System.IO;
 using Microsoft.Extensions.FileProviders;
-using Microsoft.Extensions.Hosting;
 
-namespace PracticalAspNetCore
+var app = WebApplication.Create();
+app.UseStaticFiles(); // By default this will server files out of wwwroot folder
+app.UseStaticFiles(new StaticFileOptions()
 {
-    public class Startup
-    {
-        IWebHostEnvironment _env;
+    //The PhysialFileProvider will take any valid path. This way you can 
+    //specify which folders will server the static files
+    FileProvider = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), "wwwroot2")),
+    RequestPath = new PathString("/2")
+});
 
-        public Startup(IWebHostEnvironment env)
-        {
-            _env = env;
-        }
+app.Run(async context =>
+{
+    context.Response.Headers.Add("content-type", "text/html");
 
-        public void Configure(IApplicationBuilder app)
-        {
-            app.UseStaticFiles(); // By default this will server files out of wwwroot folder
-
-            app.UseStaticFiles(new StaticFileOptions()
-            {
-                //The PhysialFileProvider will take any valid path. This way you can 
-                //specify which folders will server the static files
-                FileProvider =  new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), "wwwroot2")),
-                RequestPath = new PathString("/2")
-            });
-
-            app.Run(async context =>
-            {
-                context.Response.Headers.Add("content-type", "text/html");
-
-                await context.Response.WriteAsync(@"
+    await context.Response.WriteAsync(@"
                 <html>
                 <body>
                     From wwwroot</br>
@@ -44,9 +25,6 @@ namespace PracticalAspNetCore
                 </body>
                 </html>
                 ");
-            });
-        }
-    }
+});
 
-
-}
+app.Run();
