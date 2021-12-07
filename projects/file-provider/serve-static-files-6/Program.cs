b4debc8623cc720bd67643cf79f60441ca4661f9
Dody Gunawinata <dodyg@silverkeytech.com>
2021-12-07 11:02:07 +0200

    Upgrade to .NET 6

--- projects/file-provider/serve-static-files-6/Program.cs
+++ projects/file-provider/serve-static-files-6/Program.cs
@@ -1,39 +1,13 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.Hosting;
-
-namespace PracticalAspNetCore
+var app = WebApplication.Create();
+app.UseStaticFiles();
+app.UseDirectoryBrowser(new DirectoryBrowserOptions
 {
-    public class Startup
-    {
-        public void Configure(IApplicationBuilder app)
-        {
-            app.UseStaticFiles();
-            app.UseDirectoryBrowser(new DirectoryBrowserOptions
-            {
-                RequestPath = new PathString("/browse")
-            });
+    RequestPath = new PathString("/browse")
+});
 
-            app.Run(async context =>
-            {
-                await context.Response.WriteAsync("<a href=\"browse\">browse directory</a>");
-            });
-        }
-    }
+app.Run(async context =>
+{
+    await context.Response.WriteAsync("<html><body><a href=\"browse\">browse directory</a></body></html>");
+});
 
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
-
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                    webBuilder.UseStartup<Startup>()
-                );
-
-    }
-}
+app.Run();
