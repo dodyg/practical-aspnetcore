25a8200a05e23d31ec310e9a9801646fb9bde3db
Dody Gunawinata <dodyg@silverkeytech.com>
2021-12-07 10:57:54 +0200

    Upgrade to .NET 6

--- projects/file-provider/serve-static-files-4/Program.cs
+++ projects/file-provider/serve-static-files-4/Program.cs
@@ -1,64 +1,37 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Http;
-using System.Collections.Generic;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.StaticFiles;
-using Microsoft.Extensions.Hosting;
 
-namespace PracticalAspNetCore
+var app = WebApplication.Create();
+app.UseStaticFiles();
+app.UseDirectoryBrowser(new DirectoryBrowserOptions
 {
-    public class DirectoryFormatter : IDirectoryFormatter
-    {
-        public async Task GenerateContentAsync(HttpContext context, IEnumerable<Microsoft.Extensions.FileProviders.IFileInfo> contents)
-        {
-            context.Response.ContentType = "text/html";
+    Formatter = new DirectoryFormatter()
+});
 
-            await context.Response.WriteAsync(@"
+app.Run();
+
+public class DirectoryFormatter : IDirectoryFormatter
+{
+    public async Task GenerateContentAsync(HttpContext context, IEnumerable<Microsoft.Extensions.FileProviders.IFileInfo> contents)
+    {
+        context.Response.ContentType = "text/html";
+
+        await context.Response.WriteAsync(@"
 <html>
 <head>
-    <link href=""https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css"" rel=""stylesheet"" integrity=""sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ"" crossorigin=""anonymous"">
+    <link href=""https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"" rel=""stylesheet"" integrity=""sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"" crossorigin=""anonymous"">
 </head>
 <body>
 <div class=""container"">
 <div class=""row"">
 ");
-            foreach (var c in contents)
-            {
-                if (c.Name.Contains(".png") || c.Name.Contains(".jpg"))
-                    await context.Response.WriteAsync($@"<div class=""col""><img src=""{c.Name}""/></div>");
-                else
-                    await context.Response.WriteAsync($@"<div class=""col"">{c.Name}</div>");
-            }
-
-            await context.Response.WriteAsync("</div></div></body></html>");
-        }
-    }
-
-    public class Startup
-    {
-        public void Configure(IApplicationBuilder app)
+        foreach (var c in contents)
         {
-            app.UseStaticFiles();
-            app.UseDirectoryBrowser(new DirectoryBrowserOptions
-            {
-                Formatter = new DirectoryFormatter()
-            });
-        }
-    }
-
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
+            if (c.Name.Contains(".png") || c.Name.Contains(".jpg"))
+                await context.Response.WriteAsync($@"<div class=""col""><img src=""{c.Name}""/></div>");
+            else
+                await context.Response.WriteAsync($@"<div class=""col"">{c.Name}</div>");
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                    webBuilder.UseStartup<Startup>()
-                );
-
+        await context.Response.WriteAsync("</div></div></body></html>");
     }
 }
