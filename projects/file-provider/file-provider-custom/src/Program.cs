7105429c88f4cf099d8b673ba6ef963260a7703c
Dody Gunawinata <dodyg@silverkeytech.com>
2021-12-07 10:41:43 +0200

    Upgrade to .NET 6

--- projects/file-provider/file-provider-custom/src/Program.cs
+++ projects/file-provider/file-provider-custom/src/Program.cs
@@ -1,111 +1,86 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.DependencyInjection;
-using System.IO;
 using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Primitives;
-using System;
 using System.Collections;
-using System.Collections.Generic;
 using System.Text;
-using Microsoft.Extensions.Hosting;
 
-namespace PracticalAspNetCore
+var builder = WebApplication.CreateBuilder();
+builder.Environment.ContentRootFileProvider = new CustomFileProvider();
+var app = builder.Build();
+
+app.Run(async context =>
 {
-    public class CustomDirectoryContents : IDirectoryContents
+    context.Response.Headers.Add("content-type", "text/html");
+
+    using (var stream = app.Environment.ContentRootFileProvider.GetFileInfo("").CreateReadStream())
     {
-        readonly IEnumerable<IFileInfo> _entries;
+        var reader = new StreamReader(stream);
+        await context.Response.WriteAsync(reader.ReadToEnd());
+    }
+});
 
-        public CustomDirectoryContents(IEnumerable<IFileInfo> files)
-        {
-            _entries = files;
-        }
+app.Run();
 
-        public bool Exists => true;
-        public IEnumerator<IFileInfo> GetEnumerator() => _entries.GetEnumerator();
-        IEnumerator IEnumerable.GetEnumerator() => _entries.GetEnumerator();
+public class CustomDirectoryContents : IDirectoryContents
+{
+    readonly IEnumerable<IFileInfo> _entries;
+
+    public CustomDirectoryContents(IEnumerable<IFileInfo> files)
+    {
+        _entries = files;
     }
 
-    public class AlwaysTheSameFile : IFileInfo
+    public bool Exists => true;
+    public IEnumerator<IFileInfo> GetEnumerator() => _entries.GetEnumerator();
+    IEnumerator IEnumerable.GetEnumerator() => _entries.GetEnumerator();
+}
+
+public class AlwaysTheSameFile : IFileInfo
+{
+    public bool Exists => true;
+    public long Length { get; }
+    public string PhysicalPath => null;
+    public string Name { get; }
+    public DateTimeOffset LastModified { get; }
+    public bool IsDirectory => false;
+    public Stream CreateReadStream()
     {
-        public bool Exists => true;
-        public long Length { get; }
-        public string PhysicalPath => null;
-        public string Name { get; }
-        public DateTimeOffset LastModified { get; }
-        public bool IsDirectory => false;
-        public Stream CreateReadStream()
-        {
-            var text = @"
+        var text = @"
             Dhritarashtra said: O Sanjaya, after my sons and the sons of Pandu assembled in the place of pilgrimage at Kurukshetra, desiring to fight, what did they do?
             ";
 
-            return new MemoryStream(Encoding.UTF8.GetBytes(text));
-        }
+        return new MemoryStream(Encoding.UTF8.GetBytes(text));
     }
+}
 
-    //https://docs.microsoft.com/en-us/aspnet/core/api/microsoft.extensions.fileproviders.ifileinfo#Microsoft_Extensions_FileProviders_IFileInfo
-    public class CustomDirectory : IFileInfo
+//https://docs.microsoft.com/en-us/aspnet/core/api/microsoft.extensions.fileproviders.ifileinfo#Microsoft_Extensions_FileProviders_IFileInfo
+public class CustomDirectory : IFileInfo
+{
+    public bool Exists => true;
+    public long Length => -1; // read the offical doc        
+    public string PhysicalPath => null; // read the offical doc
+    public string Name { get; }
+    public DateTimeOffset LastModified { get; }
+    public bool IsDirectory => true;
+
+    public Stream CreateReadStream()
     {
-        public bool Exists => true;
-        public long Length => -1; // read the offical doc        
-        public string PhysicalPath => null; // read the offical doc
-        public string Name { get; }
-        public DateTimeOffset LastModified { get; }
-        public bool IsDirectory => true;
-
-        public Stream CreateReadStream()
-        {
-            throw new InvalidOperationException("Create Stream is not applicable for directory");
-        }
+        throw new InvalidOperationException("Create Stream is not applicable for directory");
     }
+}
 
-    public class CustomFileProvider : IFileProvider
+public class CustomFileProvider : IFileProvider
+{
+    public IDirectoryContents GetDirectoryContents(string subpath)
     {
-        public IDirectoryContents GetDirectoryContents(string subpath)
-        {
-            var list = new List<AlwaysTheSameFile>
+        var list = new List<AlwaysTheSameFile>
             {
                 new AlwaysTheSameFile()
             };
 
-            var contents = new CustomDirectoryContents(list);
-            return contents;
-        }
-
-        public IFileInfo GetFileInfo(string subpath) => new AlwaysTheSameFile();
-        public IChangeToken Watch(string filter) => NullChangeToken.Singleton;
+        var contents = new CustomDirectoryContents(list);
+        return contents;
     }
 
-    public class Startup
-    {
-        IWebHostEnvironment _env;
-
-        public Startup(IWebHostEnvironment env)
-        {
-            _env = env;
-        }
-
-        public void ConfigureServices(IServiceCollection services)
-        {
-            _env.ContentRootFileProvider = new CustomFileProvider();
-        }
-
-        public void Configure(IApplicationBuilder app)
-        {
-            app.Run(async context =>
-            {
-                context.Response.Headers.Add("content-type", "text/html");
-
-                using (var stream = _env.ContentRootFileProvider.GetFileInfo("").CreateReadStream())
-                {
-                    var reader = new StreamReader(stream);
-                    await context.Response.WriteAsync(reader.ReadToEnd());
-                }
-            });
-        }
-    }
-
-
+    public IFileInfo GetFileInfo(string subpath) => new AlwaysTheSameFile();
+    public IChangeToken Watch(string filter) => NullChangeToken.Singleton;
 }
