c4401ed785dfb8fc866a00a72b4f69269ca791de
Dody Gunawinata <dodyg@silverkeytech.com>
2021-12-07 10:53:17 +0200

    Upgrade .NET 6

--- projects/file-provider/serve-static-files-2/Program.cs
+++ projects/file-provider/serve-static-files-2/Program.cs
@@ -1,33 +1,4 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Http;
-using System.IO;
-using Microsoft.AspNetCore;
-using Microsoft.Extensions.Hosting;
-
-namespace ServeStaticFiles
-{
-    public class Startup
-    {
-        public void Configure(IApplicationBuilder app)
-        {
-            app.UseStaticFiles();
-            app.UseDirectoryBrowser();
-        }
-    }
-
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
-
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                    webBuilder.UseStartup<Startup>()
-                );
-
-    }
-}
+var app = WebApplication.Create();
+app.UseStaticFiles();
+app.UseDirectoryBrowser();
+app.Run();
