5558d279596c3f4493fb99569fa2fde75fc781e5
Dody Gunawinata <dodyg@silverkeytech.com>
2021-12-08 09:45:06 +0200

    upgradet to .NET 6

--- projects/json/json/Program.cs
+++ projects/json/json/Program.cs
@@ -1,65 +1,39 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.DependencyInjection;
-using System.Text.Json;
-using Microsoft.Net.Http.Headers;
-using System;
-using System.Collections.Generic;
-
-namespace PracticalAspNetCore
+var app = WebApplication.Create();
+app.MapGet("/",  () =>
 {
-    public class Person
+    var payload = new Person
     {
-        public string Name { get; set; }
-
-        public int Age { get; set; }
-
-        public bool IsMarried { get; set; }
-
-        public DateTimeOffset CurrentTime { get; set; }
-
-        public bool? IsWorking { get; set; }
-
-        public Dictionary<string, bool> Characters { get; set; }
-    }
-
-
-    public class Startup
-    {
-        public void ConfigureServices(IServiceCollection services)
+        Name = "Annie",
+        Age = 33,
+        IsMarried = false,
+        CurrentTime = DateTimeOffset.UtcNow,
+        Characters = new Dictionary<string, bool>
         {
-            services.AddControllersWithViews();
-        }
+                    {"Funny" , true},
+                    {"Feisty" , true},
+                    {"Brilliant" , true},
+                    {"FOMA", false}
+        },
+        IsWorking = true
+    };
 
-        public void Configure(IApplicationBuilder app)
-        {
-            app.UseRouting();
-            app.UseEndpoints(route =>
-            {
-                route.MapGet("/", async context =>
-                {
-                    var payload = new Person
-                    {
-                        Name = "Annie",
-                        Age = 33,
-                        IsMarried = false,
-                        CurrentTime = DateTimeOffset.UtcNow,
-                        Characters = new Dictionary<string, bool>
-                        {
-                            {"Funny" , true},
-                            {"Feisty" , true},
-                            {"Brilliant" , true},
-                            {"FOMA", false}
-                        },
-                        IsWorking = true
-                    };
+    return Results.Json(payload);
+});
 
-                    context.Response.Headers.Add(HeaderNames.ContentType, "application/json");
-                    await JsonSerializer.SerializeAsync(context.Response.Body, payload, typeof(Person));
-                });
-            });
-        }
-    }
+app.Run();
 
+public class Person
+{
+    public string Name { get; set; }
+
+    public int Age { get; set; }
+
+    public bool IsMarried { get; set; }
+
+    public DateTimeOffset CurrentTime { get; set; }
+
+    public bool? IsWorking { get; set; }
+
+    public Dictionary<string, bool> Characters { get; set; }
 }
+
