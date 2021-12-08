aff4c02cac27fe95716be174dbf5824911328853
Dody Gunawinata <dodyg@silverkeytech.com>
2021-12-08 09:56:34 +0200

    Upgrade to .NET 6

--- projects/json/json-4/Program.cs
+++ projects/json/json-4/Program.cs
@@ -1,76 +1,53 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.DependencyInjection;
 using System.Text.Json;
-using Microsoft.Net.Http.Headers;
-using System;
-using System.Collections.Generic;
 using System.Text.Json.Serialization;
 
-namespace PracticalAspNetCore
+var app = WebApplication.Create();
+app.MapGet("/", () =>
 {
-    public class Person
+    var payload = new Person
     {
-        //Setting this property name explicitly means that the JsonSerializerOptions.PropertyNamingPolicy won't apply
-        [JsonPropertyName("FullName")]
-        public string Name { get; set; }
+        Name = "Annie",
+        Age = 33,
+        IsMarried = false,
+        CurrentTime = DateTimeOffset.UtcNow,
+        Characters = new Dictionary<string, bool>
+        {
+            {"Funny" , true},
+            {"Feisty" , true},
+            {"Brilliant" , true},
+            {"FOMA", false}
+        },
+        IsWorking = true
+    };
 
-        public int Age { get; set; }
-
-        public bool IsMarried { get; set; }
-
-        public DateTimeOffset CurrentTime { get; set; }
-
-        [JsonIgnore] // Do not serialize this property
-        public bool? IsWorking { get; set; }
-
-        public Dictionary<string, bool> Characters { get; set; }
-    }
-
-    public class Startup
+    var options = new JsonSerializerOptions
     {
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddControllersWithViews();
-        }
+        WriteIndented = true,
+        DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull,
+        PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+        DictionaryKeyPolicy = JsonNamingPolicy.CamelCase
+    };
 
-        public void Configure(IApplicationBuilder app)
-        {
-            app.UseRouting();
-            app.UseEndpoints(route =>
-            {
-                route.MapGet("/", async context =>
-                {
-                    var payload = new Person
-                    {
-                        Name = "Annie",
-                        Age = 33,
-                        IsMarried = false,
-                        CurrentTime = DateTimeOffset.UtcNow,
-                        Characters = new Dictionary<string, bool>
-                        {
-                            {"Funny" , true},
-                            {"Feisty" , true},
-                            {"Brilliant" , true},
-                            {"FOMA", false}
-                        },
-                        IsWorking = true
-                    };
+    return Results.Json(payload, options);
+});
 
-                    var options = new JsonSerializerOptions
-                    {
-                        WriteIndented = true,
-                        IgnoreNullValues = true,
-                        PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
-                        DictionaryKeyPolicy = JsonNamingPolicy.CamelCase
-                    };
+app.Run();
 
-                    context.Response.Headers.Add(HeaderNames.ContentType, "application/json");
-                    await JsonSerializer.SerializeAsync(context.Response.Body, payload, typeof(Person), options);
-                });
-            });
-        }
-    }
+public class Person
+{
+    //Setting this property name explicitly means that the JsonSerializerOptions.PropertyNamingPolicy won't apply
+    [JsonPropertyName("FullName")]
+    public string Name { get; set; }
 
+    public int Age { get; set; }
+
+    public bool IsMarried { get; set; }
+
+    public DateTimeOffset CurrentTime { get; set; }
+
+    [JsonIgnore] // Do not serialize this property
+    public bool? IsWorking { get; set; }
+
+    public Dictionary<string, bool> Characters { get; set; }
 }
+
