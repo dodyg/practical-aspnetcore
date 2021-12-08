b7bc044af92026963176186ce9df0dac9f1ebedb
Dody Gunawinata <dodyg@silverkeytech.com>
2021-12-08 09:50:01 +0200

    Upgrade to .NET 6

--- projects/json/json-2/Program.cs
+++ projects/json/json-2/Program.cs
@@ -1,75 +1,49 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.AspNetCore.Mvc;
 using System.Text.Json;
-using Microsoft.Net.Http.Headers;
-using System;
-using System.Collections.Generic;
+using System.Text.Json.Serialization;
 
-namespace PracticalAspNetCore
+var app = WebApplication.Create();
+
+app.MapGet("/", () =>
 {
-    public class Person
-    {
-        public string Name { get; set; }
-
-        public int Age { get; set; }
-
-        public bool IsMarried { get; set; }
-
-        public DateTimeOffset CurrentTime { get; set; }
-
-        public bool? IsWorking { get; set; }
-
-        public Dictionary<string, bool> Characters { get; set; }
-    }
-
-    public class Startup
-    {
-        public void ConfigureServices(IServiceCollection services)
+    var payload = new Person
+        // We won't assign any value to the property IsWorking. 
         {
-            services.AddControllersWithViews();
-        }
-
-        public void Configure(IApplicationBuilder app)
+        Name = "Annie",
+        Age = 33,
+        IsMarried = false,
+        CurrentTime = DateTimeOffset.UtcNow,
+        Characters = new Dictionary<string, bool>
         {
-            app.UseRouting();
-            app.UseEndpoints(route =>
-            {
-                route.MapGet("/", async context =>
-                {
-                    var payload = new Person
-                    // We won't assign any value to the property IsWorking. 
-                    {
-                        Name = "Annie",
-                        Age = 33,
-                        IsMarried = false,
-                        CurrentTime = DateTimeOffset.UtcNow,
-                        Characters = new Dictionary<string, bool>
-                        {
-                            {"Funny" , true},
-                            {"Feisty" , true},
-                            {"Brilliant" , true},
-                            {"FOMA", false}
-                        }
-                    };
-
-                    var options = new JsonSerializerOptions
-                    {
-                        WriteIndented = true,
-                        IgnoreNullValues = true,
-                        PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
-                        DictionaryKeyPolicy = JsonNamingPolicy.CamelCase
-                    };
-
-                    context.Response.Headers.Add(HeaderNames.ContentType, "application/json");
-                    await JsonSerializer.SerializeAsync(context.Response.Body, payload, typeof(Person), options);
-                });
-            });
+                {"Funny" , true},
+                {"Feisty" , true},
+                {"Brilliant" , true},
+                {"FOMA", false}
         }
-    }
+    };
 
+    var options = new JsonSerializerOptions
+    {
+        WriteIndented = true,
+        DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull,
+        PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+        DictionaryKeyPolicy = JsonNamingPolicy.CamelCase
+    };
+
+    return Results.Json(payload, options);
+});
+
+app.Run();
+public class Person
+{
+    public string Name { get; set; }
+
+    public int Age { get; set; }
+
+    public bool IsMarried { get; set; }
+
+    public DateTimeOffset CurrentTime { get; set; }
+
+    public bool? IsWorking { get; set; }
+
+    public Dictionary<string, bool> Characters { get; set; }
 }
