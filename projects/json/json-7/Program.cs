ebf7f1c8db479538a48c8405404d0c3c31da3653
Dody Gunawinata <dodyg@silverkeytech.com>
2021-12-08 10:11:38 +0200

    Upgrade to .NET 6

--- projects/json/json-7/Program.cs
+++ projects/json/json-7/Program.cs
@@ -1,150 +1,127 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.DependencyInjection;
 using System.Text.Json;
-using Microsoft.Net.Http.Headers;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 
-namespace PracticalAspNetCore
+var app = WebApplication.Create();
+app.MapGet("/", () =>
 {
-    // Implementation from https://github.com/JamesNK/Newtonsoft.Json/blob/cdf10151d507d497a3f9a71d36d544b199f73435/Src/Newtonsoft.Json/Utilities/StringUtils.cs
-    // Modified to use span
-    internal static class StringUtils
+    var payload = new
     {
-        internal enum SnakeCaseState
+        Name = "Annie",
+        Age = 33,
+        IsMarried = false,
+        CurrentTime = DateTimeOffset.UtcNow,
+        Characters = new
         {
-            Start,
-            Lower,
-            Upper,
-            NewWord
+            Funny = true,
+            Feisty = true,
+            Brilliant = true,
+            FOMA = false,
+            HighEmpathy = true
+        }
+    };
+
+    var options = new JsonSerializerOptions
+    {
+        WriteIndented = true,
+        DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull,
+        PropertyNamingPolicy = new SnakeCaseNamingPolicy(),
+        DictionaryKeyPolicy = new SnakeCaseNamingPolicy()
+    };
+
+    return Results.Json(payload, options);
+});
+
+app.Run();
+
+
+// Implementation from https://github.com/JamesNK/Newtonsoft.Json/blob/cdf10151d507d497a3f9a71d36d544b199f73435/Src/Newtonsoft.Json/Utilities/StringUtils.cs
+// Modified to use span
+internal static class StringUtils
+{
+    internal enum SnakeCaseState
+    {
+        Start,
+        Lower,
+        Upper,
+        NewWord
+    }
+
+    public static string ToSnakeCase(string s)
+    {
+        if (string.IsNullOrEmpty(s))
+        {
+            return s;
         }
 
-        public static string ToSnakeCase(string s)
+        StringBuilder sb = new StringBuilder();
+        SnakeCaseState state = SnakeCaseState.Start;
+
+        for (int i = 0; i < s.Length; i++)
         {
-            if (string.IsNullOrEmpty(s))
+            if (s[i] == ' ')
             {
-                return s;
-            }
-
-            StringBuilder sb = new StringBuilder();
-            SnakeCaseState state = SnakeCaseState.Start;
-
-            for (int i = 0; i < s.Length; i++)
-            {
-                if (s[i] == ' ')
+                if (state != SnakeCaseState.Start)
                 {
-                    if (state != SnakeCaseState.Start)
-                    {
-                        state = SnakeCaseState.NewWord;
-                    }
+                    state = SnakeCaseState.NewWord;
                 }
-                else if (char.IsUpper(s[i]))
+            }
+            else if (char.IsUpper(s[i]))
+            {
+                switch (state)
                 {
-                    switch (state)
-                    {
-                        case SnakeCaseState.Upper:
-                            bool hasNext = (i + 1 < s.Length);
-                            if (i > 0 && hasNext)
+                    case SnakeCaseState.Upper:
+                        bool hasNext = (i + 1 < s.Length);
+                        if (i > 0 && hasNext)
+                        {
+                            char nextChar = s[i + 1];
+                            if (!char.IsUpper(nextChar) && nextChar != '_')
                             {
-                                char nextChar = s[i + 1];
-                                if (!char.IsUpper(nextChar) && nextChar != '_')
-                                {
-                                    sb.Append('_');
-                                }
+                                sb.Append('_');
                             }
-                            break;
-                        case SnakeCaseState.Lower:
-                        case SnakeCaseState.NewWord:
-                            sb.Append('_');
-                            break;
-                    }
+                        }
+                        break;
+                    case SnakeCaseState.Lower:
+                    case SnakeCaseState.NewWord:
+                        sb.Append('_');
+                        break;
+                }
 
-                    char c;
+                char c;
 #if HAVE_CHAR_TO_LOWER_WITH_CULTURE
                     c = char.ToLower(s[i], CultureInfo.InvariantCulture);
 #else
-                    c = char.ToLowerInvariant(s[i]);
+                c = char.ToLowerInvariant(s[i]);
 #endif
-                    sb.Append(c);
+                sb.Append(c);
 
-                    state = SnakeCaseState.Upper;
-                }
-                else if (s[i] == '_')
+                state = SnakeCaseState.Upper;
+            }
+            else if (s[i] == '_')
+            {
+                sb.Append('_');
+                state = SnakeCaseState.Start;
+            }
+            else
+            {
+                if (state == SnakeCaseState.NewWord)
                 {
                     sb.Append('_');
-                    state = SnakeCaseState.Start;
                 }
-                else
-                {
-                    if (state == SnakeCaseState.NewWord)
-                    {
-                        sb.Append('_');
-                    }
 
-                    sb.Append(s[i]);
-                    state = SnakeCaseState.Lower;
-                }
+                sb.Append(s[i]);
+                state = SnakeCaseState.Lower;
             }
-
-            return sb.ToString();
         }
-    }
 
-    public class SnakeCaseNamingPolicy : JsonNamingPolicy
+        return sb.ToString();
+    }
+}
+
+public class SnakeCaseNamingPolicy : JsonNamingPolicy
+{
+    public override string ConvertName(string name)
     {
-        public override string ConvertName(string name)
-        {
-            return StringUtils.ToSnakeCase(name);
-        }
+        return StringUtils.ToSnakeCase(name);
     }
-
-    public class Startup
-    {
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddControllersWithViews();
-        }
-
-        public void Configure(IApplicationBuilder app)
-        {
-            app.UseRouting();
-            app.UseEndpoints(route =>
-            {
-                route.MapGet("/", async context =>
-                {
-                    var payload = new
-                    {
-                        Name = "Annie",
-                        Age = 33,
-                        IsMarried = false,
-                        CurrentTime = DateTimeOffset.UtcNow,
-                        Characters = new
-                        {
-                            Funny = true,
-                            Feisty = true,
-                            Brilliant = true,
-                            FOMA = false,
-                            HighEmpathy = true
-                        }
-                    };
-
-                    var options = new JsonSerializerOptions
-                    {
-                        WriteIndented = true,
-                        IgnoreNullValues = true,
-                        PropertyNamingPolicy = new SnakeCaseNamingPolicy(),
-                        DictionaryKeyPolicy = new SnakeCaseNamingPolicy()
-                    };
-
-                    context.Response.Headers.Add(HeaderNames.ContentType, "application/json");
-                    await JsonSerializer.SerializeAsync(context.Response.Body, payload, options);
-                });
-            });
-        }
-    }
-
 }
