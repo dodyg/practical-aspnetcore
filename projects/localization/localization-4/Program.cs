9c51379ae52cf910b97d57a0f604f3a4b6815696
Dody Gunawinata <dodyg@silverkeytech.com>
2021-12-09 10:46:21 +0200

    Upgrade .NET 6

--- projects/localization/localization-4/Program.cs
+++ projects/localization/localization-4/Program.cs
@@ -1,82 +1,67 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.AspNetCore.Localization;
 using Microsoft.Extensions.Localization;
-using System.Collections.Generic;
 using System.Globalization;
-using System;
-using Microsoft.Extensions.Hosting;
 
-namespace PracticalAspNetCore
-{
-    public class Startup
-    {
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddLocalization(options => options.ResourcesPath = "resources");
-        }
+var builder = WebApplication.CreateBuilder();
+builder.Services.AddLocalization(options => options.ResourcesPath = "resources");
 
-        public void Configure(IApplicationBuilder app, IStringLocalizerFactory stringLocalizerFactory)
-        {
-            var local = stringLocalizerFactory.Create("Common", typeof(Program).Assembly.FullName);
+var app = builder.Build();
 
-            //This section is important otherwise aspnet won't be able to pick up the resource
-            var supportedCultures = new List<CultureInfo>
+var stringLocalizerFactory = app.Services.GetService<IStringLocalizerFactory>();
+var local = stringLocalizerFactory.Create("Common", typeof(Program).Assembly.FullName);
+
+//This section is important otherwise aspnet won't be able to pick up the resource
+var supportedCultures = new List<CultureInfo>
             {
                 new CultureInfo("fr-FR"),
                 new CultureInfo("en-US")
             };
 
-            var options = new RequestLocalizationOptions
-            {
-                DefaultRequestCulture = new RequestCulture("fr-FR"),
-                SupportedCultures = supportedCultures,
-                SupportedUICultures = supportedCultures
-            };
+var options = new RequestLocalizationOptions
+{
+    DefaultRequestCulture = new RequestCulture("fr-FR"),
+    SupportedCultures = supportedCultures,
+    SupportedUICultures = supportedCultures
+};
 
-            app.UseRequestLocalization(options);
+app.UseRequestLocalization(options);
 
-            app.Run(async context =>
-            {
-                var languageSwitch = context.Request.Query["lang"];
+app.Run(async context =>
+{
+    var languageSwitch = context.Request.Query["lang"];
 
-                try
-                {
-                    if (languageSwitch == "fr")
-                    {
-                        var val = CookieRequestCultureProvider.MakeCookieValue(new RequestCulture("fr-FR"));
-                        context.Response.Cookies.Append(CookieRequestCultureProvider.DefaultCookieName, val);
-                        context.Response.Redirect("/");
-                        return;
-                    }
-                    else if (languageSwitch == "en")
-                    {
-                        var val = CookieRequestCultureProvider.MakeCookieValue(new RequestCulture("en-US"));
-                        context.Response.Cookies.Append(CookieRequestCultureProvider.DefaultCookieName, val);
-                        context.Response.Redirect("/");
-                        return;
-                    }
-                }
-                catch (Exception ex)
-                {
-                    await context.Response.WriteAsync($"error {ex.Message}");
-                }
+    try
+    {
+        if (languageSwitch == "fr")
+        {
+            var val = CookieRequestCultureProvider.MakeCookieValue(new RequestCulture("fr-FR"));
+            context.Response.Cookies.Append(CookieRequestCultureProvider.DefaultCookieName, val);
+            context.Response.Redirect("/");
+            return;
+        }
+        else if (languageSwitch == "en")
+        {
+            var val = CookieRequestCultureProvider.MakeCookieValue(new RequestCulture("en-US"));
+            context.Response.Cookies.Append(CookieRequestCultureProvider.DefaultCookieName, val);
+            context.Response.Redirect("/");
+            return;
+        }
+    }
+    catch (Exception ex)
+    {
+        await context.Response.WriteAsync($"error {ex.Message}");
+    }
 
-                await context.Response.WriteAsync($"<h1>CookieRequestCultureProvider</h1><p>We are using a cookie named \"{CookieRequestCultureProvider.DefaultCookieName}\" to switch the request culture.</p>");
+    await context.Response.WriteAsync($"<h1>CookieRequestCultureProvider</h1><p>We are using a cookie named \"{CookieRequestCultureProvider.DefaultCookieName}\" to switch the request culture.</p>");
 
-                var requestCulture = context.Features.Get<IRequestCultureFeature>().RequestCulture;
+    var requestCulture = context.Features.Get<IRequestCultureFeature>().RequestCulture;
 
-                await context.Response.WriteAsync($@"
+    await context.Response.WriteAsync($@"
                 Click <a href=""/?lang=fr"">here</a> for French cookie and <a href=""/?lang=en"">here</a> for English cookie.<br/><br/>
                 
                 Request Culture: {requestCulture.Culture} <br/> 
                 Localized strings: {local["Hello"]} {local["Goodbye"]} {local["Yes"]} {local["No"]}
                 ");
-            });
-        }
-    }
+});
 
-
-}
+app.Run();
