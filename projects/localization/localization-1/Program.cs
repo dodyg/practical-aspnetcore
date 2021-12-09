2aad9db64000e13a9b5cf14c713e2a72356ad661
Dody Gunawinata <dodyg@silverkeytech.com>
2021-12-09 10:36:36 +0200

    Upgrade to .NET 6

--- projects/localization/localization-1/Program.cs
+++ projects/localization/localization-1/Program.cs
@@ -1,48 +1,34 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.AspNetCore.Localization;
 using Microsoft.Extensions.Localization;
-using System.Collections.Generic;
 using System.Globalization;
-using Microsoft.Extensions.Hosting;
 
-namespace PracticalAspNetCore 
+var builder = WebApplication.CreateBuilder();
+builder.Services.AddLocalization(options => options.ResourcesPath = "resources");
+
+var app = builder.Build();
+
+var stringLocalizerFactory = app.Services.GetService<IStringLocalizerFactory>();
+var local = stringLocalizerFactory.Create("Common", typeof(Program).Assembly.FullName);
+
+//This section is important otherwise aspnet won't be able to pick up the resource
+var supportedCultures = new List<CultureInfo>
 {
-    public class Startup
-    {
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddLocalization(options => options.ResourcesPath = "resources");
-        }
+    new CultureInfo("fr-FR")
+};
 
-        public void Configure(IApplicationBuilder app, IStringLocalizerFactory stringLocalizerFactory)
-        {
-            var local = stringLocalizerFactory.Create("Common", typeof(Program).Assembly.FullName);
+var options = new RequestLocalizationOptions
+{
+    DefaultRequestCulture = new RequestCulture("fr-FR"),
+    SupportedCultures = supportedCultures,
+    SupportedUICultures = supportedCultures
+};
 
-            //This section is important otherwise aspnet won't be able to pick up the resource
-            var supportedCultures = new List<CultureInfo>
-            {
-                new CultureInfo("fr-FR")
-            };
-            
-            var options = new RequestLocalizationOptions
-            {
-                DefaultRequestCulture = new RequestCulture("fr-FR"),
-                SupportedCultures = supportedCultures,
-                SupportedUICultures = supportedCultures
-            };
+app.UseRequestLocalization(options);
 
-            app.UseRequestLocalization(options);
+app.Run(async context =>
+{
+    var requestCulture = context.Features.Get<IRequestCultureFeature>();
+    await context.Response.WriteAsync($"{requestCulture.RequestCulture.Culture} - {local["Hello"]} {local["Goodbye"]} {local["Yes"]} {local["No"]}");
+});
 
-            app.Run(async context =>
-            {
-                var requestCulture = context.Features.Get<IRequestCultureFeature>();
-                await context.Response.WriteAsync($"{requestCulture.RequestCulture.Culture} - {local["Hello"]} {local["Goodbye"]} {local["Yes"]} {local["No"]}");
-            });
-        }
-    }
-    
-
-}
+app.Run();
