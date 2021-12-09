2a512903becd339cf45df559d4100d02f224803e
Dody Gunawinata <dodyg@silverkeytech.com>
2021-12-09 10:43:50 +0200

    Upgrade to .NET 6

--- projects/localization/localization-3/Program.cs
+++ projects/localization/localization-3/Program.cs
@@ -1,51 +1,39 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.AspNetCore.Localization;
 using Microsoft.Extensions.Localization;
-using System.Collections.Generic;
 using System.Globalization;
-using System;
-using Microsoft.Extensions.Hosting;
 
-namespace PracticalAspNetCore
+var builder = WebApplication.CreateBuilder();
+builder.Services.AddLocalization(options => options.ResourcesPath = "resources");
+
+var app = builder.Build();
+
+var stringLocalizerFactory = app.Services.GetService<IStringLocalizerFactory>();
+var local = stringLocalizerFactory.Create("Common", typeof(Program).Assembly.FullName);
+
+//This section is important otherwise aspnet won't be able to pick up the resource
+var supportedCultures = new List<CultureInfo>
 {
-    public class Startup
-    {
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddLocalization(options => options.ResourcesPath = "resources");
-        }
+    new CultureInfo("en-US"),
+    new CultureInfo("en-GB")
+};
 
-        public void Configure(IApplicationBuilder app, IStringLocalizerFactory stringLocalizerFactory)
-        {
-            var local = stringLocalizerFactory.Create("Common", typeof(Program).Assembly.FullName);
+var options = new RequestLocalizationOptions
+{
+    DefaultRequestCulture = new RequestCulture("en-GB"),
+    SupportedCultures = supportedCultures,
+    SupportedUICultures = supportedCultures
+};
 
-            //This section is important otherwise aspnet won't be able to pick up the resource
-            var supportedCultures = new List<CultureInfo>
-            {
-                new CultureInfo("en-US"),
-                new CultureInfo("en-GB")
-            };
-            
-            var options = new RequestLocalizationOptions
-            {
-                DefaultRequestCulture = new RequestCulture("en-GB"),
-                SupportedCultures = supportedCultures,
-                SupportedUICultures = supportedCultures
-            };
+app.UseRequestLocalization(options);
 
-            app.UseRequestLocalization(options);
+//These are the three default services available at Configure
+app.Run(async context =>
+{
+    await context.Response.WriteAsync("<h1>Culture vs UI Culture</h1><p>UI Culture affects the resource string. Culture affects number formatting, dates, etc.</p>");
 
-            //These are the three default services available at Configure
-            app.Run(async context =>
-            {
-                await context.Response.WriteAsync("<h1>Culture vs UI Culture</h1><p>UI Culture affects the resource string. Culture affects number formatting, dates, etc.</p>");
+    var requestCulture = context.Features.Get<IRequestCultureFeature>().RequestCulture;
 
-                var requestCulture = context.Features.Get<IRequestCultureFeature>().RequestCulture;
-
-                await context.Response.WriteAsync($@"
+    await context.Response.WriteAsync($@"
                 <a href=""/?culture=en-US&ui-culture=en-GB"">Culture US - UI Culture GB</a><br/>
                 <a href=""/?culture=en-GB&ui-culture=en-US"">Culture GB - UI Culture US</a><br/>
                 <br/>
@@ -54,9 +42,6 @@ namespace PracticalAspNetCore
                 Request UI Culture: {requestCulture.UICulture}<br/> 
                 Localized strings (UI Culture): {local["Hello"]} {local["Goodbye"]} {local["Yes"]} {local["No"]}
                 ");
-            });
-        }
-    }
-    
+});
 
-}
+app.Run();
