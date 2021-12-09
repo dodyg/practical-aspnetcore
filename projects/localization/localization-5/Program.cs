c733e25cf34f74b9ddba7be3b7f49101031af944
Dody Gunawinata <dodyg@silverkeytech.com>
2021-12-09 10:49:43 +0200

    Upgrade to .NET 6

--- projects/localization/localization-5/Program.cs
+++ projects/localization/localization-5/Program.cs
@@ -1,55 +1,38 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Configuration;
-using System.Collections.Generic;
 using System.Globalization;
 using Microsoft.AspNetCore.Localization;
 using Microsoft.Extensions.Localization;
-using Microsoft.Extensions.Hosting;
 
-namespace PracticalAspNetCore
+var builder = WebApplication.CreateBuilder();
+builder.Services.AddMemoryCache();
+builder.Services.AddPortableObjectLocalization();
+
+var app = builder.Build();
+
+//We are limiting the supported culture here so this sample works in any browser from different culture setting.
+//To make it pick up French or other language, simply change it-IT with something else or add more supported cultures
+//I have added PO file for French and English
+var supportedCultures = new List<CultureInfo>
 {
-    public class Startup
-    {
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddMemoryCache();
-            services.AddPortableObjectLocalization();
-        }
+    new CultureInfo("it-IT")
+};
 
-        public void Configure(IApplicationBuilder app)
-        {
-            //We are limiting the supported culture here so this sample works in any browser from different culture setting.
-            //To make it pick up French or other language, simply change it-IT with something else or add more supported cultures
-            //I have added PO file for French and English
-            var supportedCultures = new List<CultureInfo>
-                    {
-                        new CultureInfo("it-IT")
-                    };
+var option = new RequestLocalizationOptions
+{
+    DefaultRequestCulture = new RequestCulture("it-IT"),
+    SupportedCultures = supportedCultures,
+    SupportedUICultures = supportedCultures
+};
 
-            var option = new RequestLocalizationOptions
-            {
-                DefaultRequestCulture = new RequestCulture("it-IT"),
-                SupportedCultures = supportedCultures,
-                SupportedUICultures = supportedCultures
-            };
+app.UseRequestLocalization(option);
 
-            app.UseRequestLocalization(option);
+app.Run(context =>
+{
+    var fac = context.RequestServices.GetService<IStringLocalizerFactory>();
+    var local = fac.Create(string.Empty, string.Empty);
 
-            app.Run(context =>
-            {
-                var fac = context.RequestServices.GetService<IStringLocalizerFactory>();
-                var local = fac.Create(string.Empty, string.Empty);
+    var requestCulture = context.Features.Get<IRequestCultureFeature>().RequestCulture;
 
-                var requestCulture = context.Features.Get<IRequestCultureFeature>().RequestCulture;
+    return context.Response.WriteAsync($"Request Culture `{requestCulture.UICulture}` = {local["Hello"]}");
+});
 
-                return context.Response.WriteAsync($"Request Culture `{requestCulture.UICulture}` = {local["Hello"]}");
-            });
-        }
-    }
-
-
-}
+app.Run();
