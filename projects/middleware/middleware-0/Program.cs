0b7c94b7897e92c713f28beb6283ccaa563db596
Dody Gunawinata <dodyg@silverkeytech.com>
2021-12-10 14:08:59 +0200

    upgrade to .NET 6

--- projects/middleware/middleware-0/Program.cs
+++ projects/middleware/middleware-0/Program.cs
@@ -1,40 +1,25 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore;
-using System.Threading.Tasks;
-using Microsoft.Extensions.Hosting;
+var app = WebApplication.Create();
 
-namespace PracticalAspNetCore
+//The order of these things are important. 
+app.Use(async (context, next) =>
 {
-    public class Startup
-    {
-        public void Configure(IApplicationBuilder app)
-        {
-            //The order of these things are important. 
-            app.Use(async (context, next) =>
-            {
-                context.Items["Content"] += "[1] ----- \n";//1
-                await next.Invoke();
-                context.Items["Content"] += "[5] =====\n";//5
+    context.Items["Content"] += "[1] ----- \n";//1
+    await next(context);
+    context.Items["Content"] += "[5] =====\n";//5
+    await context.Response.WriteAsync(context.Items["Content"] as string);
+});
 
-                await context.Response.WriteAsync(context.Items["Content"] as string);
-            });
+app.Use(async (context, next) =>
+{
+    context.Items["Content"] += "[2] Hello world \n";//2
+    await next(context);
+    context.Items["Content"] += "[4]  \n";//4
+});
 
-            app.Use(async (context, next) =>
-            {
-                context.Items["Content"] += "[2] Hello world \n";//2
-                await next.Invoke();
-                context.Items["Content"] += "[4]  \n";//4
-            });
+app.Run(context =>
+{
+    context.Items["Content"] += "[3] ----- \n";//3
+    return Task.CompletedTask;
+});
 
-            app.Run(context =>
-            {
-                context.Items["Content"] += "[3] ----- \n";//3
-                return Task.CompletedTask;
-            });
-        }
-    }
-
-
-}
+app.Run();
