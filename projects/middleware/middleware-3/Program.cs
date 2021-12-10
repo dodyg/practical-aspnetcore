ea9e02ef22bed3452c7ff382f47125b6b3b1c51a
Dody Gunawinata <dodyg@silverkeytech.com>
2021-12-10 14:26:04 +0200

    Upgrade to .NET 6

--- projects/middleware/middleware-3/Program.cs
+++ projects/middleware/middleware-3/Program.cs
@@ -1,39 +1,22 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Http;
-using System.Threading.Tasks;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.AspNetCore;
-using Microsoft.Extensions.Hosting;
+var app = WebApplication.Create();
+app.UseMiddleware<TerminalMiddleware>();
+app.Run();
 
-namespace PracticalAspNetCore
+//A middleware class in ASP.NET Core is simply a class that 
+// - Take a constructor with RequestDelegate
+// - implements Invoke method taking HttpContext and returning Task
+//If you take a look at this code, it cannot be any simpler.
+//This is a terminal middleware. It does not invoke the subsequent middleware. It just returns its own response and that's it.
+
+public class TerminalMiddleware
 {
-    public class Startup
+    public TerminalMiddleware(RequestDelegate next)
     {
-        public void Configure(IApplicationBuilder app)
-        {
-            app.UseMiddleware<TerminalMiddleware>();
-        }
+        //We are not using the parameter next in this middleware since this middleware is terminal
     }
 
-    //A middleware class in ASP.NET Core is simply a class that 
-    // - Take a constructor with RequestDelegate
-    // - implements Invoke method taking HttpContext and returning Task
-    //If you take a look at this code, it cannot be any simpler.
-    //This is a terminal middleware. It does not invoke the subsequent middleware. It just returns its own response and that's it.
-
-    public class TerminalMiddleware
+    public async Task Invoke(HttpContext context)
     {
-        public TerminalMiddleware(RequestDelegate next)
-        {
-            //We are not using the parameter next in this middleware since this middleware is terminal
-        }
-        
-        public async Task Invoke(HttpContext context)
-        {
-            await context.Response.WriteAsync("Hello world");
-        }
+        await context.Response.WriteAsync("Hello world");
     }
-
-
 }
