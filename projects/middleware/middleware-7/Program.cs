67719037c9c5f797418d3f70ea39982eea6bd34f
Dody Gunawinata <dodyg@silverkeytech.com>
2021-12-10 14:35:14 +0200

    Upgrade to .NET 6

--- projects/middleware/middleware-7/Program.cs
+++ projects/middleware/middleware-7/Program.cs
@@ -1,48 +1,31 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Http;
-using System.Threading.Tasks;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.AspNetCore.Builder.Extensions;
-using Microsoft.AspNetCore;
-using Microsoft.Extensions.Hosting;
+var app = WebApplication.Create();
 
-namespace PracticalAspNetCore
+//Use MapMiddleWare and MapWhenMiddlware directly
+var whenOption = new MapWhenOptions
 {
-    public class Startup
-    {
-        public void Configure(IApplicationBuilder app)
-        {
-            //Use MapMiddleWare and MapWhenMiddlware directly
-            var whenOption = new MapWhenOptions
-            {
-                Branch =
-                    context =>
-                        context.Response.WriteAsync($"MapWhenMiddleware| Path: {context.Request.Path} - Path Base: {context.Request.PathBase}"),
-                Predicate = context => context.Request.Path.Value.Contains("hello")
-            };
+    Branch =
+        context =>
+            context.Response.WriteAsync($"MapWhenMiddleware| Path: {context.Request.Path} - Path Base: {context.Request.PathBase}"),
+    Predicate = context => context.Request.Path.Value.Contains("hello")
+};
 
-            app.UseMiddleware<MapWhenMiddleware>(whenOption);
+app.UseMiddleware<MapWhenMiddleware>(whenOption);
 
-            var mapOption = new MapOptions
-            {
-                Branch =
-                    context =>
-                        context.Response.WriteAsync($"MapMiddleware| Path: {context.Request.Path} - Path Base: {context.Request.PathBase}"),
-                PathMatch = "/greetings"
-            };
+var mapOption = new MapOptions
+{
+    Branch =
+        context =>
+            context.Response.WriteAsync($"MapMiddleware| Path: {context.Request.Path} - Path Base: {context.Request.PathBase}"),
+    PathMatch = "/greetings"
+};
 
-            app.UseMiddleware<MapMiddleware>(mapOption);
+app.UseMiddleware<MapMiddleware>(mapOption);
 
-            app.Run(context =>
-            {
-                context.Response.Headers.Add("content-type", "text/html");
-                return context.Response.WriteAsync(@"
-                   <a href=""/hello"">/hello</a> <a href=""/greetings"">/greetings</a>
-                ");
-            });
-        }
-    }
+app.Run(context =>
+{
+    context.Response.Headers.Add("content-type", "text/html");
+    return context.Response.WriteAsync(@"<a href=""/hello"">/hello</a> <a href=""/greetings"">/greetings</a>");
+});
 
-
-}
+app.Run();
