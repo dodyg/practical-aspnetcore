021fcc03d39cf8360ea954fca33b0f0257b686c9
Dody Gunawinata <dodyg@silverkeytech.com>
2021-12-10 14:42:14 +0200

    Upgrade to .NET 6

--- projects/middleware/middleware-10/Program.cs
+++ projects/middleware/middleware-10/Program.cs
@@ -1,38 +1,19 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Http;
-using System.Threading.Tasks;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.AspNetCore;
-using Microsoft.Extensions.Logging;
-using System;
-using Microsoft.Extensions.Hosting;
+var app = WebApplication.Create();
+app.UseMiddleware(typeof(TerminalMiddleware));
+app.Run();
 
-namespace PracticalAspNetCore
+public class TerminalMiddleware
 {
-    public class Startup
+    DateTime _date = DateTime.Now;
+
+    public TerminalMiddleware(RequestDelegate next)
     {
-        public void Configure(IApplicationBuilder app)
-        {
-            app.UseMiddleware(typeof(TerminalMiddleware));
-        }
     }
 
-    public class TerminalMiddleware
+    public async Task Invoke(HttpContext context, ILogger<TerminalMiddleware> log)
     {
-        DateTime _date = DateTime.Now;
-
-        public TerminalMiddleware(RequestDelegate next)
-        {
-        }
-
-        public async Task Invoke(HttpContext context, ILogger<TerminalMiddleware> log)
-        {
-            log.LogDebug($"Request: {context.Request.Path}");
-            context.Response.Headers.Add("Content-Type", "text/plain");
-            await context.Response.WriteAsync($"Middleware is singleton. Keep refreshing the page. You will see that the date does not change {_date}.");
-        }
+        log.LogDebug($"Request: {context.Request.Path}");
+        context.Response.Headers.Add("Content-Type", "text/plain");
+        await context.Response.WriteAsync($"Middleware is singleton. Keep refreshing the page. You will see that the date does not change {_date}.");
     }
-
-
 }
