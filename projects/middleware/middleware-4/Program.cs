9eb2f996c2894461820f58ba69f8031529153959
Dody Gunawinata <dodyg@silverkeytech.com>
2021-12-10 14:27:43 +0200

    Upgrade to .NET 6

--- projects/middleware/middleware-4/Program.cs
+++ projects/middleware/middleware-4/Program.cs
@@ -1,29 +1,14 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Http;
-using System.Threading.Tasks;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.AspNetCore;
-using Microsoft.Extensions.Hosting;
+var app = WebApplication.Create();
 
-namespace PracticalAspNetCore
+app.Map("/hello", (IApplicationBuilder pp) => pp.Run(context => context.Response.WriteAsync("/hello path")));
+app.Map("/world", (IApplicationBuilder pp) => pp.Run(context => context.Response.WriteAsync("/world path")));
+
+app.Run(context =>
 {
-    public class Startup
-    {
-        public void Configure(IApplicationBuilder app)
-        {
-            app.Map("/hello", (IApplicationBuilder pp) => pp.Run(context => context.Response.WriteAsync("/hello path")));
-            app.Map("/world", (IApplicationBuilder pp) => pp.Run(context => context.Response.WriteAsync("/world path")));
-
-            app.Run(context =>
-            {
-                context.Response.Headers.Add("Content-Type", "text/html");
-                return context.Response.WriteAsync(@"
+    context.Response.Headers.Add("Content-Type", "text/html");
+    return context.Response.WriteAsync(@"
                    <a href=""/hello"">hello</a> <a href=""/world"">world</a>
                 ");
-            });
-        }
-    }
+});
 
-
-}
+app.Run();
