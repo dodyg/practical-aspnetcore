dee2d36db7946151dd9153a649d023891e9e4ccd
Dody Gunawinata <dodyg@silverkeytech.com>
2021-12-10 14:29:33 +0200

    Upgrade  to .NET 6

--- projects/middleware/middleware-5/Program.cs
+++ projects/middleware/middleware-5/Program.cs
@@ -1,35 +1,19 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Http;
-using System.Threading.Tasks;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.AspNetCore;
-using Microsoft.Extensions.Hosting;
+var app = WebApplication.Create();
 
-namespace PracticalAspNetCore
+app.Map("/hello", (IApplicationBuilder pp) =>
 {
-    public class Startup
-    {
-        public void Configure(IApplicationBuilder app)
-        {
-            app.Map("/hello", (IApplicationBuilder pp) => {
-                //nested
-                app.Map("/world", (IApplicationBuilder ppa) => ppa.Run(context => 
-                    context.Response.WriteAsync($"Path: {context.Request.Path} - Path Base: {context.Request.PathBase}")));
-                
-                pp.Run(context => 
-                    context.Response.WriteAsync($"Path: {context.Request.Path} - Path Base: {context.Request.PathBase}"));
-            });
-            
-            app.Run(context =>
-            { 
-                context.Response.Headers.Add("Content-Type", "text/html");
-                return context.Response.WriteAsync(@"
-                   <a href=""/hello"">/hello</a> <a href=""/hello/world"">/hello/world</a>
-                ");
-            });
-        }
-    }
+    //nested
+    app.Map("/world", (IApplicationBuilder ppa) => ppa.Run(context =>
+        context.Response.WriteAsync($"Path: {context.Request.Path} - Path Base: {context.Request.PathBase}")));
 
+    pp.Run(context =>
+        context.Response.WriteAsync($"Path: {context.Request.Path} - Path Base: {context.Request.PathBase}"));
+});
 
-}
+app.Run(context =>
+{
+    context.Response.Headers.Add("Content-Type", "text/html");
+    return context.Response.WriteAsync(@"<a href=""/hello"">/hello</a> <a href=""/hello/world"">/hello/world</a>");
+});
+
+app.Run();
