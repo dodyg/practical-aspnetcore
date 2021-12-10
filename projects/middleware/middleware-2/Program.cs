0e3964c48d2cd1feb0c94731934a01544d0c3ae9
Dody Gunawinata <dodyg@silverkeytech.com>
2021-12-10 14:23:32 +0200

    Upgrade to .NET 6

--- projects/middleware/middleware-2/Program.cs
+++ projects/middleware/middleware-2/Program.cs
@@ -1,53 +1,34 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Http;
-using System.IO;
-using System.Threading.Tasks;
-using Microsoft.Extensions.Hosting;
+var app = WebApplication.Create();
 
-namespace Middleware
+//https://github.com/aspnet/Entropy/blob/master/samples/Builder.Filtering.Web/Startup.cs
+
+app.Use(BufferAsync);
+app.Run(async context =>
 {
-    //https://github.com/aspnet/Entropy/blob/dev/samples/Builder.Filtering.Web/Startup.cs
-    public class Startup
+    context.Response.Headers.Add("Content-Type", "text/html");
+    await context.Response.WriteAsync("<h1>This sample uses buffering</h1>");
+    await context.Response.WriteAsync("<p>This allows all your Middlewares to write to Response.</p>");
+});
+
+async Task BufferAsync(HttpContext context, RequestDelegate next)
+{
+    var body = context.Response.Body;
+    var buffer = new MemoryStream();
+    context.Response.Body = buffer;
+
+    try
     {
-        public void Configure(IApplicationBuilder app)
-        {
-            //We use it the type so it is clear. We will use the shortened version from now on.
-            //app.Use(next => context => FilterAsync(context, next));
-            app.Use((RequestDelegate next) =>
-            {
-                return (HttpContext context) => BufferAsync(context, next);
-            });
+        await context.Response.WriteAsync("<html><body>");
+        await next(context);
+        await context.Response.WriteAsync("</body></html>");
 
-            app.Run(async context =>
-            {
-                context.Response.Headers.Add("Content-Type", "text/html");
-                await context.Response.WriteAsync("<h1>This sample uses buffering</h1>");
-                await context.Response.WriteAsync("<p>This allows all your Middlewares to write to Response.</p>");
-            });
-        }
-
-        public async Task BufferAsync(HttpContext context, RequestDelegate next)
-        {
-            var body = context.Response.Body;
-            var buffer = new MemoryStream();
-            context.Response.Body = buffer;
-
-            try
-            {
-                await context.Response.WriteAsync("<html><body>");
-                await next(context);
-                await context.Response.WriteAsync("</body></html>");
-
-                buffer.Position = 0;
-                await buffer.CopyToAsync(body);
-            }
-            finally
-            {
-                context.Response.Body = body;
-            }
-        }
+        buffer.Position = 0;
+        await buffer.CopyToAsync(body);
     }
-
-
+    finally
+    {
+        context.Response.Body = body;
+    }
 }
+
+app.Run();
