366f2090e2d8baf1bf0b95ef3abef002baa94933
Dody Gunawinata <dodyg@silverkeytech.com>
2021-12-12 08:40:58 +0200

    Upgrade to .NET 6

--- projects/blazor-ss/HelloWorld/Program.cs
+++ projects/blazor-ss/HelloWorld/Program.cs
@@ -1,57 +1,13 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.DependencyInjection;
-using HelloWorld.Components;
+var builder = WebApplication.CreateBuilder();
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
 
-namespace HelloWorld
-{
-    public class Startup
-    {
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddRazorPages();
-            services.AddServerSideBlazor();
-        }
+var app = builder.Build();
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseHsts();
-            }
+app.UseHttpsRedirection();
+app.UseStaticFiles();
 
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
-
-            app.UseRouting();
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapBlazorHub();
-                endpoints.MapRazorPages();
-                endpoints.MapFallbackToPage("/Index");
-            });
-        }
-    }
-
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
-
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                    webBuilder.UseEnvironment(Environments.Development);
-                });
-    }
-}
+app.MapBlazorHub();
+app.MapRazorPages();
+app.MapFallbackToPage("/Index");
+app.Run();
--- projects/blazor-ss/HelloWorld/README.md
+++ projects/blazor-ss/HelloWorld/README.md
@@ -2,11 +2,4 @@
 
 This is the simplest Razor component app you can create. It will show you clearly the building block of a Razor component application.
 
-There are two extra settings for dotnet watch to monitor `*.cshtml` and `*.razor` file changes on two projects to make your development experience better.
-
-``` xml
-    <Watch Include="**\*.cshtml" />
-    <Watch Include="**\*.razor" />
-```
-
 Run the sample on `HelloWorld` using `dotnet watch`.
