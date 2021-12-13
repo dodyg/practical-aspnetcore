6b6101b503ae9ec748d4097aa936975bfd067607
Dody Gunawinata <dodyg@silverkeytech.com>
2021-12-13 10:02:39 +0200

    Upgrade to .NET 6

--- projects/blazor-ss/DependencyInjection/Program.cs
+++ projects/blazor-ss/DependencyInjection/Program.cs
@@ -1,62 +1,21 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.DependencyInjection;
 using DependencyInjection.Services;
 
-namespace DependencyInjection
-{
-    public class Startup
-    {
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddRazorPages();
-            services.AddServerSideBlazor();
+var builder = WebApplication.CreateBuilder();
 
-            services.AddTransient<TheTransientClock>();
-            services.AddSingleton<TheSingletonClock>();
-            services.AddScoped<TheScopedClock>();
-        }
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddTransient<TheTransientClock>();
+builder.Services.AddSingleton<TheSingletonClock>();
+builder.Services.AddScoped<TheScopedClock>();
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
+var app = builder.Build();
 
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
+app.UseHttpsRedirection();
+app.UseStaticFiles();
 
-            app.UseRouting();
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapRazorPages();
-                endpoints.MapFallbackToPage("/Index");
-                endpoints.MapBlazorHub();
-            });
-        }
-    }
+app.UseRouting();
+app.MapRazorPages();
+app.MapFallbackToPage("/Index");
+app.MapBlazorHub();
 
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
-
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                    webBuilder.UseEnvironment(Environments.Development);
-                });
-    }
-}
+app.Run();
