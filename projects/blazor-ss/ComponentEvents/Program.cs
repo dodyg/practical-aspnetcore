0883a6f2f498e671211d7c99e7a35651db05db9a
Dody Gunawinata <dodyg@silverkeytech.com>
2021-12-14 10:00:31 +0200

    Upgrade to .NET 6

--- projects/blazor-ss/ComponentEvents/Program.cs
+++ projects/blazor-ss/ComponentEvents/Program.cs
@@ -1,57 +1,17 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.DependencyInjection;
+using ComponentEvents;
 
-namespace ComponentEvents
-{
-    public class Startup
-    {
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddRazorPages();
-            services.AddServerSideBlazor();
-            services.AddScoped<AppState>();
-        }
+var builder = WebApplication.CreateBuilder();
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddScoped<AppState>();
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseHsts();
-            }
+var app = builder.Build();
 
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
+app.UseStaticFiles();
 
-            app.UseRouting();
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapBlazorHub();
-                endpoints.MapRazorPages();
-                endpoints.MapFallbackToPage("/Index");
-            });
-        }
-    }
+app.MapBlazorHub();
+app.MapRazorPages();
+app.MapFallbackToPage("/Index");
 
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
-
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                    webBuilder.UseEnvironment(Environments.Development);
-                });
-    }
-}
+app.Run();
