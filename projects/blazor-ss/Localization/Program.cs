ddbb851b0602b4a55cd2b5eb46f2fea9ee9c9b9c
Dody Gunawinata <dodyg@silverkeytech.com>
2021-12-14 10:16:12 +0200

    Upgrade to .NET 6

--- projects/blazor-ss/Localization/Program.cs
+++ projects/blazor-ss/Localization/Program.cs
@@ -1,86 +1,39 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.AspNetCore.Localization;
 using System.Globalization;
-using System.Threading.Tasks;
-using System;
-using System.Collections.Generic;
 
-namespace Localization
+var builder = WebApplication.CreateBuilder();
+builder.Services.AddLocalization(opts => { opts.ResourcesPath = "Resources"; });
+
+var supportedCultures = new List<CultureInfo>
 {
-    // Leave this class empty. We use it to bind to resources.
-    public class Global
-    {
-    }
-    
-    public class Startup
-    {
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddLocalization(opts => { opts.ResourcesPath = "Resources"; });
-            
-            var supportedCultures = new List<CultureInfo>
-            {
-                new CultureInfo("fr-FR"),
-                new CultureInfo("en"),
-                new CultureInfo("fr"),
-            };
+    new CultureInfo("fr-FR"),
+    new CultureInfo("en"),
+    new CultureInfo("fr"),
+};
 
-            services.Configure<RequestLocalizationOptions>(options => 
-            {
-                options.DefaultRequestCulture = new RequestCulture("fr");
-                options.SupportedCultures = supportedCultures;
-                options.SupportedUICultures = supportedCultures;
-                options.RequestCultureProviders.Clear();
-                options.RequestCultureProviders.Add(new CookieRequestCultureProvider());
-            });
+builder.Services.Configure<RequestLocalizationOptions>(options =>
+{
+    options.DefaultRequestCulture = new RequestCulture("fr");
+    options.SupportedCultures = supportedCultures;
+    options.SupportedUICultures = supportedCultures;
+    options.RequestCultureProviders.Clear();
+    options.RequestCultureProviders.Add(new CookieRequestCultureProvider());
+});
 
-            services.AddRazorPages();
-            services.AddServerSideBlazor();
-        }
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
+var app = builder.Build();
 
-            app.UseHttpsRedirection();
-            app.UseRouting();
-            app.UseStaticFiles();
-            
-            app.UseRequestLocalization();
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapRazorPages();
-                endpoints.MapFallbackToPage("/Index");
-                endpoints.MapBlazorHub();
-            });
-        }
-    }
+app.UseStaticFiles();
 
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+app.MapRazorPages();
+app.MapFallbackToPage("/Index");
+app.MapBlazorHub();
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                    webBuilder.UseEnvironment(Environments.Development);
-                });
-    }
+app.Run();
+
+// Leave this class empty. We use it to bind to resources.
+public class Global
+{
 }
