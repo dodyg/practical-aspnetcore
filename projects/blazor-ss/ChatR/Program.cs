5accbdd9b195ed429023cf60d50c0d5337884817
Dody Gunawinata <dodyg@silverkeytech.com>
2021-12-15 13:34:09 +0200

    Upgrade to .NET 6

--- projects/blazor-ss/ChatR/Program.cs
+++ projects/blazor-ss/ChatR/Program.cs
@@ -1,66 +1,18 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.DependencyInjection;
 using ChatR.Services;
-using Microsoft.Extensions.Logging;
+using ChatR;
 
-namespace ChatR
-{
-    public class Startup
-    {
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddRazorPages();
-            services.AddServerSideBlazor(); //https://github.com/dotnet/aspnetcore/blob/master/src/Components/Server/src/DependencyInjection/ComponentServiceCollectionExtensions.cs
-            services.AddScoped<NotificationService>();
-        }
+var builder = WebApplication.CreateBuilder();
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor(); //https://github.com/dotnet/aspnetcore/blob/master/src/Components/Server/src/DependencyInjection/ComponentServiceCollectionExtensions.cs
+builder.Services.AddScoped<NotificationService>();
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
+var app = builder.Build();
 
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
+app.UseStaticFiles();
 
-            app.UseRouting();
-            app.UseEndpoints(routes =>
-            {
-                routes.MapBlazorHub(); //https://github.com/dotnet/aspnetcore/blob/master/src/Components/Server/src/Builder/ComponentEndpointRouteBuilderExtensions.cs
-                routes.MapHub<NotificationHub>("/notificationhub");
-                routes.MapRazorPages();
-                routes.MapFallbackToPage("/Index");
-            });
-        }
-    }
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+app.MapBlazorHub(); //https://github.com/dotnet/aspnetcore/blob/master/src/Components/Server/src/Builder/ComponentEndpointRouteBuilderExtensions.cs
+app.MapHub<NotificationHub>("/notificationhub");
+app.MapRazorPages();
+app.MapFallbackToPage("/Index");
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                    webBuilder.ConfigureLogging(builder =>
-                    {
-                        builder.ClearProviders();
-                        builder.SetMinimumLevel(LogLevel.Information);
-                        builder.AddConsole();
-                    });
-                    webBuilder.UseEnvironment(Environments.Development);
-                });
-    }
-}
+app.Run();
