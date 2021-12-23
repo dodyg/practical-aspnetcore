ff77ccab13a8ff8bdc7dce62d78a9fc6615ea729
Dody Gunawinata <dodyg@silverkeytech.com>
2021-12-23 10:41:23 +0200

    Upgrade to .NET 6

--- projects/uri-helper/uri-helper-get-encoded-url/Program.cs
+++ projects/uri-helper/uri-helper-get-encoded-url/Program.cs
@@ -1,23 +1,15 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Http.Extensions;
-using Microsoft.Extensions.Hosting;
 
-namespace PracticalAspNetCore
+var app = WebApplication.Create();
+
+app.Run(context =>
 {
-    public class Startup
-    {
-        public void Configure(IApplicationBuilder app)
-        {
-            app.Run(context =>
-            {
-                context.Response.Headers.Add("Content-Type", "text/html");
-                return context.Response.WriteAsync($@"<html>
+    context.Response.Headers.Add("Content-Type", "text/html");
+    return context.Response.WriteAsync($@"<html>
 <body>                
     <h1>Get Encoded Url</h1>
     <i>Returns the combined components of the request URL in a fully escaped form suitable for use in HTTP headers and other HTTP operations.</i>
-    <a href=""https://docs.microsoft.com/en-us/dotnet/api/microsoft.aspnetcore.http.extensions.urihelper.getencodedurl?view=aspnetcore-3.1#Microsoft_AspNetCore_Http_Extensions_UriHelper_GetEncodedUrl_Microsoft_AspNetCore_Http_HttpRequest_"">Doc</a><br/><br/>
+    <a href=""https://docs.microsoft.com/en-us/dotnet/api/microsoft.aspnetcore.http.extensions.urihelper.getencodedurl?view=aspnetcore-6.0#Microsoft_AspNetCore_Http_Extensions_UriHelper_GetEncodedUrl_Microsoft_AspNetCore_Http_HttpRequest_"">Doc</a><br/><br/>
     <p style=""color:red;"">{ context.Request.GetEncodedUrl() }</p>
 
     <p>Click on the links to see what the helper method shows</p>
@@ -30,10 +22,7 @@ namespace PracticalAspNetCore
         <li><a href=""/"">/</a></li>
     </ul>
 </body>          
-</html>         ");
-            });
-        }
-    }
+</html>");
+});
 
-
-}
+app.Run();
