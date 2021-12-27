294f20cfe8e352c92435e8ba680cc4dfe00dcb2d
Dody Gunawinata <dodyg@silverkeytech.com>
2021-12-27 10:16:35 +0200

    Update to .NET 6

--- projects/rewrite/rewrite-2/Program.cs
+++ projects/rewrite/rewrite-2/Program.cs
@@ -1,40 +1,22 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Rewrite;
-using Microsoft.AspNetCore.Routing;
-using Microsoft.Extensions.Hosting;
 
-namespace PracticalAspNetCore
+var app = WebApplication.Create();
+
+var options = new RewriteOptions().AddRedirect("([/_0-9a-zA-Z-]+)\\.([^/]+)$", "/?path=$1&ext=$2"); //redirect any path that ends with .html 
+
+app.UseRewriter(options);
+
+app.MapGet("", (context) =>
 {
-    public class Startup
-    {
-        public void Configure(IApplicationBuilder app)
-        {
-            app.UseRouting();
-
-            var options = new RewriteOptions()
-               .AddRedirect("([/_0-9a-zA-Z-]+)\\.([^/]+)$", "/?path=$1&ext=$2"); //redirect any path that ends with .html 
-
-            app.UseRewriter(options);
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGet("", (context) =>
-                {
-                    context.Response.Headers.Add("content-type", "text/html");
-                    var path = context.Request.Query["Path"];
-                    var ext = context.Request.Query["Ext"];
-                    return context.Response.WriteAsync($@"Always display this page when path ends with an extension (e.g. .html or .aspx) and capture the their values. 
+    context.Response.Headers.Add("content-type", "text/html");
+    var path = context.Request.Query["Path"];
+    var ext = context.Request.Query["Ext"];
+    return context.Response.WriteAsync($@"Always display this page when path ends with an extension (e.g. .html or .aspx) and capture the their values. 
                     For example <a href=""/hello-world.html"">/hello-world.html</a> or <a href=""/welcome/everybody/inthis/train.aspx"">/welcome/everybody/inthis/train.aspx</a>.
                     <br/><br/>
                     Query String ""path"" = {path}<br/>
                     Query String ""ext"" = {ext}<br/>
                     ");
-                });
-            });
-        }
-    }
+});
 
-
-}
+app.Run();
