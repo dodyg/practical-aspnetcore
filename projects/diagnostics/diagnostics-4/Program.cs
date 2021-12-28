2b730ec693e50d4e874766292d88504c2409ce2d
Dody Gunawinata <dodyg@silverkeytech.com>
2021-12-28 16:05:10 +0200

    Upgrade to .NET 6

--- projects/diagnostics/diagnostics-4/Program.cs
+++ projects/diagnostics/diagnostics-4/Program.cs
@@ -1,49 +1,26 @@
-using System;
 using System.Text.Encodings.Web;
-using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Diagnostics;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.Hosting;
 
-namespace PracticalAspNetCore
+var app = WebApplication.Create();
+
+app.UseExceptionHandler("/GlobalError");
+
+app.Map("/GlobalError", errorApp =>
 {
-    public class Startup
+    errorApp.Run(async context =>
     {
-        public void Configure(IApplicationBuilder app)
+        context.Response.StatusCode = 500;
+        context.Response.ContentType = "text/html";
+
+        var feature = context.Features.Get<IExceptionHandlerFeature>();
+
+        if (feature != null)
         {
-            app.UseExceptionHandler("/GlobalError");
-
-            app.Map("/GlobalError", errorApp =>
-            {
-                errorApp.Run(async context =>
-                {
-                    context.Response.StatusCode = 500;
-                    context.Response.ContentType = "text/html";
-
-                    var feature = context.Features.Get<IExceptionHandlerFeature>();
-
-                    if (feature != null)
-                    {
-                        await context.Response.WriteAsync($"<h1>Custom Error Page</h1> {HtmlEncoder.Default.Encode(feature.Error.Message)}");
-                        await context.Response.WriteAsync($"<hr />{HtmlEncoder.Default.Encode(feature.Error.Source)}");
-                    }
-                });
-            });
-
-            app.Run(context => throw new ApplicationException("Hello World Exception"));
+            await context.Response.WriteAsync($"<h1>Custom Error Page</h1> {HtmlEncoder.Default.Encode(feature.Error.Message)}");
+            await context.Response.WriteAsync($"<hr />{HtmlEncoder.Default.Encode(feature.Error.Source)}");
         }
-    }
+    });
+});
 
-    public class Program
-    {
-        public static void Main(string[] args) =>
-            CreateHostBuilder(args).Build().Run();
-
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                    webBuilder.UseStartup<Startup>()
-                );
-    }
-}
+app.Run(context => throw new ApplicationException("Hello World Exception"));
+app.Run();
