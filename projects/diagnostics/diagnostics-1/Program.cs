d3c04842ab161e113dc3ce7c006cd8a60b3b5ffa
Dody Gunawinata <dodyg@silverkeytech.com>
2021-12-28 15:53:27 +0200

    Upgrade to .NET 6

--- projects/diagnostics/diagnostics-1/Program.cs
+++ projects/diagnostics/diagnostics-1/Program.cs
@@ -1,25 +1,4 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.Extensions.Hosting;
-namespace PracticalAspNetCore
-{
-    public class Startup
-    {
-        public void Configure(IApplicationBuilder app)
-        {
-            app.UseWelcomePage();
-        }
-    }
-
-    public class Program
-    {
-        public static void Main(string[] args) =>
-            CreateHostBuilder(args).Build().Run();
-
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                    webBuilder.UseStartup<Startup>()
-                    );
-    }
-}
+var app =  WebApplication.Create();
+app.UseWelcomePage();
+app.Run();
+ 
