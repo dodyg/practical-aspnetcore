a10544dc61de5d96d11c49bbb4195f60ba768bcb
Dody Gunawinata <dodyg@silverkeytech.com>
2021-12-28 15:56:25 +0200

    Upgrade to .NET 6

--- projects/diagnostics/diagnostics-2/Program.cs
+++ projects/diagnostics/diagnostics-2/Program.cs
@@ -1,28 +1,6 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.Extensions.DependencyInjection;
-using System;
-using Microsoft.Extensions.Hosting;
+var app = WebApplication.Create();
 
-namespace PracticalAspNetCore
-{
-    public class Startup
-    {
-        public void Configure(IApplicationBuilder app)
-        {
-            app.UseDeveloperExceptionPage(); //Don't use this in production
-            app.Run(context => throw new ApplicationException("Hello World Exception"));
-        }
-    }
+app.UseDeveloperExceptionPage(); //Don't use this in production
+app.Run(context => throw new ApplicationException("Hello World Exception"));
 
-    public class Program
-    {
-        public static void Main(string[] args) =>
-            CreateHostBuilder(args).Build().Run();
-
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                    webBuilder.UseStartup<Startup>());
-    }
-}
+app.Run();
