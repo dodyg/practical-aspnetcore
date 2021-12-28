992d1ce866c0c50c49d9c24633524f6cd5631bd1
Dody Gunawinata <dodyg@silverkeytech.com>
2021-12-28 15:58:25 +0200

    Upgrade to .NET 6

--- projects/diagnostics/diagnostics-3/Program.cs
+++ projects/diagnostics/diagnostics-3/Program.cs
@@ -1,49 +1,25 @@
-using System;
 using System.Text.Encodings.Web;
-using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Diagnostics;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.Hosting;
 
-namespace PracticalAspNetCore
+var app = WebApplication.Create();
+
+app.UseExceptionHandler(errorApp =>
 {
-    public class Startup
+    errorApp.Run(async context =>
     {
-        public void Configure(IApplicationBuilder app)
+        context.Response.StatusCode = 500;
+        context.Response.ContentType = "text/html";
+
+        var feature = context.Features.Get<IExceptionHandlerFeature>();
+
+        if (feature != null)
         {
-            app.UseExceptionHandler(errorApp =>
-            {
-                errorApp.Run(async context =>
-                {
-                    context.Response.StatusCode = 500;
-                    context.Response.ContentType = "text/html";
-
-                    var feature = context.Features.Get<IExceptionHandlerFeature>();
-
-                    if (feature != null)
-                    {
-                        await context.Response.WriteAsync($"<h1>Custom Error Page</h1> {HtmlEncoder.Default.Encode(feature.Error.Message)}");
-                        await context.Response.WriteAsync($"<hr />{HtmlEncoder.Default.Encode(feature.Error.Source)}");
-                    }
-                });
-            });
-
-            //trigger exception
-            app.Run(context => throw new Exception("Hello World Exception"));
+            await context.Response.WriteAsync($"<h1>Custom Error Page</h1> {HtmlEncoder.Default.Encode(feature.Error.Message)}");
+            await context.Response.WriteAsync($"<hr />{HtmlEncoder.Default.Encode(feature.Error.Source)}");
         }
-    }
+    });
+});
 
-    public class Program
-    {
-        public static void Main(string[] args) =>
-            CreateHostBuilder(args).Build().Run();
-
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                    webBuilder.UseStartup<Startup>()
-                );
-    }
-
-}
+//trigger exception
+app.Run(context => throw new Exception("Hello World Exception"));
+app.Run();
