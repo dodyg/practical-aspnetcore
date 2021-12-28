d3805cd8d62fedd772ca68ab027b6609033773dc
Dody Gunawinata <dodyg@silverkeytech.com>
2021-12-28 16:07:29 +0200

    Upgrade to .NET 6

--- projects/diagnostics/diagnostics-5/Program.cs
+++ projects/diagnostics/diagnostics-5/Program.cs
@@ -1,42 +1,19 @@
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.Hosting;
+var app = WebApplication.Create();
 
-namespace PracticalAspNetCore
+app.UseStatusCodePagesWithRedirects("/error?status={0}");
+
+app.Map("/error", errorApp =>
 {
-    public class Startup
+    errorApp.Run(async context =>
     {
-        public void Configure(IApplicationBuilder app)
-        {
-            app.UseStatusCodePagesWithRedirects("/error?status={0}");
+        await context.Response.WriteAsync($"This is a redirected error message status {context.Request.Query["status"]}");
+    });
+});
 
-            app.Map("/error", errorApp =>
-            {
-                errorApp.Run(async context =>
-                {
-                    await context.Response.WriteAsync($"This is a redirected error message status {context.Request.Query["status"]}");
-                });
-            });
+app.Run(context =>
+{
+    context.Response.StatusCode = 500;//change this as necessary
+        return Task.CompletedTask;
+});
 
-            app.Run(context =>
-            {
-                context.Response.StatusCode = 500;//change this as necessary
-                return Task.CompletedTask;
-            });
-        }
-    }
-
-    public class Program
-    {
-        public static void Main(string[] args) =>
-            CreateHostBuilder(args).Build().Run();
-
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                    webBuilder.UseStartup<Startup>()
-                );
-    }
-}
+app.Run();
