1ecbd7faf8e643e2aac243ed9058e6ed90c3d9a1
Dody Gunawinata <dodyg@silverkeytech.com>
2022-01-06 13:12:33 +0200

    Upgrade to .NET 6

---
+++ projects/endpoint-routing/new-routing-6/Program.cs
@@ -0,0 +1,48 @@
+using Microsoft.AspNetCore.Mvc;
+
+var builder = WebApplication.CreateBuilder();
+builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
+
+var app = builder.Build();
+
+app.MapGet("hello", async context => await context.Response.WriteAsync("hello"));
+app.MapPost("new-hello", async context => await context.Response.WriteAsync("hello"));
+app.MapDelete("hello", async context => await context.Response.WriteAsync("hello"));
+app.MapPut("hello", async context => await context.Response.WriteAsync("hello"));
+app.MapControllers();
+app.MapRazorPages();
+
+app.Map("", async context =>
+{
+    var route = app as IEndpointRouteBuilder;
+
+    foreach (EndpointDataSource x in route.DataSources)
+    {
+        await context.Response.WriteAsync($"{x}\n");
+        foreach (RouteEndpoint e in x.Endpoints)
+        {
+            await context.Response.WriteAsync($"Display Name: {e.DisplayName}\n");
+            await context.Response.WriteAsync($"Route Pattern: {e.RoutePattern.RawText}\n");
+            await context.Response.WriteAsync($"Metadata Count: {e.Metadata.Count}\n");
+            foreach (var mm in e.Metadata)
+            {
+                await context.Response.WriteAsync($"Metadata: {mm}\n");
+            }
+            await context.Response.WriteAsync("\n");
+        }
+        await context.Response.WriteAsync("\n\n");
+    }
+});
+
+app.Run();
+
+[Route("MVC")]
+public class HomeController : Controller
+{
+    [HttpGet("Greeting")]
+    public IActionResult Index() => Content("Oi");
+
+    [HttpPost("Greeting")]
+    public IActionResult Greeting() => Content("Oi");
+}
