d7fcf9802dcf3e4b9168512219122b97c69f1032
Dody Gunawinata <dodyg@silverkeytech.com>
2022-01-10 08:49:44 +0200

    Upgrde to .NET 6

--- projects/endpoint-routing/new-routing-20/Program.cs
+++ projects/endpoint-routing/new-routing-20/Program.cs
@@ -1,49 +1,19 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Routing;
-using Microsoft.AspNetCore.Connections;
-using System.Buffers;
-using System.Threading.Tasks;
-using Microsoft.Extensions.FileProviders;
-using System.IO;
-using System;
-using Microsoft.AspNetCore.Http.Features;
+var builder = WebApplication.CreateBuilder();
+builder.Services.AddControllersWithViews();
 
-namespace PracticalAspNetCore
+var app = builder.Build();
+app.MapControllerRoute("Admin", "Admin", new
 {
-    public class Startup
-    {
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddControllersWithViews();
-        }
+    Controller = "Admin",
+    Action = "Index"
+});
 
-        public void Configure(IApplicationBuilder app, IHostEnvironment environment)
-        {
-            app.UseRouting();
+app.MapControllerRoute("About", "About", new
+{
+    Controller = "Home",
+    Action = "About"
+});
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllerRoute("Admin", "Admin", new
-                {
-                    Controller = "Admin",
-                    Action = "Index"
-                });
+app.MapControllerRoute("Default", "{controller=Home}/{action=Index}/{id?}");
 
-                endpoints.MapControllerRoute("About", "About", new
-                {
-                    Controller = "Home",
-                    Action = "About"
-                });
-
-                endpoints.MapControllerRoute("Default", "{controller=Home}/{action=Index}/{id?}");
-            });
-        }
-    }
-
-}
+app.Run();
