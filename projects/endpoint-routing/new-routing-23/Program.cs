9bd9165f5c73bbe66938a9933591c361f5127246
Dody Gunawinata <dodyg@silverkeytech.com>
2022-01-12 10:17:06 +0200

    Upgrade .NET 6

---
+++ projects/endpoint-routing/new-routing-23/Program.cs
@@ -0,0 +1,22 @@
+var builder = WebApplication.CreateBuilder();
+builder.Services.AddRazorPages();
+
+builder.WebHost.ConfigureKestrel(k =>
+{
+    k.ListenLocalhost(8111);
+    k.ListenLocalhost(8112); 
+});
+
+var app = builder.Build();
+
+app.MapGet("/", async context =>
+{
+    await context.Response.WriteAsync("hello world from http://localhost:8111");
+}).RequireHost("localhost:8111");
+
+app.MapGet("/", async context =>
+{
+    await context.Response.WriteAsync("hello world from http://localhost:8112");
+}).RequireHost("localhost:8112");
+
+app.Run();
