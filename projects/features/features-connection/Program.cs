5bc9924969c0e97461f4e909a5a491aba9c2d0dd
Dody Gunawinata <dodyg@silverkeytech.com>
2022-01-14 11:10:54 +0200

    Upgrade .NET 6

---
+++ projects/features/features-connection/Program.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Http.Features;
+
+var app = WebApplication.Create();
+app.Run(context =>
+{
+    var connection = context.Features.Get<IHttpConnectionFeature>();
+    var str = string.Empty;
+    str += $"Local IP:Port : {connection.LocalIpAddress}:{connection.LocalPort}\n";
+    str += $"Remote IP:Port : {connection.RemoteIpAddress}:{connection.RemotePort}\n";
+    str += $"Connection Id : {connection.ConnectionId}\n";
+
+    return context.Response.WriteAsync($"{str}");
+});
+app.Run();
