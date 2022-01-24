efc2ce850e4b1b10d161ed35f18241db9ab45915
Dody Gunawinata <dodyg@silverkeytech.com>
2022-01-24 09:20:42 +0700

    Upgrade to .NET 6

--- projects/mvc/view-component/view-component-2/Program.cs
+++ projects/mvc/view-component/view-component-2/Program.cs
@@ -1,45 +1,14 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.AspNetCore;
-using Microsoft.Extensions.Configuration;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Hosting;
 
-namespace PracticalAspNetCore
+var builder = WebApplication.CreateBuilder();
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+app.MapDefaultControllerRoute();
+app.Run();
+
+public class HomeController : Controller
 {
-    public class Startup
-    {
-        public void ConfigureServices(IServiceCollection services) =>
-            services.AddControllersWithViews();
-
-        public void Configure(IApplicationBuilder app)
-        {
-            app.UseRouting();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapDefaultControllerRoute();
-            });
-        }
-    }
-
-    public class HomeController : Controller
-    {
-        public ActionResult Index() => View();
-    }
-
-    public class Program
-    {
-        public static void Main(string[] args) =>
-            CreateHostBuilder(args).Build().Run();
-
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                    webBuilder.UseStartup<Startup>()
-                );
-    }
+    public ActionResult Index() => View();
 }
+
--- projects/mvc/view-component/view-component-4/Program.cs
+++ projects/mvc/view-component/view-component-4/Program.cs
@@ -1,52 +1,21 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.AspNetCore;
-using Microsoft.Extensions.Configuration;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Hosting;
 
-namespace PracticalAspNetCore
+var builder = WebApplication.CreateBuilder();
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+app.MapDefaultControllerRoute();
+app.Run();
+
+public class RepeatMessage
 {
-    public class Startup
-    {
-        public void ConfigureServices(IServiceCollection services) =>
-            services.AddControllersWithViews();
+    public string Content { get; set; }
 
-        public void Configure(IApplicationBuilder app)
-        {
-            app.UseRouting();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapDefaultControllerRoute();
-            });
-        }
-    }
-
-    public class RepeatMessage
-    {
-        public string Content { get; set; }
-
-        public int Repeat { get; set; }
-    }
-
-    public class HomeController : Controller
-    {
-        public ActionResult Index() => View();
-    }
-
-    public class Program
-    {
-        public static void Main(string[] args) =>
-            CreateHostBuilder(args).Build().Run();
-
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                    webBuilder.UseStartup<Startup>()
-                );
-    }
+    public int Repeat { get; set; }
 }
+
+public class HomeController : Controller
+{
+    public ActionResult Index() => View();
+}
+
