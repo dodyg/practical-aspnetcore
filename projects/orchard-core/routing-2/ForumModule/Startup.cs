1c04890ff8ed61a49823a361d1faf6779ed0dd98
Dody Gunawinata <dodyg@silverkeytech.com>
2022-01-26 15:19:27 +0800

    Upgrade to .NET 6

--- projects/orchard-core/routing-2/ForumModule/Startup.cs
+++ projects/orchard-core/routing-2/ForumModule/Startup.cs
@@ -1,19 +1,17 @@
-using System;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.DependencyInjection;
 using OrchardCore.Modules;
 
-namespace ForumModule
-{
-    public class Startup : StartupBase
-    {
-        public override void ConfigureServices(IServiceCollection services)
-        {
-        }
+namespace ForumModule;
 
-        public override void Configure(IApplicationBuilder builder, IEndpointRouteBuilder routes, IServiceProvider serviceProvider)
-        {
-        }
+public class Startup : StartupBase
+{
+    public override void ConfigureServices(IServiceCollection services)
+    {
+    }
+
+    public override void Configure(IApplicationBuilder builder, IEndpointRouteBuilder routes, IServiceProvider serviceProvider)
+    {
     }
 }
--- projects/orchard-core/routing-2/TicketModule/Startup.cs
+++ projects/orchard-core/routing-2/TicketModule/Startup.cs
@@ -1,19 +1,17 @@
-using System;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.DependencyInjection;
 using OrchardCore.Modules;
 
-namespace TicketModule
-{
-    public class Startup : StartupBase
-    {
-        public override void ConfigureServices(IServiceCollection services)
-        {
-        }
+namespace TicketModule;
 
-        public override void Configure(IApplicationBuilder builder, IEndpointRouteBuilder routes, IServiceProvider serviceProvider)
-        {
-        }
+public class Startup : StartupBase
+{
+    public override void ConfigureServices(IServiceCollection services)
+    {
+    }
+
+    public override void Configure(IApplicationBuilder builder, IEndpointRouteBuilder routes, IServiceProvider serviceProvider)
+    {
     }
 }
