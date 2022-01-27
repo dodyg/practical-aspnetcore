eddcf95ae07976d52a0aa9d596e9fc9e9bf52add
Dody Gunawinata <dodyg@silverkeytech.com>
2022-01-27 22:45:02 +0800

    Upgrade to .NET 6

--- projects/orchard-core/static-files/Host/Program.cs
+++ projects/orchard-core/static-files/Host/Program.cs
@@ -1,44 +1,7 @@
+var builder = WebApplication.CreateBuilder();
+builder.Services.AddOrchardCore().AddMvc();
 
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.DependencyInjection;
+var app = builder.Build();
+app.UseOrchardCore();
+app.Run();
 
-namespace Host
-{
-    public class Startup
-    {
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddOrchardCore().AddMvc();
-        }
-
-        public void Configure(IApplicationBuilder app, IHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.UseOrchardCore();
-        }
-    }
-
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
-
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Microsoft.Extensions.Hosting.Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>().
-                    UseEnvironment("Development");
-                });
-    }
-}
