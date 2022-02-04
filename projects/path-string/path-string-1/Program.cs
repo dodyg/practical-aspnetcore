a59495bc660ddb04bc9e0d0271878b9022866f51
Dody Gunawinata <dodyg@silverkeytech.com>
2022-02-04 09:51:06 +0800

    Upgrade to .NET 6

---
+++ projects/path-string/path-string-1/Program.cs
@@ -0,0 +1,27 @@
+var app = WebApplication.Create();
+app.Run(async context =>
+{
+    var protocol = context.Request.IsHttps ? "https://": "http://";
+    var host = protocol + context.Request.Host;
+    context.Response.Headers["ContentType"] = "text/html";
+
+    await context.Response.WriteAsync($@"
+        <html>
+        <body>
+            <h1>HttpContext.Request.Path</h1>
+            <a href=""{host}/hello"" target=""_blank"">{host}/hello</a> <br/>
+            <a href=""{host}//double-slash"" target=""_blank"">{host}//double-slash</a> <br/>
+            <a href=""{host}/double-slash//version-2"" target=""_blank"">{host}/double-slash//version-2</a> <br/>
+            <a href=""{host}/about-us/"" target=""_blank"">{host}/about-us/</a> <br/>
+            <a href=""{host}/catalog/?id=10"" target=""_blank"">{host}/catalog/?id=10</a> <br/>
+            <a href=""{host}/admin/index?secure=true"" target=""_blank"">{host}/admin/index?secure=true</a> <br/>
+            <p>
+                Value of HttpContext.Request.Path :
+                { context.Request.Path }
+            </p>
+        </body>
+        </html>
+        ");
+});
+
+app.Run();
