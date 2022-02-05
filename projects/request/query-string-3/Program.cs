c0d3140ba8d8f866701dbe4bae50bdcf7d6fb5ab
Dody Gunawinata <dodyg@silverkeytech.com>
2022-02-05 12:02:33 +0800

    update to .NET 6

--- projects/request/query-string-3/Program.cs
+++ projects/request/query-string-3/Program.cs
@@ -1,32 +1,20 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.Hosting;
+var app = WebApplication.Create();
 
-namespace PracticalAspNetCore
+app.Run(async context =>
 {
-    public class Startup
+    context.Response.Headers.Add("content-type", "text/html");
+
+    await context.Response.WriteAsync("<html><body>");
+    await context.Response.WriteAsync("<h1>All query string</h1>");
+    await context.Response.WriteAsync(@"<a href=""?message=hello&message=world&message=again&isTrue=1&morning=good"">click this link to add query string</a><br/><br/>");
+    await context.Response.WriteAsync("<ul>");
+    foreach (var v in context.Request.Query)
     {
-        public void Configure(IApplicationBuilder app)
-        {
-            app.Run(async context =>
-            {
-                context.Response.Headers.Add("content-type", "text/html");
-
-                await context.Response.WriteAsync("<html><body>");
-                await context.Response.WriteAsync("<h1>All query string</h1>");
-                await context.Response.WriteAsync(@"<a href=""?message=hello&message=world&message=again&isTrue=1&morning=good"">click this link to add query string</a><br/><br/>");
-                await context.Response.WriteAsync("<ul>");
-                foreach (var v in context.Request.Query)
-                {
-                    string str = v.Value; //implicit conversion from StringValues to String
-                    await context.Response.WriteAsync($"<li>{v.Key} - {str}</li>");
-                }
-                await context.Response.WriteAsync("</ul>");
-                await context.Response.WriteAsync("</body></html>");
-            });
-        }
+        string str = v.Value; //implicit conversion from StringValues to String
+        await context.Response.WriteAsync($"<li>{v.Key} - {str}</li>");
     }
+    await context.Response.WriteAsync("</ul>");
+    await context.Response.WriteAsync("</body></html>");
+});
 
-
-}
+app.Run();
