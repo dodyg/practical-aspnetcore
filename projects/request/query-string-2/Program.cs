786e8eaa45e2f6a62eab9354e08755e4f0ee9645
Dody Gunawinata <dodyg@silverkeytech.com>
2022-02-05 12:04:48 +0800

    Upgrade to .NET 6

--- projects/request/query-string-2/Program.cs
+++ projects/request/query-string-2/Program.cs
@@ -1,38 +1,24 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Primitives;
-using Microsoft.AspNetCore;
-using Microsoft.Extensions.Hosting;
 
-namespace PracticalAspNetCore
+var app = WebApplication.Create();
+
+//These are the three default services available at Configure
+app.Run(async context =>
 {
-    public class Startup
+    context.Response.Headers.Add("Content-Type", "text/html");
+
+    StringValues queryString = context.Request.Query["message"];
+
+    await context.Response.WriteAsync("<html><body>");
+    await context.Response.WriteAsync("<h1>Query String with multiple values</h1>");
+    await context.Response.WriteAsync(@"<a href=""?message=hello&message=world&message=again"">click this link to add query string</a><br/><br/>");
+    await context.Response.WriteAsync("<ul>");
+    foreach (string v in queryString)
     {
-        public void Configure(IApplicationBuilder app)
-        {
-            //These are the three default services available at Configure
-            app.Run(async context =>
-            {
-                context.Response.Headers.Add("Content-Type", "text/html");
-
-                StringValues queryString = context.Request.Query["message"];
-
-                await context.Response.WriteAsync("<html><body>");
-                await context.Response.WriteAsync("<h1>Query String with multiple values</h1>");
-                await context.Response.WriteAsync(@"<a href=""?message=hello&message=world&message=again"">click this link to add query string</a><br/><br/>");
-                await context.Response.WriteAsync("<ul>");
-                foreach (string v in queryString)
-                {
-                    await context.Response.WriteAsync($"<li>{v}</li>");
-                }
-                await context.Response.WriteAsync("</ul>");
-                await context.Response.WriteAsync("</body></html>");
-            });
-        }
+        await context.Response.WriteAsync($"<li>{v}</li>");
     }
+    await context.Response.WriteAsync("</ul>");
+    await context.Response.WriteAsync("</body></html>");
+});
 
-
-}
+app.Run();
