31ae931215ccdefd0d4b562d536ec1ad5a1e3785
Dody Gunawinata <dodyg@silverkeytech.com>
2022-02-05 12:07:07 +0800

    Upgrade to .NET 6

--- projects/request/query-string-1/Program.cs
+++ projects/request/query-string-1/Program.cs
@@ -1,32 +1,16 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Primitives;
-using Microsoft.Extensions.Hosting;
-
-namespace PracticalAspNetCore
+var app = WebApplication.Create();
+//These are the three default services available at Configure
+app.Run(async context =>
 {
-    public class Startup
-    {
-        public void Configure(IApplicationBuilder app)
-        {
-            //These are the three default services available at Configure
-            app.Run(async context =>
-            {
-                context.Response.Headers.Add("content-type", "text/html");
+    context.Response.Headers.Add("content-type", "text/html");
 
-                StringValues queryString = context.Request.Query["message"];
+    StringValues queryString = context.Request.Query["message"];
 
-                await context.Response.WriteAsync("<html><body>");
-                await context.Response.WriteAsync("<h1>Query String with a single value</h1>");
-                await context.Response.WriteAsync(@"<a href=""?message=hello world"">click this link to add query string</a><br/><br/>");
-                await context.Response.WriteAsync($"'Message' query string: {queryString}");
-                await context.Response.WriteAsync("</body></html>");
-            });
-        }
-    }
-
-
-}
+    await context.Response.WriteAsync("<html><body>");
+    await context.Response.WriteAsync("<h1>Query String with a single value</h1>");
+    await context.Response.WriteAsync(@"<a href=""?message=hello world"">click this link to add query string</a><br/><br/>");
+    await context.Response.WriteAsync($"'Message' query string: {queryString}");
+    await context.Response.WriteAsync("</body></html>");
+});
+app.Run();
