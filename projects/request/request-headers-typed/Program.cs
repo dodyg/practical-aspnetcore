42d12d96be2fb115a1a6a2953cc8edb03931e263
Dody Gunawinata <dodyg@silverkeytech.com>
2022-02-05 11:56:23 +0800

    Upgrade to .NET 6

--- projects/request/request-headers-typed/Program.cs
+++ projects/request/request-headers-typed/Program.cs
@@ -1,28 +1,14 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.Logging;
-using System.Linq;
-using Microsoft.Extensions.Hosting;
+var app = WebApplication.Create();
 
-namespace PracticalAspNetCore
+app.Run(context =>
 {
-    public class Startup
-    {
-        public void Configure(IApplicationBuilder app)
-        {
-            app.Run(context =>
-            {
-                var typedHeaders = context.Request.GetTypedHeaders();
+    var typedHeaders = context.Request.GetTypedHeaders();
 
-                return context.Response.WriteAsync($@"
+    return context.Response.WriteAsync($@"
 There are more common HTTP headers properties available in HttpContext.Request.GetTypedHeaders()              
 Accept: {typedHeaders.Accept[0]}
 Accept Language : {typedHeaders.AcceptLanguage.FirstOrDefault()?.Value}                
-                ");
-            });
-        }
-    }
+    ");
+});
 
-
-}
+app.Run();
