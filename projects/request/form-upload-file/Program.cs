d8820f00a9efc89d683706876ca0b438f2b15ba1
Dody Gunawinata <dodyg@silverkeytech.com>
2022-02-06 08:46:22 +0800

    Upgrade to .NET 6

--- projects/request/form-upload-file/Program.cs
+++ projects/request/form-upload-file/Program.cs
@@ -1,67 +1,51 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Routing;
-using System.IO;
-using Microsoft.Extensions.Hosting;
+var app = WebApplication.Create();
 
-namespace PracticalAspNetCore
+app.MapGet("", async context =>
 {
-    public class Startup
-    {
-        public void Configure(IApplicationBuilder app, IHostEnvironment env)
-        {
-            app.UseRouting();
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGet("", async context =>
-                {
-                    context.Response.Headers.Add("content-type", "text/html");
+    context.Response.Headers.Add("content-type", "text/html");
 
-                    var body = $@"
+    var body = $@"
+                <html><body>
                 <h1>Upload File</h1>
                 <form action=""Upload"" method=""post"" enctype=""multipart/form-data"">
                         <input type=""file"" name=""file"" />
                         <input type=""submit"" value=""Upload"" />
                 </form>
+                </body></html>
                 ";
 
-                    await context.Response.WriteAsync(body);
-                });
+    await context.Response.WriteAsync(body);
+});
 
-                endpoints.MapPost("Upload", async context =>
-                {
-                    if (context.Request.HasFormContentType)
-                    {
-                        var form = await context.Request.ReadFormAsync();
+app.MapPost("Upload", async context =>
+{
+    if (context.Request.HasFormContentType)
+    {
+        var form = await context.Request.ReadFormAsync();
 
-                        foreach (var f in form.Files)
-                        {
-                            using (var body = f.OpenReadStream())
-                            {
-                                var fileName = Path.Combine(env.ContentRootPath, f.FileName);
-                                File.WriteAllBytes(fileName, ReadFully(body));
-                                await context.Response.WriteAsync($"Uploaded file written to {fileName}");
-                            }
-                        }
-                    }
-                    await context.Response.WriteAsync("");
-                });
-            });
-        }
-
-        public static byte[] ReadFully(Stream input)
+        foreach (var f in form.Files)
         {
-            byte[] buffer = new byte[16 * 1024];
-            using var ms = new MemoryStream();
-            int read;
-            while ((read = input.Read(buffer, 0, buffer.Length)) > 0)
+            using (var body = f.OpenReadStream())
             {
-                ms.Write(buffer, 0, read);
+                var fileName = Path.Combine(app.Environment.ContentRootPath, f.FileName);
+                File.WriteAllBytes(fileName, ReadFully(body));
+                await context.Response.WriteAsync($"Uploaded file written to {fileName}");
             }
-            return ms.ToArray();
         }
     }
+    await context.Response.WriteAsync("");
+});
 
+app.Run();
 
+static byte[] ReadFully(Stream input)
+{
+    byte[] buffer = new byte[16 * 1024];
+    using var ms = new MemoryStream();
+    int read;
+    while ((read = input.Read(buffer, 0, buffer.Length)) > 0)
+    {
+        ms.Write(buffer, 0, read);
+    }
+    return ms.ToArray();
 }
