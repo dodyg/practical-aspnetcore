eaa69b25a56e59f2d8d5d260983224a0eb053820
Dody Gunawinata <dodyg@silverkeytech.com>
2022-02-21 10:14:16 +0800

    Change rollforward to  major

---
+++ projects/mvc/localization/mvc-localization-5/Program.cs
@@ -0,0 +1,39 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Globalization;
+using Microsoft.AspNetCore.Localization;
+
+var builder = WebApplication.CreateBuilder();
+builder.Services.AddLocalization(opts => { opts.ResourcesPath = "Resources"; });
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+app.UseStaticFiles();
+
+var supportedCultures = new List<CultureInfo>
+{
+    new CultureInfo("fr-FR")
+};
+
+var options = new RequestLocalizationOptions
+{
+    DefaultRequestCulture = new RequestCulture("fr-FR"),
+    SupportedCultures = supportedCultures,
+    SupportedUICultures = supportedCultures
+};
+
+app.UseRequestLocalization(options);
+app.MapDefaultControllerRoute();
+app.Run();
+
+// Leave this class empty
+public class Global
+{
+
+}
+public class HomeController : Controller
+{
+    public ActionResult Index()
+    {
+        return View();
+    }
+}
