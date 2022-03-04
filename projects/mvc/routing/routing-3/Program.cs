f092abaf93e4dd551ea8c952de82f2eeb1d7fb3e
Dody Gunawinata <dodyg@silverkeytech.com>
2022-03-04 11:41:10 +0800

    Upgrade .NET 6

---
+++ projects/mvc/routing/routing-3/Program.cs
@@ -0,0 +1,33 @@
+using Microsoft.AspNetCore.Mvc;
+var builder = WebApplication.CreateBuilder();
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+app.MapControllers();
+
+app.Run();
+
+[Route("")]
+[Route("Home")]
+[Route("Home/Index")]
+public class HomeController : Controller
+{
+    public ActionResult Index()
+    {
+        return new ContentResult
+        {
+            Content = @"
+                <html><body>
+                <b>Hello World</b>
+                <br/><br/>
+                The following links call the same controller and action.
+                <ul>
+                    <li><a href=""/"">/</a></li>
+                    <li><a href=""/home"">/home</a></li>
+                    <li><a href=""/home/index"">/home/index</a></li>
+                </ul>
+                </body></html>",
+            ContentType = "text/html"
+        };
+    }
+}
