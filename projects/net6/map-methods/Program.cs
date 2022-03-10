301114500f6bb7aab1225ecf8e7eb5868ce62176
Dody Gunawinata <dodyg@silverkeytech.com>
2022-03-10 21:57:52 +0800

    Refactor

--- projects/net6/map-methods/Program.cs
+++ projects/net6/map-methods/Program.cs
@@ -1,70 +1,46 @@
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Hosting;
-namespace PracticalAspNetCore
-{
-    public class Startup
-    {
-        public record Greeting(string Message);
 
-        object Greet([FromBody] Greeting greet, HttpContext context) 
+object Greet([FromBody] Greeting greet, HttpContext context) 
             => new { Message = context.Request.Method + ":" + greet.Message };
 
-        public void Configure(IApplicationBuilder app)
-        {
-            app.UseRouting();
-                
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapMethods("/greet", new[] { "POST", "PUT", "PATCH" }, Greet);
-           
-                endpoints.MapGet("/", async context =>
-                {
-                    string page = @"<!doctype html><html><body>
-                            <div id=""response""></div>
-                            <button type=""button"" onclick=""send('POST')"">Send POST</button>
-                            <button type=""button"" onclick=""send('PUT')"">Send PUT</button>
-                            <button type=""button"" onclick=""send('PATCH')"">Send PATCH</button>
-                            <script>
-                                async function postData(method, url = '', data = {}) {
-                                const response = await fetch(url, {
-                                    method: method, 
-                                    headers: {
-                                    'Content-Type': 'application/json'
-                                    },
-                                    body: JSON.stringify({ message : ""Hello World""}) 
-                                });
-                                return response.json(); // parses JSON response into native JavaScript objects
-                                }
-                                
-                                function send(method){
-                                    postData(method, window.location + 'greet', { answer: 42 })
-                                    .then(data => {
-                                        document.getElementById(""response"").innerHTML = data.message;
-                                    });
-                                }
-                            </script>
-                        </body></html>
-                    ";
+var app = WebApplication.Create();
 
-                    context.Response.Headers.Append("Content-Type", "text/html");
-                    await context.Response.WriteAsync(page);
+app.MapMethods("/greet", new[] { "POST", "PUT", "PATCH" }, Greet);
+
+app.MapGet("/", async context =>
+{
+    string page = @"<!doctype html><html><body>
+            <div id=""response""></div>
+            <button type=""button"" onclick=""send('POST')"">Send POST</button>
+            <button type=""button"" onclick=""send('PUT')"">Send PUT</button>
+            <button type=""button"" onclick=""send('PATCH')"">Send PATCH</button>
+            <script>
+                async function postData(method, url = '', data = {}) {
+                const response = await fetch(url, {
+                    method: method, 
+                    headers: {
+                    'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({ message : ""Hello World""}) 
                 });
-            });
-        }
-    }
+                return response.json(); // parses JSON response into native JavaScript objects
+                }
+                
+                function send(method){
+                    postData(method, window.location + 'greet', { answer: 42 })
+                    .then(data => {
+                        document.getElementById(""response"").innerHTML = data.message;
+                    });
+                }
+            </script>
+        </body></html>
+    ";
 
-    public class Program
-    {
-        public static void Main(string[] args) =>
-            CreateHostBuilder(args).Build().Run();
+    context.Response.Headers.Append("Content-Type", "text/html");
+    await context.Response.WriteAsync(page);
+});
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                    webBuilder.UseStartup<Startup>()
-                );
-    }
-}
+app.Run();
+
+public record Greeting(string Message);
+
