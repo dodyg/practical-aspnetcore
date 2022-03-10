554e6eb5f6dd8a3d909bc6d840957fe3ffe33a1d
Dody Gunawinata <dodyg@silverkeytech.com>
2022-03-10 22:03:40 +0800

    Clean up

--- projects/net6/map-post-2/Program.cs
+++ projects/net6/map-post-2/Program.cs
@@ -1,70 +1,45 @@
 using System;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Hosting;
-namespace PracticalAspNetCore
+
+var app = WebApplication.Create();
+
+app.MapPost("/greet", MyApi.Greet);
+
+app.MapGet("/", async context =>
 {
-    public record Greeting(string Message);
-
-    public static class MyApi
-    {
-        public static IResult Greet([FromBody] Greeting greet) => Results.Json(new { greet.Message });
-    }
-
-    public class Startup
-    {
-        public void Configure(IApplicationBuilder app)
-        {
-            app.UseRouting();
-                
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapPost("/greet", MyApi.Greet);
-           
-                endpoints.MapGet("/", async context =>
-                {
-                    string page = @"<!doctype html><html><body>
-                            <div id=""response""></div><button type=""button"" onclick=""send()"">Click</button>
-                            <script>
-                                async function postData(url = '', data = {}) {
-                                const response = await fetch(url, {
-                                    method: 'POST', 
-                                    headers: {
-                                    'Content-Type': 'application/json'
-                                    },
-                                    body: JSON.stringify({ message : ""Hello World""}) 
-                                });
-                                return response.json(); // parses JSON response into native JavaScript objects
-                                }
-                                
-                                function send(){
-                                    postData(window.location + 'greet', { answer: 42 })
-                                    .then(data => {
-                                        document.getElementById(""response"").innerHTML = data.message;
-                                    });
-                                }
-                            </script>
-                        </body></html>
-                    ";
-
-                    context.Response.Headers.Append("Content-Type", "text/html");
-                    await context.Response.WriteAsync(page);
+    string page = @"<!doctype html><html><body>
+            <div id=""response""></div><button type=""button"" onclick=""send()"">Click</button>
+            <script>
+                async function postData(url = '', data = {}) {
+                const response = await fetch(url, {
+                    method: 'POST', 
+                    headers: {
+                    'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({ message : ""Hello World""}) 
                 });
-            });
-        }
-    }
+                return response.json(); // parses JSON response into native JavaScript objects
+                }
+                
+                function send(){
+                    postData(window.location + 'greet', { answer: 42 })
+                    .then(data => {
+                        document.getElementById(""response"").innerHTML = data.message;
+                    });
+                }
+            </script>
+        </body></html>
+    ";
 
-    public class Program
-    {
-        public static void Main(string[] args) =>
-            CreateHostBuilder(args).Build().Run();
+    context.Response.Headers.Append("Content-Type", "text/html");
+    await context.Response.WriteAsync(page);
+});
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                    webBuilder.UseStartup<Startup>()
-                );
-    }
+app.Run();
+
+public record Greeting(string Message);
+
+public static class MyApi
+{
+    public static IResult Greet([FromBody] Greeting greet) => Results.Json(new { greet.Message });
 }
