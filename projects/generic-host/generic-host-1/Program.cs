72c02cd345408fbb4ebd871d3ed01ada9467279b
Dody Gunawinata <dodyg@silverkeytech.com>
2022-04-02 10:56:40 +0200

    Simplify

--- projects/generic-host/generic-host-1/Program.cs
+++ projects/generic-host/generic-host-1/Program.cs
@@ -1,55 +1,38 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.DependencyInjection;
-using System.Threading.Tasks;
-using System.Threading;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+await Host.CreateDefaultBuilder(args)
+        .ConfigureServices((hostContext, services) =>
+        {
+            services.AddHostedService<HelloWorldService>();
+        })
+        .ConfigureLogging(logging =>
+        {
+            logging.ClearProviders();
+            logging.AddConsole();
+            logging.AddFilter((provider, category, logLevel) =>
+            {
+                return !category.Contains("Microsoft");
+            });
+        }).Build()
+        .RunAsync();
 
-namespace PracticalAspNetCore
+public class HelloWorldService : IHostedService
 {
-    public class HelloWorldService : IHostedService
+    ILogger _log;
+
+    public HelloWorldService(ILogger<HelloWorldService> logger)
     {
-        ILogger _log;
-
-        public HelloWorldService(ILogger<HelloWorldService> logger)
-        {
-            _log = logger;
-        }   
-
-        public Task StartAsync(CancellationToken cancellationToken)
-        {
-            _log.LogDebug("Start Hello world");
-            return Task.CompletedTask;
-        }
-
-        public Task StopAsync(CancellationToken cancellationToken)
-        {
-            _log.LogDebug("Goodbye");
-            return Task.CompletedTask;
-        }
+        _log = logger;
     }
 
-    public class Program
+    public Task StartAsync(CancellationToken cancellationToken)
     {
-        public static async Task Main(string[] args)
-        {
-            await CreateHostBuilder(args).Build().RunAsync();
-        }
+        _log.LogDebug("Start Hello world");
+        return Task.CompletedTask;
+    }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureServices((hostContext, services) =>
-                {
-                    services.AddHostedService<HelloWorldService>();
-                })
-                .ConfigureLogging(logging =>
-                {
-                    logging.ClearProviders();
-                    logging.AddConsole();
-                    logging.AddFilter((provider, category, logLevel) =>
-                    {
-                        return !category.Contains("Microsoft");
-                    });
-                });
+    public Task StopAsync(CancellationToken cancellationToken)
+    {
+        _log.LogDebug("Goodbye");
+        return Task.CompletedTask;
     }
 }
+
