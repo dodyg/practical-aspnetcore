f04fe45792cffd219b5768dbbdf48c177bc6f812
Dody Gunawinata <dodyg@silverkeytech.com>
2022-04-03 09:48:46 +0200

    Clean up

--- projects/generic-host/generic-host-5/Program.cs
+++ projects/generic-host/generic-host-5/Program.cs
@@ -1,84 +1,65 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.DependencyInjection;
-using System.Threading.Tasks;
-using Microsoft.Extensions.Hosting;
-using System;
-using System.Threading;
-using Microsoft.Extensions.Logging;
+await new HostBuilder()
+    .ConfigureServices((hostContext, services) =>
+    {
+        services.AddHostedService<CountingService>();
+    })
+    .UseConsoleLifetime()
+    .ConfigureLogging(logging =>
+    {
+        logging.ClearProviders();
+        logging.AddConsole();
+        logging.AddFilter((provider, category, logLevel) =>
+        {
+            return !category.Contains("Microsoft");
+        });
+    })
+    .Build().RunAsync();
 
-namespace PracticalAspNetCore
+public class CountingService : IHostedService, IDisposable
 {
-    public class CountingService : IHostedService, IDisposable
+    CancellationTokenSource _stoppingCts = new CancellationTokenSource();
+
+    Task _executingTask;
+
+    readonly ILogger _log;
+
+    public CountingService(ILogger<CountingService> logger)
     {
-        CancellationTokenSource _stoppingCts = new CancellationTokenSource();
-
-        Task _executingTask;
-
-        readonly ILogger _log;
-
-        public CountingService(ILogger<CountingService> logger)
-        {
-            _log = logger;
-        }
-
-        public Task StartAsync(CancellationToken cancellationToken)
-        {
-            _executingTask = ExecuteAsync(_stoppingCts.Token);
-
-            return _executingTask.IsCompleted ? _executingTask : Task.CompletedTask;
-        }
-
-        async Task ExecuteAsync(CancellationToken cancellationToken)
-        {
-            var count = 0;
-            do
-            {
-                _log.LogDebug(count.ToString());
-                count++;
-                await Task.Delay(1000, cancellationToken);
-            } while (!cancellationToken.IsCancellationRequested);
-        }
-
-        public async Task StopAsync(CancellationToken cancellationToken)
-        {
-            if (_executingTask == null)
-                return;
-
-            try
-            {
-                _stoppingCts.Cancel();
-            }
-            finally
-            {
-                await Task.WhenAny(_executingTask, Task.Delay(Timeout.Infinite, cancellationToken));
-            }
-        }
-
-        public void Dispose() => _stoppingCts.Cancel();
+        _log = logger;
     }
 
-    public class Program
+    public Task StartAsync(CancellationToken cancellationToken)
     {
-        public static async Task Main(string[] args)
-        {
-            var host = new HostBuilder()
-                .ConfigureServices((hostContext, services) =>
-                {
-                    services.AddHostedService<CountingService>();
-                })
-                .UseConsoleLifetime()
-                .ConfigureLogging(logging =>
-                {
-                    logging.ClearProviders();
-                    logging.AddConsole();
-                    logging.AddFilter((provider, category, logLevel) =>
-                    {
-                        return !category.Contains("Microsoft");
-                    });
-                })
-                .Build();
+        _executingTask = ExecuteAsync(_stoppingCts.Token);
 
-            await host.RunAsync();
+        return _executingTask.IsCompleted ? _executingTask : Task.CompletedTask;
+    }
+
+    async Task ExecuteAsync(CancellationToken cancellationToken)
+    {
+        var count = 0;
+        do
+        {
+            _log.LogDebug(count.ToString());
+            count++;
+            await Task.Delay(1000, cancellationToken);
+        } while (!cancellationToken.IsCancellationRequested);
+    }
+
+    public async Task StopAsync(CancellationToken cancellationToken)
+    {
+        if (_executingTask == null)
+            return;
+
+        try
+        {
+            _stoppingCts.Cancel();
+        }
+        finally
+        {
+            await Task.WhenAny(_executingTask, Task.Delay(Timeout.Infinite, cancellationToken));
         }
     }
+
+    public void Dispose() => _stoppingCts.Cancel();
 }
