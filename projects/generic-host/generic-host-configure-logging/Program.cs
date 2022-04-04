be2ee20b5237e5f562f8789ffddb9f8e2c3b567e
Dody Gunawinata <dodyg@silverkeytech.com>
2022-04-04 09:35:43 +0200

    Clean up

--- projects/generic-host/generic-host-configure-logging/Program.cs
+++ projects/generic-host/generic-host-configure-logging/Program.cs
@@ -1,50 +1,33 @@
-using Microsoft.Extensions.DependencyInjection;
-using System.Threading.Tasks;
-using System.Threading;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-
-namespace PracticalAspNetCore
-{
-    public class HelloWorldService : IHostedService
+await new HostBuilder()
+    .ConfigureLogging((hostContext, configLogging) =>
     {
-        readonly ILogger _logger;
-        public HelloWorldService(ILogger<HelloWorldService> logger)
-        {
-            _logger = logger;
-        }
+        configLogging.ClearProviders();
+        configLogging.AddConsole();
+        configLogging.SetMinimumLevel(LogLevel.Trace);
+    })
+    .ConfigureServices((hostContext, services) =>
+    {
+        services.AddHostedService<HelloWorldService>();
+    })
+    .Build().RunAsync();
 
-        public Task StartAsync(CancellationToken cancellationToken)
-        {
-            _logger.LogDebug("Hello world");
-            return Task.CompletedTask;
-        }
-
-        public Task StopAsync(CancellationToken cancellationToken)
-        {
-            _logger.LogDebug("Goodbye");
-            return Task.CompletedTask;
-        }
+public class HelloWorldService : IHostedService
+{
+    readonly ILogger _logger;
+    public HelloWorldService(ILogger<HelloWorldService> logger)
+    {
+        _logger = logger;
     }
 
-    public class Program
+    public Task StartAsync(CancellationToken cancellationToken)
     {
-        public static async Task Main(string[] args)
-        {
-            var host = new HostBuilder()
-                .ConfigureLogging((hostContext, configLogging) =>
-                {
-                    configLogging.ClearProviders();
-                    configLogging.AddConsole();
-                    configLogging.SetMinimumLevel(LogLevel.Trace);
-                })
-                .ConfigureServices((hostContext, services) =>
-                {
-                    services.AddHostedService<HelloWorldService>();
-                })
-                .Build();
+        _logger.LogDebug("Hello world");
+        return Task.CompletedTask;
+    }
 
-            await host.RunAsync();
-        }
+    public Task StopAsync(CancellationToken cancellationToken)
+    {
+        _logger.LogDebug("Goodbye");
+        return Task.CompletedTask;
     }
 }
