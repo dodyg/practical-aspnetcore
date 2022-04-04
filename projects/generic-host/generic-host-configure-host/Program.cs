cc52f5843a1c8aaf63e59ccd775e2459a2e53886
Dody Gunawinata <dodyg@silverkeytech.com>
2022-04-04 09:33:55 +0200

    Clean up

--- projects/generic-host/generic-host-configure-host/Program.cs
+++ projects/generic-host/generic-host-configure-host/Program.cs
@@ -1,68 +1,49 @@
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Configuration;
-using System.Threading.Tasks;
-using System.Threading;
-using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using Microsoft.Extensions.Logging;
-
-namespace PracticalAspNetCore
-{
-    public class HelloWorldService : IHostedService
+await new HostBuilder()
+    .ConfigureHostConfiguration(configHost =>
     {
-        readonly IConfiguration _config;
-        readonly ILogger _log;
-
-        public HelloWorldService(IConfiguration config, ILogger<HelloWorldService> logger)
+        var dict = new Dictionary<string, string>
         {
-            _config = config;
-            _log = logger;
-        }
-
-        public Task StartAsync(CancellationToken cancellationToken)
+            {"Greet", "Hello World"},
+            {"Goodbye", "Goodbye Luv"}
+        };
+        configHost.AddInMemoryCollection(dict);
+    })
+    .ConfigureServices((hostContext, services) =>
+    {
+        services.AddHostedService<HelloWorldService>();
+    })
+    .ConfigureLogging(logging =>
+    {
+        logging.ClearProviders();
+        logging.AddConsole();
+        logging.AddFilter((provider, category, logLevel) =>
         {
-            _log.LogDebug($"{_config["Greet"]}");
-            return Task.CompletedTask;
-        }
+            return !category.Contains("Microsoft");
+        });
+    })
+    .Build().RunAsync();
 
-        public Task StopAsync(CancellationToken cancellationToken)
-        {
-            _log.LogDebug($"{_config["Goodbye"]}");
-            return Task.CompletedTask;
-        }
+public class HelloWorldService : IHostedService
+{
+    readonly IConfiguration _config;
+    readonly ILogger _log;
+
+    public HelloWorldService(IConfiguration config, ILogger<HelloWorldService> logger)
+    {
+        _config = config;
+        _log = logger;
     }
 
-    public class Program
+    public Task StartAsync(CancellationToken cancellationToken)
     {
-        public static async Task Main(string[] args)
-        {
-            var host = new HostBuilder()
-                .ConfigureHostConfiguration(configHost =>
-                {
-                    var dict = new Dictionary<string, string>
-                    {
-                        {"Greet", "Hello World"},
-                        {"Goodbye", "Goodbye Luv"}
-                    };
-                    configHost.AddInMemoryCollection(dict);
-                })
-                .ConfigureServices((hostContext, services) =>
-                {
-                    services.AddHostedService<HelloWorldService>();
-                })
-                .ConfigureLogging(logging =>
-                {
-                  logging.ClearProviders();
-                  logging.AddConsole();
-                  logging.AddFilter((provider, category, logLevel) =>
-                  {
-                      return !category.Contains("Microsoft");
-                  });
-                })
-                .Build();
+        _log.LogDebug($"{_config["Greet"]}");
+        return Task.CompletedTask;
+    }
 
-            await host.RunAsync();
-        }
+    public Task StopAsync(CancellationToken cancellationToken)
+    {
+        _log.LogDebug($"{_config["Goodbye"]}");
+        return Task.CompletedTask;
     }
 }
+
