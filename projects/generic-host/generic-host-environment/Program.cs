d0a21c4fa7ff0afc15b2f8ee1da8786228904895
Dody Gunawinata <dodyg@silverkeytech.com>
2022-04-04 09:38:08 +0200

    Clean up

--- projects/generic-host/generic-host-environment/Program.cs
+++ projects/generic-host/generic-host-environment/Program.cs
@@ -1,62 +1,45 @@
-using Microsoft.Extensions.DependencyInjection;
-using System.Threading.Tasks;
-using System.Threading;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-
-namespace PracticalAspNetCore
-{
-    public class HelloWorldService : IHostedService
+await new HostBuilder()
+    .UseEnvironment(Environments.Development) // change this to use other
+    .ConfigureServices((hostContext, services) =>
     {
-        readonly IHostEnvironment _env;
-        readonly ILogger _log;
+        services.AddHostedService<HelloWorldService>();
+    })
+    .ConfigureLogging(logging =>
+    {
+        logging.ClearProviders();
+        logging.AddConsole();
+        logging.AddFilter((provider, category, logLevel) =>
+                {
+          return !category.Contains("Microsoft");
+      });
+    })
+    .Build().RunAsync();
 
-        public HelloWorldService(IHostEnvironment env, ILogger<HelloWorldService> logger)
-        {
-            _env = env;
-            _log = logger;
-        }
+public class HelloWorldService : IHostedService
+{
+    readonly IHostEnvironment _env;
+    readonly ILogger _log;
 
-        public Task StartAsync(CancellationToken cancellationToken)
-        {
-            if (_env.IsProduction())
-                _log.LogDebug("Hello world production");
-            else if (_env.IsStaging())
-                _log.LogDebug("Hello world staging");
-            else if (_env.IsDevelopment())
-                _log.LogDebug("Hello world development");
-
-            return Task.CompletedTask;
-        }
-
-        public Task StopAsync(CancellationToken cancellationToken)
-        {
-            return Task.CompletedTask;
-        }
+    public HelloWorldService(IHostEnvironment env, ILogger<HelloWorldService> logger)
+    {
+        _env = env;
+        _log = logger;
     }
 
-    public class Program
+    public Task StartAsync(CancellationToken cancellationToken)
     {
-        public static async Task Main(string[] args)
-        {
-            var host = new HostBuilder()
-                .UseEnvironment(Environments.Development) // change this to use other
-                .ConfigureServices((hostContext, services) =>
-                {
-                    services.AddHostedService<HelloWorldService>();
-                })
-                .ConfigureLogging(logging =>
-                {
-                  logging.ClearProviders();
-                  logging.AddConsole();
-                  logging.AddFilter((provider, category, logLevel) =>
-                  {
-                      return !category.Contains("Microsoft");
-                  });
-                })
-                .Build();
+        if (_env.IsProduction())
+            _log.LogDebug("Hello world production");
+        else if (_env.IsStaging())
+            _log.LogDebug("Hello world staging");
+        else if (_env.IsDevelopment())
+            _log.LogDebug("Hello world development");
 
-            await host.RunAsync();
-        }
+        return Task.CompletedTask;
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken)
+    {
+        return Task.CompletedTask;
     }
 }
