c1af3a4fb94562975a9e32ab6d6ccd2a7e15efea
Dody Gunawinata <dodyg@silverkeytech.com>
2022-04-06 10:46:24 +0200

    Upgrade to .NET 6

--- projects/logging/logging-3/Program.cs
+++ projects/logging/logging-3/Program.cs
@@ -1,48 +1,21 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
 using System.Text.Json;
 
-namespace PracticalAspNetCore
+var builder = WebApplication.CreateBuilder();
+// Trace, Debug, Information, Warning, Error, Critical, None
+builder.Logging.AddFilter("Microsoft", LogLevel.Warning); //Only show Warning log and above from anything that contains Microsoft.
+builder.Logging.AddFilter("AppLogger", LogLevel.Trace);//Pretty much show everything from AppLogger
+builder.Logging.AddJsonConsole(options =>
 {
-    public class Startup
-    {
-        public void Configure(IApplicationBuilder app, IConfiguration configuration, ILogger<Startup> log)
-        {
-            app.Run(context =>
-            {
-                log.LogInformation("This is a information message");
-                log.LogDebug("This is debug message");
-                return context.Response.WriteAsync(configuration["greeting"]);
-            });
-        }
-    }
+    options.JsonWriterOptions = new JsonWriterOptions { Indented = true }; 
+});
 
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+var app = builder.Build();
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                    webBuilder
-                    .ConfigureLogging(builder =>
-                    {
-                        // Trace, Debug, Information, Warning, Error, Critical, None
-                        builder.AddFilter("Microsoft", LogLevel.Warning); //Only show Warning log and above from anything that contains Microsoft.
-                        builder.AddFilter("AppLogger", LogLevel.Trace);//Pretty much show everything from AppLogger
-                        builder.AddJsonConsole(options =>
-                        {
-                            options.JsonWriterOptions = new JsonWriterOptions { Indented = true }; 
-                        });
-                    })
-                    .UseStartup<Startup>()
-                );
-    }
-}
+app.Run(context =>
+{
+    app.Logger.LogInformation("This is a information message");
+    app.Logger.LogDebug("This is debug message");
+    return context.Response.WriteAsync(app.Configuration["greeting"]);
+});
+
+app.Run();
