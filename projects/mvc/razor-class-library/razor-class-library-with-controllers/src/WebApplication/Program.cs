289326a0cb491e099ae6b0b7a3d3c44e36d6acae
Dody Gunawinata <dodyg@silverkeytech.com>
2022-04-07 09:19:47 +0200

    Upgrade to .NET 6

--- projects/mvc/razor-class-library/razor-class-library-with-controllers/src/WebApplication/Program.cs
+++ projects/mvc/razor-class-library/razor-class-library-with-controllers/src/WebApplication/Program.cs
@@ -1,44 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Hosting;
+﻿var builder = WebApplication.CreateBuilder();
+builder.Services.AddControllersWithViews();
 
-namespace WebApplication
+var app = builder.Build();
+app.MapControllerRoute("areas", "{area}/{controller=Home}/{action=Index}/{id?}");
+app.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
+
+app.Run(async (context) =>
 {
-    public class Startup
-    {
-        public void ConfigureServices(IServiceCollection services)
-        {
-			services.AddControllersWithViews();
-        }
+    context.Response.Headers.Add("Content-Type", "text/html");
 
-        public void Configure(IApplicationBuilder app)
-        {
-            app.UseRouting();
+    await context.Response.WriteAsync(@"<html>
+        <body>
+        <h1>Razor Class Library sample with MVC structure (controllers and views)</h1>
+        <ul><li><a href=""/MyFeature"">Home</a></li><li><a href=""/MyFeature/About"">About (with attribute routing)</a></li></ul>
+        </body>
+        </html>");
+});
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllerRoute("areas", "{area}/{controller=Home}/{action=Index}/{id?}");
-                endpoints.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
-            });
-
-            app.Run(async (context) =>
-            {
-                context.Response.Headers.Add("Content-Type", "text/html");
-
-                await context.Response.WriteAsync(@"<html>
-                <body>
-                <h1>Razor Class Library sample with MVC structure (controllers and views)</h1>
-                <ul><li><a href=""/MyFeature"">Home</a></li><li><a href=""/MyFeature/About"">About (with attribute routing)</a></li></ul>
-                </body>
-                </html>");
-			});
-        }
-    }
-
-
-}
+app.Run();
