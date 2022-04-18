1aee3e78a732621388141ab1dd4dd561249e2e5c
Dody Gunawinata <dodyg@silverkeytech.com>
2022-04-18 11:15:49 +0200

    Update to .NET 6

--- projects/yarp/basic-demo/Yarp.Demo.FrontEnd/Program.cs
+++ projects/yarp/basic-demo/Yarp.Demo.FrontEnd/Program.cs
@@ -1,25 +1,10 @@
-using System;
-using System.Net.Http;
-using System.Collections.Generic;
-using System.Threading.Tasks;
-using System.Text;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
+using Yarp.Demo.FrontEnd;
 
-namespace Yarp.Demo.FrontEnd
-{
-    public class Program
-    {
-        public static async Task Main(string[] args)
-        {
-            var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.RootComponents.Add<App>("#app");
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
 
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-            await builder.Build().RunAsync();
-        }
-    }
-}
+await builder.Build().RunAsync();
+
