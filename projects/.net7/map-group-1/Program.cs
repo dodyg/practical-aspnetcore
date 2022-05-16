a1a1dce7870a64f55ad0a22dafa156b81b7ae464
Dody Gunawinata <dodyg@silverkeytech.com>
2022-05-16 11:00:03 +0200

    Add a sample for map group

---
+++ projects/.net7/map-group-1/Program.cs
@@ -0,0 +1,37 @@
+var builder = WebApplication.CreateBuilder();
+var app = builder.Build();
+
+app.MapGet("/", () => Results.Content("""
+<html>
+<head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+</head>
+<body>
+    <div class="container">
+        <ul>
+            <li><a href="/about">/about</a></li>
+            <li><a href="/about/us">/about/us</a></li>
+            <li><a href="/about/all">/about/all</a></li>
+        </ul>
+    </div>
+</body>
+</html>
+""", "text/html"));
+
+
+app.MapGroup("/about").MapAboutApi();
+
+app.Run();
+
+public static class AboutApi
+{
+    public static GroupRouteBuilder MapAboutApi(this GroupRouteBuilder group)
+    {
+        group.MapGet("/", () => Results.Ok("about"));
+        group.MapGet("/us", () => Results.Ok("Us"));
+        group.MapGet("/all", () => Results.Ok("All"));
+
+        return group;
+    }
+}
+
---
+++ projects/.net7/map-group-1/README.md
@@ -0,0 +1,3 @@
+# Route Groups
+
+`MapGroup()` extension methods allow grouping of endpoints with a common prefix. It also allow group metadata to be attached to the group.
