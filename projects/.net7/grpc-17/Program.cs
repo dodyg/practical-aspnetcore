6089a6ea849a5390b44e42082561cbdcdc7aff56
Dody Gunawinata <dodyg@silverkeytech.com>
2022-06-01 08:44:56 +0200

    Add gRPC JSON transcoding PATCH

--- projects/.net7/grpc-17/Program.cs
+++ projects/.net7/grpc-17/Program.cs
@@ -1,9 +1,9 @@
 using Grpc.Core;
 using Microsoft.AspNetCore.Server.Kestrel.Core;
-using Microsoft.AspNetCore.Grpc.JsonTranscoding;
 
 var builder = WebApplication.CreateBuilder();
 builder.Services.AddGrpc().AddJsonTranscoding();
+builder.Services.AddSingleton<Accumulator>();
 builder.WebHost.ConfigureKestrel(k =>
 {
     k.ConfigureEndpointDefaults(options => options.Protocols = HttpProtocols.Http2);
@@ -19,13 +19,11 @@ app.MapGet("/", () => Results.Content("""
 <body>
 
 <div class="container">
-    <h1>gRPC JSON Transcoding PUT</h1>
+    <h1>gRPC JSON Transcoding PATCH</h1>
     <div class="row mb-3">
         <div class="col-md-3">
             <form>
-                <input type="text" class="form-control" id="senderInput" placeholder="Please enter sender" /><br/>
-                <input type="text" class="form-control" id="cityInput" placeholder="Please enter city" /><br/>
-                <input type="number"  min="18" step="1" class="form-control" id="ageInput" placeholder="Please enter age" /><br/>
+                <input type="number"  min="18" step="1" class="form-control" id="numberInput" placeholder="Please enter a number" /><br/>
                 <button type="button" id="postBtn" class="btn btn-primary">Send</button>
             </form>
         </div>
@@ -33,16 +31,8 @@ app.MapGet("/", () => Results.Content("""
     <div class="card">
         <div class="card-body">
             <p>
-            sender:<br/>
-            <span id="sender"/>
-            </p>
-            <p>
-            city:<br/>
-            <span id="city"/>
-            </p>
-            <p>
-            sender:<br/>
-            <span id="age"/>
+            total:<br/>
+            <span id="total"/>
             </p>
         </div>
     </div>
@@ -50,33 +40,27 @@ app.MapGet("/", () => Results.Content("""
 <script>
     const postBtn = document.getElementById("postBtn");
     postBtn.addEventListener("click", function() {
-        const senderInput = document.getElementById("senderInput");
-        const cityInput = document.getElementById("cityInput");
-        const ageInput = document.getElementById("ageInput");
+        const numberInput = document.getElementById("numberInput");
 
-        const input = { sender: senderInput.value, city: cityInput.value, age: parseInt(ageInput.value) };
+        const input = { number : parseInt(numberInput.value) };
         grpc(input).then( result => { 
-            document.getElementById("sender").innerHTML = result.receivedFrom;
-            document.getElementById("city").innerHTML = result.receivedCity;
-            document.getElementById("age").innerHTML = result.receivedAge;
-            console.log(result);
+            document.getElementById("total").innerHTML = result.totalNumber;
         });
     });
 
     async function grpc(input){
         const payload = {
-            city: input.city,
-            age: input.age
+            addNumber: input.number
         };
 
         console.log("Payload ", payload);
 
-        const result = await fetch(`https://localhost:5500/v1/message/${input.sender}`, {
+        const result = await fetch(`https://localhost:5500/v1/message`, {
             headers: {
                 'Content-Type' : 'application/json'
             },
             body: JSON.stringify(payload),
-            method: 'PUT'
+            method: 'PATCH'
         });
         const response = await result.json();
         return response
@@ -88,16 +72,29 @@ app.MapGet("/", () => Results.Content("""
 
 app.Run();
 
+public class Accumulator
+{
+    public int Total { get; set;}
+
+    public void Add(int number) => Total += number;
+}
+
 public class BillboardService : Billboard.Board.BoardBase
 {
+    Accumulator _acc;
+
+    public BillboardService(Accumulator acc) 
+    {
+        _acc = acc;
+    }
+    
     public override Task<Billboard.MessageReply> ShowMessage(Billboard.MessageRequest request, ServerCallContext context)
     {
-        var now = DateTime.UtcNow;
+        _acc.Add(request.AddNumber);
+
         return Task.FromResult(new Billboard.MessageReply
         {
-            ReceivedFrom = request.Sender,
-            ReceivedAge = request.Age,
-            ReceivedCity = request.City
+            TotalNumber = _acc.Total
         });
     }
 }
---
+++ projects/.net7/grpc-17/README.md
@@ -0,0 +1,3 @@
+# gRPC JSON transcoding PATCH
+
+This sample shows how to make a PATCH call to a gRPC endpoint via gRPC JSON transcoding
