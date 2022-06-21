6bb88047899df7505bb514f3c637f80f10572209
Dody Gunawinata <dodyg@silverkeytech.com>
2022-06-21 10:01:59 +0200

    Start a span and add event

---
+++ projects/open-telemetry/open-telemetry-2/Program.cs
@@ -0,0 +1,37 @@
+using System.Diagnostics;
+
+using OpenTelemetry.Trace;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Exporter;
+
+var builder = WebApplication.CreateBuilder();
+builder.Services.AddOpenTelemetryTracing(b =>
+{
+    b.SetResourceBuilder(ResourceBuilder.CreateDefault().AddService(builder.Environment.ApplicationName))
+     .AddAspNetCoreInstrumentation()
+     .AddConsoleExporter( options => options.Targets = ConsoleExporterOutputTargets.Console);
+});
+
+var activitySource = new ActivitySource("open-telemetry-2-sample", "1.0.0");
+
+WebApplication app = builder.Build();
+app.Run(async context =>
+{
+    if (context.Request.Path == "/")
+    {
+        using(var activity = activitySource.StartActivity("ShowPage", ActivityKind.Producer))
+        {
+            await Task.Delay(2000);
+            Activity.Current?.AddEvent(new ActivityEvent("Getting trace id"));
+            var traceId = Activity.Current?.TraceId;
+            await context.Response.WriteAsync($"Trace Id {traceId}");
+            Activity.Current?.AddEvent(new ActivityEvent("After showing trace id"));
+
+            await Task.Delay(2000);
+            await context.Response.WriteAsync($"\nHello World");
+            Activity.Current?.AddEvent(new ActivityEvent("After showing hello world"));
+        }
+    }
+});
+
+await app.RunAsync();
---
+++ projects/open-telemetry/open-telemetry-2/Readme.md
@@ -0,0 +1,29 @@
+# Start a Span and record event
+
+Start creating `ActivitySource`, start a span (`Activity`) with it and record events in the span.
+
+```
+Activity.TraceId:          5e1f4ddfeecde7b52a3d7823fd2256a3
+Activity.SpanId:           442681a750f843b8
+Activity.TraceFlags:           Recorded
+Activity.ActivitySourceName: OpenTelemetry.Instrumentation.AspNetCore
+Activity.DisplayName: /
+Activity.Kind:        Server
+Activity.StartTime:   2022-06-21T08:01:16.0231599Z
+Activity.Duration:    00:00:04.0263921
+Activity.Tags:
+    http.host: localhost:5000
+    http.method: GET
+    http.target: /
+    http.url: http://localhost:5000/
+    http.user_agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:101.0) Gecko/20100101 Firefox/101.0
+    http.status_code: 200
+   StatusCode : UNSET
+Activity.Events:
+    Getting trace id [6/21/2022 8:01:18 AM +00:00]
+    After showing trace id [6/21/2022 8:01:18 AM +00:00]
+    After showing hello world [6/21/2022 8:01:20 AM +00:00]
+Resource associated with Activity:
+    service.name: open-telemetry-2
+    service.instance.id: 49c0f853-4ba3-4920-b2d5-6c1147c40402
+```
