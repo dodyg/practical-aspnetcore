dd61ba59551fb7b98ece3990c85c04a990a04b2f
Dody Gunawinata <dodyg@silverkeytech.com>
2022-06-28 09:38:42 +0200

    Add OTel baggage sample

---
+++ projects/open-telemetry/open-telemetry-4/Program.cs
@@ -0,0 +1,38 @@
+using System.Diagnostics;
+using OpenTelemetry.Trace;
+using OpenTelemetry.Resources;
+
+var builder = WebApplication.CreateBuilder();
+builder.Services.AddOpenTelemetryTracing(b =>
+{
+    b.SetResourceBuilder(ResourceBuilder.CreateDefault().AddService(builder.Environment.ApplicationName));
+});
+builder.Services.AddHttpClient();
+builder.Logging.SetMinimumLevel(LogLevel.Information);
+
+WebApplication app = builder.Build();
+app.MapGet("/", async (HttpRequest request, IHttpClientFactory clientFactory) =>
+{
+        Activity.Current?.AddBaggage ("project", "practical-aspnetcore");
+        Activity.Current?.AddBaggage ("location", "Cairo");
+
+        var baggage = string.Join(",", Activity.Current.Baggage.ToDictionary(b => b.Key, b => b.Value).Select(x => x.Key + "=" + x.Value));  
+
+        var client = clientFactory.CreateClient();
+        client.DefaultRequestHeaders.Add("baggage", baggage);
+
+        var url = request.Scheme + "://" + request.Host + "/baggage";
+        app.Logger.LogInformation("REQUEST URL " + url);
+        var response = await client.GetAsync(url);
+
+        return Results.Text("Check the log", "text/plain");
+});
+
+app.MapGet("/baggage", (HttpRequest request) =>
+{
+    var baggage = request.Headers["baggage"];
+    app.Logger.LogInformation($"BAGGAGE VALUES {baggage}");
+    return Results.Ok();
+});
+
+app.Run();
---
+++ projects/open-telemetry/open-telemetry-4/Readme.md
@@ -0,0 +1,3 @@
+# Record OTel baggage and pass it to HttpClient
+
+This sample is about setting up [OTel baggage](https://www.w3.org/TR/baggage/), which pass user-defined properties via `baggage` HTTP header, to the next HTTP request.  
