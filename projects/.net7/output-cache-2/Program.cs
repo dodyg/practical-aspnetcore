0e23c4d20ea94245d6a0e0b635fd65ce2ddf1358
Dody Gunawinata <dodyg@silverkeytech.com>
2022-07-19 09:52:34 +0200

    Add sample for output cache vary by query string

---
+++ projects/.net7/output-cache-2/Program.cs
@@ -0,0 +1,26 @@
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddOutputCache();
+
+var app = builder.Build();
+app.MapGet("/", () =>
+{
+    return Results.Content("""
+    <html>
+        <body>
+            <ul>
+                <li><a href="/now">DateTime.UtcNow()</a></li>
+                <li><a href="/cached-now?version=1">DateTime.UtcNow() cached version 1</a></li>
+                <li><a href="/cached-now?version=2">DateTime.UtcNow() cached version 2</a></li>
+                <li><a href="/cached-now?version=1&culture=en">DateTime.UtcNow() cached version 1 in en culture</a></li>
+                <li><a href="/cached-now?version=1&culture=ar">DateTime.UtcNow() cached version 1 in ar culture</a></li>
+            </ul>
+        </body>
+    </html>
+    """, "text/html");
+});
+
+app.UseOutputCache();
+app.MapGet("/now", () => DateTime.UtcNow.ToString());
+app.MapGet("/cached-now", () => DateTime.UtcNow.ToString()).CacheOutput(p => p.VaryByQuery("version", "culture"));
+
+app.Run();
---
+++ projects/.net7/output-cache-2/Readme.md
@@ -0,0 +1,4 @@
+# Output Cache vary by query string
+
+This example a very basic example of using output cache that vary by query string e.g. `?page=1` or `?version=1`. You can add more than one query string. 
+
