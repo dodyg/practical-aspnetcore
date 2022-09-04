a7dd55f053636bf100871ad1af051a626d0b0ce4
Dody Gunawinata <dodyg@silverkeytech.com>
2022-09-04 09:21:16 +0200

    Simplify sample

--- projects/configurations/configuration-IOption/Program.cs
+++ projects/configurations/configuration-IOption/Program.cs
@@ -2,15 +2,12 @@ using Microsoft.Extensions.Options;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.Configure<MyOptions>(
-    builder.Configuration.GetSection("MyOptions"));
+builder.Services.Configure<MyOptions>(builder.Configuration.GetSection("MyOptions"));
 
 var app = builder.Build();
 
-
-
 app.MapGet("/", (IOptions<MyOptions> optionsDelegate) => 
-optionsDelegate.Value.Option2
+    optionsDelegate.Value.Option2
 );
 
 app.Run();
@@ -18,7 +15,7 @@ app.Run();
 
 public class MyOptions
 {
-    public string Option1 { get; set; }
+    public string Option1 { get; set; } = string.Empty;
     public int Option2 { get; set; }
 }
 
