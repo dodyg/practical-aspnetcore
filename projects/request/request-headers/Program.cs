1286deef65bcf8623d95ca44763ee245ed36908a
Ayub Kokabi <44271870+kokabi1365@users.noreply.github.com>
2022-09-21 22:36:12 +0430

    Refactor

--- projects/request/request-headers/Program.cs
+++ projects/request/request-headers/Program.cs
@@ -3,12 +3,13 @@ var app = WebApplication.Create();
 app.Run(async context =>
 {
     context.Response.Headers.Add("content-type", "text/html");
+    
     await context.Response.WriteAsync("<h1>Request Headers</h1>");
     await context.Response.WriteAsync("<ul>");
+    
     foreach (var h in context.Request.Headers)
-    {
         await context.Response.WriteAsync($"<li>{h.Key} : {h.Value}</li>");
-    }
+    
     await context.Response.WriteAsync("</ul>");
 });
 
