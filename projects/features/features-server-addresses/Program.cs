77c3906e53ac4421c25ccda65a78c0112f346b48
Ayub Kokabi <44271870+kokabi1365@users.noreply.github.com>
2022-09-21 18:21:13 +0430

    Refactor

--- projects/features/features-server-addresses/Program.cs
+++ projects/features/features-server-addresses/Program.cs
@@ -2,13 +2,17 @@ using Microsoft.AspNetCore.Hosting.Server.Features;
 
 var app = WebApplication.Create();
 var serverAddress = (app as IApplicationBuilder).ServerFeatures.Get<IServerAddressesFeature>();
+
 app.Run(context =>
 {
     var str = string.Empty;
-    foreach(var a in serverAddress.Addresses)
-    {
-        str += $"{a}\n";
-    }
+
+    if (serverAddress != null)
+        foreach (var address in serverAddress.Addresses)
+        {
+            str += $"{address}\n";
+        }
+
     return context.Response.WriteAsync(str);
 });
 
