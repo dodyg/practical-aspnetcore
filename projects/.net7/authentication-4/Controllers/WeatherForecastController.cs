8e9bd0f901aa7baef7b762ea00e4d07fda319b73
khusroo hayat <khusroo.hayat@gmail.com>
2022-12-10 23:07:21 +0500

    Add jwt bearer

--- projects/.net7/authentication-4/Controllers/WeatherForecastController.cs
+++ projects/.net7/authentication-4/Controllers/WeatherForecastController.cs
@@ -1,9 +1,11 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
 
 namespace authentication_4.Controllers;
 
 [ApiController]
 [Route("[controller]")]
+[Authorize]
 public class WeatherForecastController : ControllerBase
 {
     private static readonly string[] Summaries = new[]
--- projects/.net7/authentication-4/Program.cs
+++ projects/.net7/authentication-4/Program.cs
@@ -1,4 +1,4 @@
-var builder = WebApplication.CreateBuilder(args);
+ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
@@ -6,6 +6,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddAuthentication()
+    .AddJwtBearer();
 
 var app = builder.Build();
 
