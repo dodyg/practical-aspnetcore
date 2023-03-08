96add877ad645112f807f498babeb9c79c9df5ac
Dody Gunawinata <dodyg@silverkeytech.com>
2023-03-08 10:14:50 +0200

    Add sample on how to pass values to workflow via constructor

---
+++ projects/elsa/workflow-5/Program.cs
@@ -0,0 +1,37 @@
+using Elsa.Extensions;
+using Elsa.Workflows.Core.Activities;
+using Elsa.Workflows.Core.Models;
+using Elsa.Workflows.Core.Services;
+
+var services = new ServiceCollection();
+services.AddElsa();
+
+var serviceProvider = services.BuildServiceProvider();
+var runner = serviceProvider.GetRequiredService<IWorkflowRunner>();
+
+await runner.RunAsync(new ConstructorWorkflow("Anne", 37));
+
+public class ConstructorWorkflow : WorkflowBase
+{
+    private readonly Variable<string> _name;
+    private readonly Variable<int> _age;
+
+    public ConstructorWorkflow(string name, int age)
+    {
+        _name = new Variable<string>(name);
+        _age = new Variable<int>(age);
+    }
+
+    protected override void Build(IWorkflowBuilder builder)
+    {
+        builder.Root = new Sequence
+        {
+            Variables = { _name, _age },
+            Activities = 
+            {
+                new WriteLine(ctx => $"Name: {_name.Get(ctx)}"),
+                new WriteLine(ctx => $"Age: {_age.Get(ctx)}")
+            }
+        };
+    }
+}
---
+++ projects/elsa/workflow-5/README.md
@@ -0,0 +1,3 @@
+# Passing values to workflow via constructor
+
+This sample demonstrates on how to pass values to a workflow via constructor. 
