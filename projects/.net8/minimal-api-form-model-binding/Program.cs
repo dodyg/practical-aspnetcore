b63bc7685599996e370aa340af8b627afe503425
Dody Gunawinata <dodyg@silverkeytech.com>
2023-07-24 15:40:10 +0300

    Clean up

--- projects/.net8/minimal-api-form-model-binding/Program.cs
+++ projects/.net8/minimal-api-form-model-binding/Program.cs
@@ -58,11 +58,6 @@ static string Template(string body)
     <head>
       <title>Form Model Binding</title>
       <link href="https://fastly.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
-      <style>
-        .error-feedback {
-            color: red;
-        }
-      </style>
     </head>
     <body>
         <div class="container">
