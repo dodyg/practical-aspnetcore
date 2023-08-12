1c8eea8890d06cb37feb45683de8cdb68844daaf
Dody Gunawinata <dodyg@silverkeytech.com>
2023-08-12 12:48:17 +0300

    Broken sample

---
+++ projects/.net8/ComponentTwentySix/Program.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.AspNetCore.Components;
+using ComponentTwentySix;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("app");
+builder.Services.AddCascadingValue("hello", sp => 
+{
+    var msg = new MessageCascade("Hello World! " + DateTime.Now);
+    var source = new CascadingValueSource<MessageCascade>(msg, isFixed: false);
+    return source;
+});
+
+var app = builder.Build();
+await app.RunAsync();
+
+public record MessageCascade(string Message);
