d0266188023b722d168522e8f80440e9fd43c1d4
Dody Gunawinata <dodyg@silverkeytech.com>
2023-09-02 15:52:35 +0300

    WIP keyed service from Minimal  API

---
+++ projects/.net8/keyed-service/Program.cs
@@ -0,0 +1,57 @@
+var builder = WebApplication.CreateBuilder();
+
+builder.Services.AddKeyedSingleton<IGreeting, MorningGreeting>("morning");
+builder.Services.AddKeyedSingleton<IGreeting, DayGreeting>("day");
+builder.Services.AddKeyedSingleton<IGreeting, EveningGreeting>("evening");
+
+
+var app = builder.Build();
+
+app.MapGet("/", (IKeyedServiceProvider provider) => 
+{
+    string GetServiceKey()
+    {
+        var currentTime = DateTime.Now;
+        if (currentTime.Hour < 12)
+            return "morning";
+        else if (currentTime.Hour < 18)
+            return "day";
+        else
+            return "evening";
+    }
+
+    var key = GetServiceKey();
+    var greeting = provider.GetRequiredKeyedService<IGreeting>(key);
+
+    return Results.Content("""
+    <html>
+    <body>
+        {{ greeting.Message }}}
+    </body>
+    </html>
+    """, "text/html");
+});
+
+app.Run();
+
+
+interface IGreeting
+{
+    string Message { get; }
+}
+
+public class MorningGreeting : IGreeting
+{
+    public string Message => "Good morning";
+}
+
+public class DayGreeting : IGreeting
+{
+    public string Message => "Good day";
+}
+
+public class EveningGreeting : IGreeting
+{
+    public string Message => "Good evening";
+}
+
---
+++ projects/.net8/keyed-service/README.md
@@ -0,0 +1,3 @@
+# Using Keyed Service with Minimal API
+
+Use `IKeyedServiceProvider.GetRequiredKeyedService<T>(key)` to get different type of implementation instances based on string key.
