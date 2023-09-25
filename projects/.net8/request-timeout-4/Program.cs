3274b37d28576fe41d1311d9a790073c4c843b46
Dody Gunawinata <dodyg@silverkeytech.com>
2023-09-25 10:42:23 +0300

    Add RequestTimeout sample

---
+++ projects/.net8/request-timeout-4/Program.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Http.Timeouts;
+using Microsoft.AspNetCore.Mvc;
+
+var builder = WebApplication.CreateBuilder();
+builder.Services.AddControllers();
+
+var app = builder.Build();
+app.UseRequestTimeouts();
+app.MapControllers();
+
+app.Run();
+
+public class HomeController : ControllerBase
+{
+    [HttpGet("/")]
+    [RequestTimeout(milliseconds: 1)]
+    public async Task<IActionResult> Index()
+    {
+        await Task.Delay(100);
+        HttpContext.RequestAborted.ThrowIfCancellationRequested();
+        return Ok("Hello World!");
+    }
+}
---
+++ projects/.net8/request-timeout-4/README.md
@@ -0,0 +1,3 @@
+# Use RequestTimeout attribute
+
+Use `RequestTimeout` attribute to specify how many milliseconds a time out will be trigerred. 
