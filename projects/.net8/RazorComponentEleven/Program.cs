b90276f2e8823fe7638199028647ab61ad5716cc
Dody Gunawinata <dodyg@silverkeytech.com>
2023-10-14 16:45:44 +0300

    Now EditForm must have FormName

--- projects/.net8/RazorComponentEleven/Program.cs
+++ projects/.net8/RazorComponentEleven/Program.cs
@@ -1,7 +1,8 @@
 var builder = WebApplication.CreateBuilder();
 builder.Services.AddRazorComponents();
-
+builder.Services.AddAntiforgery();
 var app = builder.Build();
+app.UseAntiforgery();
 app.MapRazorComponents<RazorComponentEleven.App>();
 app.Run();
 
