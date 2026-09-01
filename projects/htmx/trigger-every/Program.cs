ed7eddc54b89748a688e46299bfe0098c90f3040
Dody Gunawinata <dodyg@silverkeytech.com>
2026-09-01 15:08:23 +0300

    validate sample

--- projects/htmx/trigger-every/Program.cs
+++ projects/htmx/trigger-every/Program.cs
@@ -6,13 +6,14 @@ app.MapGet("/", () =>
         <!DOCTYPE html>
         <html>
             <head>
+                <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.min.css">
                 <style>
                     div[hx-get]{
                         cursor:pointer;
                     }
                 </style>
             </head>
-            <body>
+            <body class="container">
                 <div hx-get="/htmx" hx-trigger="every 1s">..wait</div>
                 <script src="https://cdn.jsdelivr.net/npm/htmx.org@4.0.0/dist/htmx.min.js"></script>
             </body>
