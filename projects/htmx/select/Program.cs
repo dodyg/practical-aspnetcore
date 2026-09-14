e8bb4a01cc3171b1e2da7a2dba060281cfba6b94
Dody Gunawinata <dodyg@silverkeytech.com>
2026-09-14 15:42:48 +0300

    validate sample

--- projects/htmx/select/Program.cs
+++ projects/htmx/select/Program.cs
@@ -15,64 +15,64 @@ app.MapGet("/", (HttpContext context, [FromServices] IAntiforgery anti) =>
         <!DOCTYPE html>
         <html>
             <head>
+                <meta name="htmx-config" content='{ "antiForgery": {"headerName" : "{{ token.HeaderName}}", "requestToken" : "{{token.RequestToken }}" } }'>
+                <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.min.css">
                 <style>
                     li{
                         cursor:pointer;
                     }
                 </style>
-                <meta name="htmx-config" content='{ "antiForgery": {"headerName" : "{{ token.HeaderName}}", "requestToken" : "{{token.RequestToken }}" } }'>
-                <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
             </head>
             <body>
-            <div class="container">
-            <h1>Select element from the server response</h1>
-            <p>Click on the below links to see the response</p>
-            
-            <div class="row mt-5">
-                <div class="col-md-6">
-                    <h2>Without hx-select</h2>
-                    <ul>
-                        <li hx-get="/htmx">GET</li>
-                        <li hx-post="/htmx">POST</li>
-                        <li hx-put="/htmx">PUT</li>
-                        <li hx-patch="/htmx">PATCH</li>
-                        <li hx-delete="/htmx">DELETE</li>
-                    </ul>
+                <div class="container">
+                <h1>Select element from the server response</h1>
+                <p>Click on the below links to see the response</p>
+                
+                <div class="row mt-5">
+                    <div class="col-md-6">
+                        <h2>Without hx-select</h2>
+                        <ul>
+                            <li hx-get="/htmx">GET</li>
+                            <li hx-post="/htmx">POST</li>
+                            <li hx-put="/htmx">PUT</li>
+                            <li hx-patch="/htmx">PATCH</li>
+                            <li hx-delete="/htmx">DELETE</li>
+                        </ul>
+                    </div>
+                    <div class="col-md-6">
+                        <h2>With hx-select</h2>
+                        <ul hx-select:inherited="#result">
+                            <li hx-get="/htmx">GET</li>
+                            <li hx-post="/htmx">POST</li>
+                            <li hx-put="/htmx">PUT</li>
+                            <li hx-patch="/htmx">PATCH</li>
+                            <li hx-delete="/htmx">DELETE</li>
+                        </ul>
+                    </div>
                 </div>
-                <div class="col-md-6">
-                    <h2>With hx-select</h2>
-                    <ul hx-select:inherited="#result">
-                        <li hx-get="/htmx">GET</li>
-                        <li hx-post="/htmx">POST</li>
-                        <li hx-put="/htmx">PUT</li>
-                        <li hx-patch="/htmx">PATCH</li>
-                        <li hx-delete="/htmx">DELETE</li>
-                    </ul>
                 </div>
-            </div>
-            </div>
-            <script src="https://cdn.jsdelivr.net/npm/htmx.org@4.0.0/dist/htmx.min.js"></script>
-            <script>
-                document.addEventListener("htmx:config:request", (evt) => {
-                    let httpVerb = evt.detail.ctx.request.method.toUpperCase();
-                    if (httpVerb === 'GET') return;
-                    
-                    let antiForgery = htmx.config.antiForgery;
-                    if (antiForgery) {
-                        // already specified on form, short circuit
-                        if (evt.detail.ctx.request.body.has(antiForgery.formFieldName))
-                            return;
+                <script src="https://cdn.jsdelivr.net/npm/htmx.org@4.0.0/dist/htmx.min.js"></script>
+                <script>
+                    document.addEventListener("htmx:config:request", (evt) => {
+                        let httpVerb = evt.detail.ctx.request.method.toUpperCase();
+                        if (httpVerb === 'GET') return;
                         
-                        if (antiForgery.headerName) {
-                            evt.detail.ctx.request.headers[antiForgery.headerName] = antiForgery.requestToken;
+                        let antiForgery = htmx.config.antiForgery;
+                        if (antiForgery) {
+                            // already specified on form, short circuit
+                            if (evt.detail.ctx.request.body.has(antiForgery.formFieldName))
+                                return;
+                            
+                            if (antiForgery.headerName) {
+                                evt.detail.ctx.request.headers[antiForgery.headerName] = antiForgery.requestToken;
 
-                        } else {
-                            evt.detail.ctx.request.body.set(antiForgery.formFieldName, antiForgery.requestToken);
+                            } else {
+                                evt.detail.ctx.request.body.set(antiForgery.formFieldName, antiForgery.requestToken);
 
+                            }
                         }
-                    }
-                });
-            </script>
+                    });
+                </script>
             </body>
         </html>
     """;
